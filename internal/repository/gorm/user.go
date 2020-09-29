0d1063f8c48f32b1b2dbdc32affc39e1d5140c60
sunguroku <65516095+sunguroku@users.noreply.github.com>
2020-09-29 13:19:47 -0400

    login endpoint

--- internal/repository/gorm/user.go
+++ internal/repository/gorm/user.go
@@ -34,6 +34,15 @@ func (repo *UserRepository) ReadUser(id uint) (*models.User, error) {
 	return user, nil
 }
 
+// ReadUserByEmail finds a single user based on their Email. Used primarily for Login.
+func (repo *UserRepository) ReadUserByEmail(email string) (*models.User, error) {
+	u := &models.User{}
+	if err := repo.db.Where("email = ?", email).First(&u).Error; err != nil {
+		return nil, err
+	}
+	return u, nil
+}
+
 // UpdateUser modifies an existing User in the database
 func (repo *UserRepository) UpdateUser(user *models.User) (*models.User, error) {
 	if err := repo.db.First(&models.User{}, user.ID).Updates(user).Error; err != nil {

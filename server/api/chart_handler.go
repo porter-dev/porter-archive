c08dbc98135f273b3c49d7996a01f32a39099b51
Alexander Belanger <belanger@sas.upenn.edu>
2020-10-02 15:43:05 -0400

    remove required fields on update

--- server/api/chart_handler.go
+++ server/api/chart_handler.go
@@ -7,4 +7,6 @@ func (app *App) HandleListCharts(w http.ResponseWriter, r *http.Request) {
 	// get the user id
 
 	// create a client config using the app's helm/kubernetes agents
+
+	// call the list charts method
 }

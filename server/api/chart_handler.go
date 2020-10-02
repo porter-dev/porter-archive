ad338c62f74f83fb30dbb7f24965f586751ef15a
Alexander Belanger <belanger@sas.upenn.edu>
2020-10-02 15:33:48 -0400

    initial helm list agent

---
+++ server/api/chart_handler.go
@@ -0,0 +1,10 @@
+package api
+
+import "net/http"
+
+// TODO -- IMPLEMENT
+func (app *App) HandleListCharts(w http.ResponseWriter, r *http.Request) {
+	// get the user id
+
+	// create a client config using the app's helm/kubernetes agents
+}

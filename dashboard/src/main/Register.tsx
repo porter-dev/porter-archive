b87c7a4922fbdee4514a54a970def475c491e756
Alexander Belanger <belanger@sas.upenn.edu>
2020-10-07 15:06:08 -0400

    fix merge conflict

--- dashboard/src/main/Register.tsx
+++ dashboard/src/main/Register.tsx
@@ -48,7 +48,7 @@ export default class Register extends Component<PropsType, StateType> {
         email: email,
         password: password
       }, {}, (err: any, res: any) => {
-        err ? setCurrentError(err.response.data.errors[0]) : authenticate();
+        err ? setCurrentError(JSON.stringify(err)) : authenticate();
       });
     } 
   };

964e2e026a4df020bc06e035167d57cafcf95451
sunguroku <65516095+sunguroku@users.noreply.github.com>
2020-11-07 02:01:52 -0500

    get pod logs by label selectors - backend

--- server/router/router.go
+++ server/router/router.go
@@ -46,6 +46,7 @@ func New(
 		// /api/k8s routes
 		r.Method("GET", "/k8s/namespaces", auth.BasicAuthenticate(requestlog.NewHandler(a.HandleListNamespaces, l)))
 		r.Method("GET", "/k8s/{namespace}/pod/{name}/logs", auth.BasicAuthenticate(requestlog.NewHandler(a.HandleGetPodLogs, l)))
+		r.Method("GET", "/k8s/pods", auth.BasicAuthenticate(requestlog.NewHandler(a.HandleListPods, l)))
 	})
 
 	fs := http.FileServer(http.Dir(staticFilePath))

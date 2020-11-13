1de8b805956fdcfce1b97976047848038619be94
jusrhee <jusrhee@sas.upenn.edu>
2020-11-12 20:41:50 -0500

    project deletion boilerplate + fixed revision preview w/ podselectors

---
+++ dashboard/src/main/home/modals/UpdateProjectModal.tsx
@@ -0,0 +1,175 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import close from '../../../assets/close.png';
+import gradient from '../../../assets/gradient.jpg';
+
+import api from '../../../shared/api';
+import { Context } from '../../../shared/Context';
+
+import SaveButton from '../../../components/SaveButton';
+import InputRow from '../../../components/values-form/InputRow';
+import ConfirmOverlay from '../../../components/ConfirmOverlay';
+
+type PropsType = {
+};
+
+type StateType = {
+  projectName: string,
+  status: string | null,
+  showDeleteOverlay: boolean
+};
+
+export default class UpdateProjectModal extends Component<PropsType, StateType> {
+  state = {
+    projectName: this.context.currentModalData.currentProject.name,
+    status: null as string | null,
+    showDeleteOverlay: false,
+  };
+  
+  handleDelete = () => {
+    alert('ay lmao');
+  }
+
+  render() {
+    return (
+      <StyledCreateProjectModal>
+        <CloseButton onClick={() => {
+          this.context.setCurrentModal(null, null);
+        }}>
+          <CloseButtonImg src={close} />
+        </CloseButton>
+
+        <ModalTitle>Project Settings</ModalTitle>
+        <Subtitle>
+          Project name
+        </Subtitle>
+
+        <InputWrapper>
+          <ProjectIcon>
+            <ProjectImage src={gradient} />
+            <Letter>{this.state.projectName ? this.state.projectName[0].toUpperCase() : '-'}</Letter>
+          </ProjectIcon>
+          <InputRow
+            disabled={true}
+            type='string'
+            value={this.state.projectName}
+            setValue={(x: string) => this.setState({ projectName: x })}
+            placeholder='ex: perspective-vortex'
+            width='470px'
+          />
+        </InputWrapper>
+
+        <SaveButton
+          text='Delete Project'
+          color='#b91133'
+          onClick={() => this.setState({ showDeleteOverlay: true })}
+          status={this.state.status}
+        />
+
+        <ConfirmOverlay
+          show={this.state.showDeleteOverlay}
+          message={`Are you sure you want to delete ${this.state.projectName}?`}
+          onYes={this.handleDelete}
+          onNo={() => this.setState({ showDeleteOverlay: false })}
+        />
+      </StyledCreateProjectModal>
+      );
+  }
+}
+
+UpdateProjectModal.contextType = Context;
+
+const Letter = styled.div`
+  height: 100%;
+  width: 100%;
+  position: absolute;
+  background: #00000028;
+  top: 0;
+  left: 0;
+  display: flex;
+  color: white;
+  align-items: center;
+  justify-content: center;
+`;
+
+const ProjectImage = styled.img`
+  width: 100%;
+  height: 100%;
+`;
+
+const ProjectIcon = styled.div`
+  width: 25px;
+  min-width: 25px;
+  height: 25px;
+  border-radius: 3px;
+  overflow: hidden;
+  position: relative;
+  margin-right: 10px;
+  font-weight: 400;
+  margin-top: 14px;
+`;
+
+const InputWrapper = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const Subtitle = styled.div`
+  margin-top: 23px;
+  font-family: 'Work Sans', sans-serif;
+  font-size: 13px;
+  color: #aaaabb;
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  margin-bottom: -10px;
+`;
+
+const ModalTitle = styled.div`
+  margin: 0px 0px 13px;
+  display: flex;
+  flex: 1;
+  font-family: 'Assistant';
+  font-size: 18px;
+  color: #ffffff;
+  user-select: none;
+  font-weight: 700;
+  align-items: center;
+  position: relative;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+`;
+
+const CloseButton = styled.div`
+  position: absolute;
+  display: block;
+  width: 40px;
+  height: 40px;
+  padding: 13px 0 12px 0;
+  z-index: 1;
+  text-align: center;
+  border-radius: 50%;
+  right: 15px;
+  top: 12px;
+  cursor: pointer;
+  :hover {
+    background-color: #ffffff11;
+  }
+`;
+
+const CloseButtonImg = styled.img`
+  width: 14px;
+  margin: 0 auto;
+`;
+
+const StyledCreateProjectModal= styled.div`
+  width: 100%;
+  position: absolute;
+  left: 0;
+  top: 0;
+  height: 100%;
+  padding: 25px 32px;
+  overflow: hidden;
+  border-radius: 6px;
+  background: #202227;
+`;
--- dashboard/src/main/home/templates/expanded-template/ExpandedTemplate.tsx
+++ dashboard/src/main/home/templates/expanded-template/ExpandedTemplate.tsx
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
-import launch from '../../../../assets/launch.svg';
-import Markdown from 'markdown-to-jsx';
-
-import { Context } from '../../../../shared/Context';
 
 import { PorterChart } from '../../../../shared/types';
+
+import TemplateInfo from './TemplateInfo';
+import LaunchTemplate from './LaunchTemplate';
 
 type PropsType = {
   currentTemplate: PorterChart,
@@ -13,200 +12,44 @@ type PropsType = {
 };
 
 type StateType = {
+  showLaunchTemplate: boolean
 };
 
 export default class ExpandedTemplate extends Component<PropsType, StateType> {
   state = {
+    showLaunchTemplate: false
   }
 
-  renderIcon = (icon: string) => {
-    if (icon) {
-      return <Icon src={icon} />
+  renderContents = () => {
+    if (this.state.showLaunchTemplate) {
+      return (
+        <LaunchTemplate
+          currentTemplate={this.props.currentTemplate}
+          hideLaunch={() => this.setState({ showLaunchTemplate: false })}
+        />
+      );
     }
 
     return (
-      <Polymer><i className="material-icons">layers</i></Polymer>
+      <TemplateInfo
+        currentTemplate={this.props.currentTemplate}
+        setCurrentTemplate={this.props.setCurrentTemplate}
+        launchTemplate={() => this.setState({ showLaunchTemplate: true })}
+      />
     );
   }
 
-  renderTagList = () => {
-    return this.props.currentTemplate.Form.Tags.map((tag: string, i: number) => {
-      return (
-        <Tag key={i}>{tag}</Tag>
-      )
-    });
-  }
-
-  renderMarkdown = () => {
-    let { currentTemplate } = this.props;
-    if (currentTemplate.Markdown) {
-      return (
-        <Markdown>{currentTemplate.Markdown}</Markdown>
-      );
-    } else if (currentTemplate.Form.Description) {
-      return currentTemplate.Form.Description;
-    }
-
-    return currentTemplate.Description;
-  }
-
   render() {
-    let { Name, Icon, Description } = this.props.currentTemplate.Form;
-    let { currentTemplate } = this.props;
-    let name = Name ? Name : currentTemplate.Name;
-
     return (
       <StyledExpandedTemplate>
-        <TitleSection>
-          <Flex>
-            <i className="material-icons" onClick={() => this.props.setCurrentTemplate(null)}>
-              keyboard_backspace
-            </i>
-            {Icon ? this.renderIcon(Icon) : this.renderIcon(currentTemplate.Icon)}
-            <Title>{name}</Title>
-          </Flex>
-          <Button onClick={() => this.context.setCurrentModal('LaunchTemplateModal', { template: currentTemplate })}>
-            <img src={launch} />
-            Launch Template
-          </Button>
-        </TitleSection>
-        <TagSection>
-          <i className="material-icons">local_offer</i>
-          {this.renderTagList()}
-        </TagSection>
-        <ContentSection>
-          {this.renderMarkdown()}
-        </ContentSection>
+        {this.renderContents()}
       </StyledExpandedTemplate>
     );
   }
 }
 
-ExpandedTemplate.contextType = Context;
-
-const ContentSection = styled.div`
-  margin-top: 50px;
-  font-size: 14px;
-  line-height: 1.8em;
-  padding-bottom: 100px;
-  overflow: hidden;
-`;
-
-const Tag = styled.div`
-  border: 1px solid #ffffff44;
-  border-radius: 3px;
-  display: flex;
-  margin-right: 7px;
-  align-items: center;
-  padding: 5px 10px;
-`;
-
-const TagSection = styled.div`
-  margin-top: 20px;
-  display: flex;
-  font-size: 13px;
-  font-family: 'Work Sans', sans-serif;
-  align-items: center;
-
-  > i {
-    font-size: 20px;
-    margin-right: 10px;
-    color: #aaaabb;
-  }
-`;
-
-const Flex = styled.div`
-  display: flex;
-  align-items: center;
-
-  > i {
-    cursor: pointer;
-    font-size 24px;
-    color: #969Fbbaa;
-    padding: 3px;
-    border-radius: 100px;
-    :hover {
-      background: #ffffff11;
-    }
-  }
-`;
-
-const Button = styled.div`
-  height: 100%;
-  background: #616FEEcc;
-  :hover {
-    background: #505edddd;
-  }
-  color: white;
-  font-weight: 500;
-  font-size: 13px;
-  padding: 10px 15px;
-  border-radius: 3px;
-  cursor: pointer;
-  box-shadow: 0 5px 8px 0px #00000010;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-
-  > img {
-    width: 20px;
-    height: 20px;
-    display: flex;
-    align-items: center;
-    margin-right: 10px;
-    justify-content: center;
-  }
-`;
-
-const Icon = styled.img`
-  width: 27px;
-  margin-left: 14px;
-  margin-right: 4px;
-  margin-bottom: -1px;
-`;
-
-
-const Polymer = styled.div`
-  margin-bottom: -3px;
-
-  > i {
-    color: ${props => props.theme.containerIcon};
-    font-size: 24px;
-    margin-left: 12px;
-    margin-right: 3px;
-  }
-`;
-
-const Description = styled.div`
-  font-size: 14px;
-  font-family: 'Work Sans', sans-serif;
-  margin-left: 30px;
-  width: calc(100% - 60px);
-  height: 4em;
-  border-radius: 2px;
-  color: #aaaabb;
-  padding: 5px 10px;
-`;
-
-const Title = styled.div`
-  font-size: 24px;
-  font-weight: 600;
-  font-family: 'Work Sans', sans-serif;
-  margin-left: 10px;
-  border-radius: 2px;
-  color: #ffffff;
-`;
-
-const TitleSection = styled.div`
-  display: flex;
-  margin-left: -42px;
-  flex-direction: row;
-  justify-content: space-between;
-  width: calc(100% + 42px);
-  align-items: center;
-`;
-
 const StyledExpandedTemplate = styled.div`
-  width: calc(90% - 70px);
-  padding-top: 20px;
+  width: calc(90% - 150px);
+  min-width: 300px;
+  padding-top: 30px;
 `;

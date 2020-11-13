956f7c28b84163f06285e71a61ded6e259284e65
Alexander Belanger <belanger@sas.upenn.edu>
2020-11-13 14:33:54 -0500

    resolve relative filepaths

---
+++ cli/cmd/utils/file.go
@@ -0,0 +1,33 @@
+package utils
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+func GetFileReferenceFromKubeconfig(
+	filename string,
+	kubeconfigPath string,
+) (string, error) {
+	if info, err := os.Stat(filename); os.IsNotExist(err) && !info.IsDir() {
+		// attempt to discover the filename relative to the kubeconfig location
+		absPath, err := filepath.Abs(kubeconfigPath)
+
+		if err != nil {
+			return "", err
+		}
+
+		fPath := filepath.Join(filepath.Dir(absPath), filename)
+
+		if info, err := os.Stat(fPath); !os.IsNotExist(err) && !info.IsDir() {
+			return fPath, nil
+		} else {
+			return "", fmt.Errorf("%s not found", filename)
+		}
+	} else if info.IsDir() {
+		return "", fmt.Errorf("%s is a directory", filename)
+	}
+
+	return filename, nil
+}

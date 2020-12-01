def1c228e5d36b087b9982b71c3ec454cdfb8a6c
sunguroku <65516095+sunguroku@users.noreply.github.com>
2020-12-01 11:00:13 -0500

    disable save unless image is specified

--- dashboard/src/main/home/cluster-dashboard/expanded-chart/SettingsSection.tsx
+++ dashboard/src/main/home/cluster-dashboard/expanded-chart/SettingsSection.tsx
@@ -79,6 +79,7 @@ export default class SettingsSection extends Component<PropsType, StateType> {
           onClick={() => this.redeployWithNewImage(this.state.selectedImageUrl, this.state.selectedTag)}
           status={this.state.saveValuesStatus}
           makeFlush={true}
+          disabled={this.state.selectedImageUrl && this.state.selectedTag ? false : true}
         />
       </Wrapper>
     );

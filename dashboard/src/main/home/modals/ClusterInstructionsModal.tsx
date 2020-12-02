5a51a9920dcd7af71d02a31b9a4b8813c829ae5b
sunguroku <65516095+sunguroku@users.noreply.github.com>
2020-12-02 09:20:11 -0500

    udpate installation command

--- dashboard/src/main/home/modals/ClusterInstructionsModal.tsx
+++ dashboard/src/main/home/modals/ClusterInstructionsModal.tsx
@@ -31,7 +31,7 @@ export default class ClusterInstructionsModal extends Component<PropsType, State
             1. To install the Porter CLI, first retrieve the latest binary:
             <Code>
               &#123;<br />
-              name=$(curl -s https://api.github.com/repos/porter-dev/porter/releases/latest | grep "browser_download_url.*_Darwin_x86_64\.zip" | cut -d ":" -f 2,3 | tr -d \")<br />
+              name=$(curl -s https://api.github.com/repos/porter-dev/porter/releases/latest | grep "browser_download_url.*porter_.*_Darwin_x86_64\.zip" | cut -d ":" -f 2,3 | tr -d \")<br />
               name=$(basename $name)<br />
               curl -L https://github.com/porter-dev/porter/releases/latest/download/$name --output $name<br />
               unzip -a $name<br />

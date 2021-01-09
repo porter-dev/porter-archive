781eec3c21259d0299b1d10f7e4b6eff0c76cb3e
jusrhee <jusrhee@sas.upenn.edu>
2021-01-09 16:12:38 -0500

    basic mr.p analytics

--- dashboard/src/shared/feedback.tsx
+++ dashboard/src/shared/feedback.tsx
@@ -1,19 +1,21 @@
 import axios from 'axios';
 
 export const handleSubmitFeedback = (msg: string, callback?: (err: any, res: any) => void) => {
-  axios.post(process.env.FEEDBACK_ENDPOINT, {
-    key: process.env.DISCORD_KEY,
-    cid: process.env.DISCORD_CID,
-    message: msg,
-  }, {
-    headers: {
-      Authorization: `Bearer <>`
-    }
-  })
-  .then(res => {
-    callback && callback(null, res);
-  })
-  .catch(err => {
-    callback && callback(err, null);
-  });
+  if (!window.location.href.includes('localhost:8080')) {
+    axios.post(process.env.FEEDBACK_ENDPOINT, {
+      key: process.env.DISCORD_KEY,
+      cid: process.env.DISCORD_CID,
+      message: msg,
+    }, {
+      headers: {
+        Authorization: `Bearer <>`
+      }
+    })
+    .then(res => {
+      callback && callback(null, res);
+    })
+    .catch(err => {
+      callback && callback(err, null);
+    });
+  }
 }

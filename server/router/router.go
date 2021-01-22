0c1959f8f4057e7b27d18e7245e0cb82ae02055d
sunguroku <65516095+sunguroku@users.noreply.github.com>
2021-01-21 21:53:47 -0500

    upgrade via webhook keeps original chart values. fixes #237

--- server/router/router.go
+++ server/router/router.go
@@ -842,12 +842,6 @@ func New(a *api.App) *chi.Mux {
 			),
 		)
 
-		// r.Method(
-		// 	"POST",
-		// 	"/projects/{project_id}/releases/{name}/upgrade/hook",
-		// 	requestlog.NewHandler(a.HandleReleaseDeployHook, l),
-		// )
-
 		r.Method(
 			"POST",
 			"/webhooks/deploy/{token}",

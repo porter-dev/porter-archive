637191daf9eadce83ca1dc705cdee5a612d67b2f
Sean Rhee <seanrhee2024@u.northwestern.edu>
2021-01-25 23:10:50 -0500

    project and cluster memory

--- dashboard/src/main/home/sidebar/ClusterSection.tsx
+++ dashboard/src/main/home/sidebar/ClusterSection.tsx
@@ -55,7 +55,22 @@ export default class ClusterSection extends Component<PropsType, StateType> {
           clusters.sort((a: any, b: any) => a.id - b.id);
           if (clusters.length > 0) {
             this.setState({ clusters });
-            setCurrentCluster(clusters[0]);
+            let saved = JSON.parse(localStorage.getItem(currentProject.id + '-cluster'));
+            if (saved !== 'null') {
+              setCurrentCluster(clusters[0]);
+              for (let i = 0; i < clusters.length; i++) {
+                if (
+                  clusters[i].id === saved.id &&
+                  clusters[i].project_id === saved.project_id && 
+                  clusters[i].name === saved.name
+                ) {
+                  setCurrentCluster(clusters[i]);
+                  break;
+                }
+              }
+            } else {
+              setCurrentCluster(clusters[0]);
+            }
           } else if (
             this.props.currentView !== 'provisioner'
             && this.props.currentView !== 'new-project'

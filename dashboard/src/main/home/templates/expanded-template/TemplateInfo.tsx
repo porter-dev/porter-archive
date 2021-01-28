42768aadfc65aee8677ddd3151a98719f2936f16
jusrhee <jusrhee@sas.upenn.edu>
2021-01-27 20:01:59 -0500

    misc changes

--- dashboard/src/main/home/templates/expanded-template/TemplateInfo.tsx
+++ dashboard/src/main/home/templates/expanded-template/TemplateInfo.tsx
@@ -99,6 +99,22 @@ export default class TemplateInfo extends Component<PropsType, StateType> {
           </Banner>
         </>
       );
+    } else if (this.props.currentTemplate.name.toLowerCase() === 'https-issuer') {
+      return (
+        <>
+          <Br />
+          <Banner>
+            <i className="material-icons-outlined">info</i>
+            To use this template you must first follow
+            <Link 
+              target="_blank"
+              href="https://docs.getporter.dev/docs/https-and-custom-domains"
+            >
+              Porter's HTTPS setup guide
+            </Link> (5 minutes).
+          </Banner>
+        </>
+      );
     }
   }
 
@@ -145,6 +161,7 @@ TemplateInfo.contextType = Context;
 
 const Link = styled.a`
   color: #8590ff;
+  margin-right: 5px;
   cursor: pointer;
   margin-left: 5px;
 `;

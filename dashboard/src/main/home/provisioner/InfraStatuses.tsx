51c998a2b7e796ce0635f7a8b00e92696f0619c6
sunguroku <65516095+sunguroku@users.noreply.github.com>
2021-02-04 19:31:36 -0500

    provisioning integrated, unstyled

--- dashboard/src/main/home/provisioner/InfraStatuses.tsx
+++ dashboard/src/main/home/provisioner/InfraStatuses.tsx
@@ -8,6 +8,7 @@ import { infraNames } from 'shared/common';
 type PropsType = {
   infras: InfraType[],
   selectInfra: (infra: InfraType) => void,
+  selectedInfra: InfraType,
 };
 
 type StateType = {
@@ -20,9 +21,9 @@ export default class InfraStatuses extends Component<PropsType, StateType> {
   renderStatusIcon = (status: string) => {
     if (status === 'created') {
       return <StatusIcon>✓</StatusIcon>;
-    } else if (status === 'creating') {
+    } else if (status === 'creating' || status === 'destroying') {
       return <StatusIcon><img src={loadingDots} /></StatusIcon>
-    } else if (status === 'error') {
+    } else if (status === 'error' || status === 'destroyed') {
       return <StatusIcon color='#e3366d'>✗</StatusIcon>
     }
   }
@@ -34,10 +35,11 @@ export default class InfraStatuses extends Component<PropsType, StateType> {
           return (
             <InfraRow 
               key={infra.id}
+              selected={(infra.id === this.props.selectedInfra?.id)}
               onClick={() => this.props.selectInfra(infra)}
             >
-              {this.renderStatusIcon(infra.status)}
               {infraNames[infra.kind]}
+              {this.renderStatusIcon(infra.status)}
             </InfraRow>
           )
         })}
@@ -53,21 +55,41 @@ const StatusIcon = styled.div<{ color?: string }>`
   width: 20px;
   font-size: 16px;
   color: ${props => props.color ? props.color : '#68c49c'};
-  margin-right: 10px;
+  margin-left: 10px;
 `;
 
-const InfraRow = styled.div`
+const Tab = styled.div`
   width: 100%;
   height: 25px;
   padding-left: 2px;
   margin-top: 10px;
   font-size: 13px;
   color: #aaaabb;
+  color: white;
   display: flex;
   align-items: center;
 `;
 
+const InfraRow = styled.div`
+  width: 100%;
+  height: 50px;
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  color: ${(props: {selected: boolean}) => props.selected ? 'white' : '#ffffff66'};
+  background: ${(props: {selected: boolean}) => props.selected ? '#ffffff18' : ''};
+  font-size: 13px;
+  padding: 20px 19px 20px 42px;
+  text-shadow: 0px 0px 8px none;
+  overflow: visible;
+  cursor: pointer;
+  :hover {
+    color: white;
+    background: #ffffff18;
+  }
+`;
+
 const StyledInfraStatuses = styled.div`
-  margin-top: 20px;
   margin-bottom: 0;
 `;

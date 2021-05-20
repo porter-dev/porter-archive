7236aab7f765d7e877405e89e6969bcb67da3da2
Alexander Belanger <belanger@sas.upenn.edu>
2021-05-20 17:44:01 -0400

    on-prem fixes + g suite

--- server/api/capability_handler.go
+++ server/api/capability_handler.go
@@ -5,23 +5,9 @@ import (
 	"net/http"
 )
 
-// CapabilitiesExternal represents the Capabilities struct that will be sent over REST
-type CapabilitiesExternal struct {
-	Provisioner bool `json:"provisioner"`
-	GitHub bool	`json:"github"`
-}
-
 // HandleGetCapabilities gets the capabilities of the server
 func (app *App) HandleGetCapabilities(w http.ResponseWriter, r *http.Request) {
-
-	cap := app.CapConf
-
-	capExternal := &CapabilitiesExternal{
-		Provisioner: cap.Provisioner,
-		GitHub: cap.Github,
-	}
-
-	if err := json.NewEncoder(w).Encode(capExternal); err != nil {
+	if err := json.NewEncoder(w).Encode(app.Capabilities); err != nil {
 		app.handleErrorFormDecoding(err, ErrK8sDecode, w)
 		return
 	}

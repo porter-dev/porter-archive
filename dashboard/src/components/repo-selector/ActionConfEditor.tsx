eb1e5542bb8f6d2bc8fb5ae843728d67a255936f
Ivan Galakhov <igalakhov.nyc@gmail.com>
2021-06-17 16:19:28 -0400

    remove debug code

--- dashboard/src/components/repo-selector/ActionConfEditor.tsx
+++ dashboard/src/components/repo-selector/ActionConfEditor.tsx
@@ -52,9 +52,6 @@ const ActionConfEditor: React.FC<Props> = (props) => {
 
   const { actionConfig, setBranch, setActionConfig, branch } = props;
 
-  actionConfig.git_repo = "igalakhov/flask-porter-test";
-  actionConfig.git_repo_id = 4;
-
   useEffect(() => {
     if (
       !actionConfig.git_repo ||

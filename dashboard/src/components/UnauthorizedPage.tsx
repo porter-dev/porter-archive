b3982f2a78ae5b4bcb78e678f87e2535930822c5
jnfrati <nicofrati@gmail.com>
2021-07-09 18:08:00 -0300

    Cleaned up UnauthorizedPage

--- dashboard/src/components/UnauthorizedPage.tsx
+++ dashboard/src/components/UnauthorizedPage.tsx
@@ -1,72 +1,16 @@
-import React, { Component } from "react";
+import React from "react";
 import styled from "styled-components";
 
-type PropsType = {};
-
-type StateType = {};
-
-class UnauthorizedPage extends Component<PropsType, StateType> {
-  state = {};
-
-  render() {
-    return (
-      <StyledUnauthorizedPage>
-        <Mega>
-          401
-          <Inside>You're not authorized to access this page</Inside>
-        </Mega>
-      </StyledUnauthorizedPage>
-    );
-  }
-}
+const UnauthorizedPage: React.FunctionComponent = () => (
+  <StyledUnauthorizedPage>
+    <Mega>
+      401
+      <Inside>You're not authorized to access this page</Inside>
+    </Mega>
+  </StyledUnauthorizedPage>
+);
 
 export default UnauthorizedPage;
-
-const Splitter = styled.div`
-  margin: 0 20px;
-  font-size: 27px;
-  font-weight: 200;
-  color: #ffffff15;
-`;
-
-const Flex = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`;
-
-const Helper = styled.div`
-  font-size: 15px;
-  max-width: 550px;
-  margin-right: -50px;
-`;
-
-const BackButton = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  cursor: pointer;
-  font-size: 13px;
-  height: 35px;
-  padding: 5px 16px;
-  padding-right: 15px;
-  border: 1px solid #ffffff55;
-  border-radius: 100px;
-  width: ${(props: { width: string }) => props.width};
-  color: white;
-  background: #ffffff11;
-
-  :hover {
-    background: #ffffff22;
-  }
-
-  > i {
-    color: white;
-    font-size: 16px;
-    margin-right: 6px;
-    margin-left: -2px;
-  }
-`;
 
 const StyledUnauthorizedPage = styled.div`
   font-family: "Work Sans", sans-serif;

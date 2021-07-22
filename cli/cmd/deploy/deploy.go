8e7a5912e4dbb66ca75d9466a4e8efe542aff7a8
Alexander Belanger <belanger@sas.upenn.edu>
2021-07-22 19:12:50 -0400

    re-add image pull

--- cli/cmd/deploy/deploy.go
+++ cli/cmd/deploy/deploy.go
@@ -381,9 +381,7 @@ func (d *DeployAgent) pullCurrentReleaseImage() error {
 
 	fmt.Printf("attempting to pull image: %s\n", fmt.Sprintf("%s:%s", d.imageRepo, tagStr))
 
-	// return d.agent.PullImage(fmt.Sprintf("%s:%s", d.imageRepo, tagStr))
-
-	return nil
+	return d.agent.PullImage(fmt.Sprintf("%s:%s", d.imageRepo, tagStr))
 }
 
 func (d *DeployAgent) downloadRepoToDir(downloadURL string) (string, error) {

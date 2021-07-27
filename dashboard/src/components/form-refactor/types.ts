ab13430a5872429115e50d5f1471fb23b7512fe5
Ivan Galakhov <igalakhov.nyc@gmail.com>
2021-07-27 13:05:22 -0400

    small change to validation

--- dashboard/src/components/form-refactor/types.ts
+++ dashboard/src/components/form-refactor/types.ts
@@ -13,6 +13,7 @@ export interface GenericInputField extends GenericField {
   isReadOnly?: boolean;
   required?: boolean;
   variable: string;
+  settings: any
 }
 
 export interface HeadingField extends GenericField {

a83c0efd2ac05cea4210695135a35cb1e10bf5be
jnfrati <nicofrati@gmail.com>
2021-08-03 17:05:31 -0300

    Added function to check if the linux environment is under wsl

--- cli/cmd/utils/browser.go
+++ cli/cmd/utils/browser.go
@@ -2,8 +2,21 @@ package utils
 
 import (
 	"os/exec"
+	"regexp"
 	"runtime"
 )
+
+func checkIfWsl() bool {
+	out, err := exec.Command("uname", "-a").Output()
+	if err != nil {
+		return false
+	}
+	// On some cases, uname on wsl outputs microsoft capitalized
+	if matched, err := regexp.Match(`microsoft|Microsoft`, out); err != nil {
+		return matched
+	}
+	return false
+}
 
 // OpenBrowser opens the specified URL in the default browser of the user.
 func OpenBrowser(url string) error {
@@ -17,7 +30,11 @@ func OpenBrowser(url string) error {
 	case "darwin":
 		cmd = "open"
 	default: // "linux", "freebsd", "openbsd", "netbsd"
-		cmd = "xdg-open"
+		if checkIfWsl() {
+			cmd = "explorer.exe"
+		} else {
+			cmd = "xdg-open"
+		}
 	}
 	args = append(args, url)
 	return exec.Command(cmd, args...).Start()

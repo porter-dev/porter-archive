f36f31a7d27303458dbc7b0fa3985a5b8e90a0d8
jnfrati <nicofrati@gmail.com>
2021-08-11 12:43:26 -0300

    Final touches for global error boundary

--- dashboard/src/App.tsx
+++ dashboard/src/App.tsx
@@ -10,7 +10,7 @@ export default class App extends Component {
     return (
       <StyledMain>
         <GlobalStyle />
-        <PorterErrorBoundary>
+        <PorterErrorBoundary errorBoundaryLocation="globalErrorBoundary">
           <BrowserRouter>
             <MainWrapper />
           </BrowserRouter>

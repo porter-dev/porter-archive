8565538f8f2df612aef08f02c5515e4e865e5bb4
Alexander Belanger <belanger@sas.upenn.edu>
2021-08-13 10:09:40 -0400

    check for token before session

--- server/api/user_handler.go
+++ server/api/user_handler.go
@@ -838,17 +838,17 @@ func (app *App) sendUser(w http.ResponseWriter, userID uint, email string, email
 }
 
 func (app *App) getUserIDFromRequest(r *http.Request) (uint, error) {
-	session, err := app.Store.Get(r, app.ServerConf.CookieName)
-
-	if err != nil {
-		return 0, err
-	}
-
 	// first, check for token
 	tok := app.getTokenFromRequest(r)
 
 	if tok != nil {
 		return tok.IBy, nil
+	}
+
+	session, err := app.Store.Get(r, app.ServerConf.CookieName)
+
+	if err != nil {
+		return 0, err
 	}
 
 	userID, _ := session.Values["user_id"].(uint)

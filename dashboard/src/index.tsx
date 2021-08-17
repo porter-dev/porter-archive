fbcff112eb02ef369db2e7978b3e72dbbd413331
jnfrati <nicofrati@gmail.com>
2021-08-17 19:35:20 -0300

    Moved regenerator runtime to top import

--- dashboard/src/index.tsx
+++ dashboard/src/index.tsx
@@ -1,9 +1,9 @@
+import "core-js/stable";
+import "regenerator-runtime/runtime";
+
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import App from "./App";
-
-import "core-js/stable";
-import "regenerator-runtime/runtime";
 
 declare global {
   interface Window {

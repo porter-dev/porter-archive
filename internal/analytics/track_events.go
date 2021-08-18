361d4d4e874d39bdf2ccec15b0dbb6d9840cdeb5
Alexander Belanger <belanger@sas.upenn.edu>
2021-08-18 09:57:27 -0400

    temp analytics overhaul

--- internal/analytics/track_events.go
+++ internal/analytics/track_events.go
@@ -3,7 +3,30 @@ package analytics
 type SegmentEvent string
 
 const (
-	NewUser            SegmentEvent = "New User"
-	RedeployViaWebhook SegmentEvent = "Triggered Re-deploy via Webhook"
-	NewClusterEvent    SegmentEvent = "New Cluster Event"
+	// onboarding flow
+	UserCreate    SegmentEvent = "New User"
+	ProjectCreate SegmentEvent = "New Project Event"
+
+	ClusterProvisioningStart   SegmentEvent = "Cluster Provisioning Started"
+	ClusterProvisioningError   SegmentEvent = "Cluster Provisioning Error"
+	ClusterProvisioningSuccess SegmentEvent = "Cluster Provisioning Success"
+
+	RegistryProvisioningStart   SegmentEvent = "Registry Provisioning Started"
+	RegistryProvisioningError   SegmentEvent = "Registry Provisioning Error"
+	RegistryProvisioningSuccess SegmentEvent = "Registry Provisioning Success"
+
+	ClusterConnectedStart   SegmentEvent = "Cluster Connection Started"
+	ClusterConnectedError   SegmentEvent = "Cluster Connection Started"
+	ClusterConnectedSuccess SegmentEvent = "Cluster Connection Error"
+
+	RegistryConnectedSuccess SegmentEvent = "Registry Connection Success"
+	RegistryConnectedError   SegmentEvent = "Registry Connection Error"
+
+	GithubConnectedSuccess SegmentEvent = "Github Connection Success"
+	GithubConnectedError   SegmentEvent = "Github Connection Error"
+
+	// launch flow
+	ApplicationLaunch            SegmentEvent = "New Application Launched"
+	ApplicationLaunchError       SegmentEvent = "Application Deployment Error"
+	ApplicationDeploymentWebhook SegmentEvent = "Triggered Re-deploy via Webhook"
 )
--- server/api/release_handler.go
+++ server/api/release_handler.go
@@ -1059,8 +1059,7 @@ func (app *App) HandleUpgradeRelease(w http.ResponseWriter, r *http.Request) {
 		notifyOpts.Status = slack.StatusFailed
 		notifyOpts.Info = upgradeErr.Error()
 
-		slackErr := notifier.Notify(notifyOpts)
-		fmt.Println("SLACK ERROR IS", slackErr)
+		notifier.Notify(notifyOpts)
 
 		app.sendExternalError(err, http.StatusInternalServerError, HTTPError{
 			Code:   ErrReleaseDeploy,
@@ -1316,7 +1315,18 @@ func (app *App) HandleReleaseDeployWebhook(w http.ResponseWriter, r *http.Reques
 
 	notifier.Notify(notifyOpts)
 
-	app.analyticsClient.Track(analytics.CreateSegmentRedeployViaWebhookTrack("anonymous", repository.(string)))
+	userID, _ := app.getUserIDFromRequest(r)
+
+	app.analyticsClient.Track(analytics.ApplicationDeploymentWebhookTrack(&analytics.ApplicationDeploymentWebhookTrackOpts{
+		ImageURI: fmt.Sprintf("%v", repository),
+		ApplicationScopedTrackOpts: analytics.GetApplicationScopedTrackOpts(
+			userID,
+			release.ProjectID,
+			release.ClusterID,
+			release.Name,
+			release.Namespace,
+		),
+	}))
 
 	w.WriteHeader(http.StatusOK)
 }

642402d024a64667dc7c63ff98e9471083a29000
Alexander Belanger <belanger@sas.upenn.edu>
2021-08-19 13:39:18 -0400

    some temp notes

---
+++ api/server/handlers/handler.go
@@ -0,0 +1,60 @@
+package handlers
+
+type PorterHandler interface{
+	Config() *shared.Config
+}
+
+type PorterHandlerWriter interface{
+	PorterHandler
+	WriteResult(w http.ResponseWriter, v interface{})
+}
+
+type PorterHandlerReader interface{
+	PorterHandler
+	DecodeAndValidate(w http.ResponseWriter, r *http.Request, v interface{})
+}
+
+type PorterHandlerReadWriter interface{
+	PorterHandlerWriter
+	PorterHandlerReader
+}
+
+// type PorterHandler struct {
+// 	config           *shared.Config
+// 	decoderValidator shared.RequestDecoderValidator
+// 	writer           shared.ResultWriter
+// }
+
+// handler needs:
+// - interface for decodervalidator+writer
+// - shared configuration
+// - writer
+// - context set (user, project, etc)
+// - standard error
+
+
+// notes:
+// decode and validate should happen above the handler itself. the scopes and strongly typed 
+
+// "handlers" refer to an aggregation of application-logic. they should not contain any logic
+// for:
+// - error aggregation (so they accept api error interface)
+// - analytics
+// - authentication
+// - reading in required context (part of authentication)
+
+// ProjectScopedHandler()
+// - read project model from context 
+// - so "Get Project" accepts a ProjectGetter, which calls readUser() and readProject()
+
+// The errors that a handler can throw should be defined in API spec
+
+type UserGetter struct {
+	readUser() *models.User
+}
+
+type ProjectGetter struct {
+	UserGetter
+
+	readProject() *models.Project
+}

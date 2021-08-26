4e4b4e5ba0e67e00304a861a8144ff62f39cc0d1
Anukul Sangwan <anukulsangwan@icloud.com>
2021-08-27 00:13:18 +0530

    add ProjectGetPolicyHandler

---
+++ api/server/handlers/project/get_policy.go
@@ -0,0 +1,42 @@
+package project
+
+import (
+	"github.com/porter-dev/porter/api/server/authz/policy"
+	"github.com/porter-dev/porter/api/server/shared/apierrors"
+	"net/http"
+
+	"github.com/porter-dev/porter/api/server/handlers"
+	"github.com/porter-dev/porter/api/server/shared"
+	"github.com/porter-dev/porter/api/types"
+	"github.com/porter-dev/porter/internal/models"
+)
+
+type ProjectGetPolicyHandler struct {
+	handlers.PorterHandlerWriter
+}
+
+func NewProjectGetPolicyHandler(
+	config *shared.Config,
+	writer shared.ResultWriter,
+) *ProjectGetPolicyHandler {
+	return &ProjectGetPolicyHandler{
+		PorterHandlerWriter: handlers.NewDefaultPorterHandler(config, nil, writer),
+	}
+}
+
+func (p *ProjectGetPolicyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	user, _ := r.Context().Value(types.UserScope).(*models.User)
+	proj, _ := r.Context().Value(types.ProjectScope).(*models.Project)
+
+	policyDocLoader := policy.NewBasicPolicyDocumentLoader(p.Config().Repo.Project())
+
+	policyDocs, err := policyDocLoader.LoadPolicyDocuments(user.ID, proj.ID)
+
+	if err != nil {
+		p.HandleAPIError(w, apierrors.NewErrInternal(err))
+	}
+
+	var res types.GetProjectPolicyResponse = policyDocs
+
+	p.WriteResult(w, res)
+}

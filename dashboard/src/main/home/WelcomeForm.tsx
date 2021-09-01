69ad3020c0a53802ca078a128f6b40e22ab27e92
jusrhee <jusrhee@sas.upenn.edu>
2021-08-31 20:00:38 -0400

    allow logout from welcome form

--- dashboard/src/main/home/WelcomeForm.tsx
+++ dashboard/src/main/home/WelcomeForm.tsx
@@ -291,7 +291,7 @@ const StyledWelcomeForm = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  z-index: 999;
+  z-index: 1;
   background: #202227;
 
   &.alert-exit {

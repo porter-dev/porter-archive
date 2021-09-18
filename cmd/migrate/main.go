5cd1bce6515305e90b183ced9f7f04ad5d5c26ed
Alexander Belanger <belanger@sas.upenn.edu>
2021-09-18 13:01:38 -0400

    consolidate entrypoint logging

--- cmd/migrate/main.go
+++ cmd/migrate/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 
 	"github.com/porter-dev/porter/api/server/shared/config/loader"
@@ -16,35 +15,35 @@ import (
 
 func main() {
 	logger := lr.NewConsole(true)
-	fmt.Println("running migrations...")
+	logger.Info().Msg("running migrations")
 
 	envConf, err := loader.FromEnv()
 
 	if err != nil {
-		logger.Fatal().Err(err).Msg("")
+		logger.Fatal().Err(err).Msg("could not load env conf")
 		return
 	}
 
 	db, err := adapter.New(envConf.DBConf)
 
 	if err != nil {
-		logger.Fatal().Err(err).Msg("")
+		logger.Fatal().Err(err).Msg("could not connect to the database")
 		return
 	}
 
 	err = gorm.AutoMigrate(db)
 
 	if err != nil {
-		logger.Fatal().Err(err).Msg("")
+		logger.Fatal().Err(err).Msg("gorm auto-migration failed")
 		return
 	}
 
 	if err := db.Raw("ALTER TABLE clusters DROP CONSTRAINT IF EXISTS fk_cluster_token_caches").Error; err != nil {
-		logger.Fatal().Err(err).Msg("")
+		logger.Fatal().Err(err).Msg("failed to drop cluster token cache constraint")
 		return
 	}
 	if err := db.Raw("ALTER TABLE cluster_token_caches DROP CONSTRAINT IF EXISTS fk_clusters_token_cache").Error; err != nil {
-		logger.Fatal().Err(err).Msg("")
+		logger.Fatal().Err(err).Msg("failed to drop clusters token cache constraint")
 		return
 	}
 
@@ -58,7 +57,7 @@ func main() {
 		err := keyrotate.Rotate(db, &oldKey, &newKey)
 
 		if err != nil {
-			panic(err)
+			logger.Fatal().Err(err).Msg("key rotation failed")
 		}
 	}
 }

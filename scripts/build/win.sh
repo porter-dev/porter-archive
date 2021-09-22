a33590047bf4f8afca08348788882044c44a6221
Alexander Belanger <belanger@sas.upenn.edu>
2021-09-22 12:53:46 -0400

    remove windows porter server and cred helper

--- scripts/build/win.sh
+++ scripts/build/win.sh
@@ -2,12 +2,7 @@
 #
 # Accepts the version as an argument
 
-go build -ldflags="-w -s -X 'github.com/porter-dev/porter/cli/cmd.Version=$1'" -a -tags cli -o ./porter.exe ./cli &
-go build -ldflags="-w -s -X 'main.Version=$1'" -a -o ./docker-credential-porter.exe ./cmd/docker-credential-porter/ &
-go build -ldflags="-w -s -X 'main.Version=$1'" -a -o ./portersvr.exe ./cmd/app/ &
-wait
+go build -ldflags="-w -s -X 'github.com/porter-dev/porter/cli/cmd.Version=$1'" -a -tags cli -o ./porter.exe ./cli
 
 mkdir -p /release/windows
 zip --junk-paths /release/windows/porter_$1_Windows_x86_64.zip ./porter.exe
-zip --junk-paths /release/windows/portersvr_$1_Windows_x86_64.zip ./portersvr.exe
-zip --junk-paths /release/windows/docker-credential-porter_$1_Windows_x86_64.zip ./docker-credential-porter.exe

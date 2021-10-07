a1e4e495b928abcbbde3efa9028e88625588a319
jnfrati <nicofrati@gmail.com>
2021-10-07 20:48:26 -0300

    Initialized forms

---
+++ dashboard/src/main/home/onboarding/ConnectRegistry/forms/_GCPRegistryForm.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const CredentialsForm = () => {
+  return <></>;
+};
+
+export const SettingsForm = () => {
+  return <></>;
+};
+
+export const TestRegistryConnection = () => {
+  return <></>;
+};
--- dashboard/src/main/home/onboarding/Routes.tsx
+++ dashboard/src/main/home/onboarding/Routes.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Route, Switch } from "react-router";
+import ConnectRegistry from "./ConnectRegistry/ConnectRegistry";
 import ConnectSource from "./ConnectSource";
 import { NewProjectFC } from "./NewProject";
 import ProvisionerForms from "./ProvisionerForms";
@@ -13,6 +14,9 @@ export const Routes = () => {
         </Route>
         <Route path={`/onboarding/integrations`}>
           <ConnectSource />
+        </Route>
+        <Route path={`/onboarding/registry`}>
+          <ConnectRegistry />
         </Route>
         <Route path={`/onboarding/provision`}>
           <ProvisionerForms />

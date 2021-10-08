32cb7388b6410a4c24807803b54a9bd7d008a52d
jnfrati <nicofrati@gmail.com>
2021-10-08 20:41:35 -0300

    Made basic form flow work

--- dashboard/src/main/home/onboarding/ConnectRegistry/forms/_AWSRegistryForm.tsx
+++ dashboard/src/main/home/onboarding/ConnectRegistry/forms/_AWSRegistryForm.tsx
@@ -1,13 +1,184 @@
-import React from "react";
+import InputRow from "components/form-components/InputRow";
+import SelectRow from "components/form-components/SelectRow";
+import SaveButton from "components/SaveButton";
+import React, { useState } from "react";
+import api from "shared/api";
 
-export const CredentialsForm = () => {
-  return <></>;
+const regionOptions = [
+  { value: "us-east-1", label: "US East (N. Virginia) us-east-1" },
+  { value: "us-east-2", label: "US East (Ohio) us-east-2" },
+  { value: "us-west-1", label: "US West (N. California) us-west-1" },
+  { value: "us-west-2", label: "US West (Oregon) us-west-2" },
+  { value: "af-south-1", label: "Africa (Cape Town) af-south-1" },
+  { value: "ap-east-1", label: "Asia Pacific (Hong Kong) ap-east-1" },
+  { value: "ap-south-1", label: "Asia Pacific (Mumbai) ap-south-1" },
+  { value: "ap-northeast-2", label: "Asia Pacific (Seoul) ap-northeast-2" },
+  { value: "ap-southeast-1", label: "Asia Pacific (Singapore) ap-southeast-1" },
+  { value: "ap-southeast-2", label: "Asia Pacific (Sydney) ap-southeast-2" },
+  { value: "ap-northeast-1", label: "Asia Pacific (Tokyo) ap-northeast-1" },
+  { value: "ca-central-1", label: "Canada (Central) ca-central-1" },
+  { value: "eu-central-1", label: "Europe (Frankfurt) eu-central-1" },
+  { value: "eu-west-1", label: "Europe (Ireland) eu-west-1" },
+  { value: "eu-west-2", label: "Europe (London) eu-west-2" },
+  { value: "eu-south-1", label: "Europe (Milan) eu-south-1" },
+  { value: "eu-west-3", label: "Europe (Paris) eu-west-3" },
+  { value: "eu-north-1", label: "Europe (Stockholm) eu-north-1" },
+  { value: "me-south-1", label: "Middle East (Bahrain) me-south-1" },
+  { value: "sa-east-1", label: "South America (São Paulo) sa-east-1" },
+];
+
+export const CredentialsForm: React.FC<{ nextFormStep: () => void }> = ({
+  nextFormStep,
+}) => {
+  const [accessId, setAccessId] = useState("");
+  const [secretKey, setSecretKey] = useState("");
+  const [buttonStatus, setButtonStatus] = useState("");
+
+  const validate = () => {
+    if (!accessId) {
+      return {
+        hasError: true,
+        error: "Access ID cannot be empty",
+      };
+    }
+    if (!secretKey) {
+      return {
+        hasError: true,
+        error: "AWS Secret key cannot be empty",
+      };
+    }
+    return {
+      hasError: false,
+      error: "",
+    };
+  };
+
+  const submit = async () => {
+    const validation = validate();
+    if (validation.hasError) {
+      setButtonStatus(validation.error);
+      return;
+    }
+    // TODO: Ask alex how to request the aws_integration_id on this step
+    nextFormStep();
+  };
+
+  return (
+    <>
+      <InputRow
+        type="text"
+        value={accessId}
+        setValue={(x: string) => {
+          setAccessId(x);
+        }}
+        label="👤 AWS Access ID"
+        placeholder="ex: AKIAIOSFODNN7EXAMPLE"
+        width="100%"
+        isRequired={true}
+      />
+      <InputRow
+        type="password"
+        value={secretKey}
+        setValue={(x: string) => {
+          setSecretKey(x);
+        }}
+        label="🔒 AWS Secret Key"
+        placeholder="○ ○ ○ ○ ○ ○ ○ ○ ○"
+        width="100%"
+        isRequired={true}
+      />
+      <SaveButton
+        text="Continue"
+        disabled={false}
+        onClick={submit}
+        makeFlush={true}
+        clearPosition={true}
+        status={buttonStatus}
+        statusPosition={"right"}
+      />
+    </>
+  );
 };
 
-export const SettingsForm = () => {
-  return <></>;
+export const SettingsForm: React.FC<{ nextFormStep: () => void }> = ({
+  nextFormStep,
+}) => {
+  const [registryName, setRegistryName] = useState("");
+  const [awsRegion, setAWSRegion] = useState("us-east-1");
+  const [buttonStatus, setButtonStatus] = useState("");
+
+  const validate = () => {
+    if (!registryName) {
+      return {
+        hasError: true,
+        error: "Registry name cannot be empty",
+      };
+    }
+    return {
+      hasError: false,
+      error: "",
+    };
+  };
+
+  const submit = async () => {
+    const validation = validate();
+    if (validation.hasError) {
+      setButtonStatus(validation.error);
+      return;
+    }
+
+    nextFormStep();
+  };
+
+  return (
+    <>
+      <InputRow
+        type="text"
+        value={registryName}
+        setValue={(x) => {
+          setRegistryName(String(x));
+        }}
+        label="🏷️ Registry Name"
+        placeholder="ex: porter-awesome-registry"
+        width="100%"
+      />
+      <SelectRow
+        options={regionOptions}
+        width="100%"
+        value={awsRegion}
+        dropdownMaxHeight="240px"
+        setActiveValue={(x: string) => {
+          setAWSRegion(x);
+        }}
+        label="📍 AWS Region"
+      />
+      <SaveButton
+        text="Connect Registry"
+        disabled={false}
+        onClick={submit}
+        makeFlush={true}
+        clearPosition={true}
+        status={buttonStatus}
+        statusPosition={"right"}
+      />
+    </>
+  );
 };
 
-export const TestRegistryConnection = () => {
-  return <></>;
+export const TestRegistryConnection: React.FC<{ nextFormStep: () => void }> = ({
+  nextFormStep,
+}) => {
+  return (
+    <>
+      <SaveButton
+        text="Continue"
+        disabled={false}
+        onClick={nextFormStep}
+        makeFlush={true}
+        clearPosition={true}
+        status={""}
+        statusPosition={"right"}
+      />
+    </>
+  );
 };
--- dashboard/src/main/home/onboarding/OnboardingState.ts
+++ dashboard/src/main/home/onboarding/OnboardingState.ts
@@ -1,7 +1,8 @@
-import { isAlphanumeric } from "shared/common";
 import { ContextProps } from "shared/types";
 import { proxy } from "valtio";
-import { derive, devtools } from "valtio/utils";
+import { devtools } from "valtio/utils";
+import { SupportedProviders } from "./components/ProviderSelector";
+import { State as ConnectRegistryState } from "./ConnectRegistry/ConnectRegistryState";
 
 export type OnboardingStateType = {
   [key: string]: unknown;
@@ -11,20 +12,12 @@ export type OnboardingStateType = {
   userId: number | null;
   // Check if it's the first project that will be created
   isFirstProject: boolean | null;
+
+  selectedProvider: SupportedProviders | null;
+  actions: typeof actions;
 };
 
-const initialState: OnboardingStateType = {
-  projectName: "",
-  isProvisionerEnabled: null,
-  userId: null,
-  isFirstProject: null,
-};
-
-export const OnboardingState = proxy<OnboardingStateType>(initialState);
-
-devtools(OnboardingState, "Onboarding state");
-
-export const actions = {
+const actions = {
   setProjectName: (name: string) => {
     OnboardingState.projectName = name;
   },
@@ -58,11 +51,37 @@ export const actions = {
       actions.setIsFirstProject(true);
     }
   },
+  // Clear own and substates
   clearState: () => {
     Object.keys(OnboardingState).forEach((key) => {
-      if (key in initialState) {
+      if (key in initialState && key !== "actions") {
+        if (
+          key.toLowerCase().includes("state") &&
+          typeof OnboardingState === "object"
+        ) {
+          const subState = OnboardingState[key] as any;
+          if (
+            "clearState" in subState.actions &&
+            typeof subState?.actions?.clearState === "function"
+          ) {
+            subState.actions.clearState();
+          }
+        }
         OnboardingState[key] = initialState[key];
       }
     });
   },
 };
+
+const initialState: OnboardingStateType = {
+  projectName: "",
+  isProvisionerEnabled: null,
+  userId: null,
+  isFirstProject: null,
+  selectedProvider: null,
+  actions,
+  connectRegistry_state: ConnectRegistryState,
+};
+
+export const OnboardingState = proxy<OnboardingStateType>(initialState);
+devtools(OnboardingState, "Onboarding state");

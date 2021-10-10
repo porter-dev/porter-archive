89ee0908b4f95ede15bd879f1456654168b6ce43
jnfrati <nicofrati@gmail.com>
2021-10-10 14:40:23 -0300

    Added step handler

--- dashboard/src/main/home/onboarding/Onboarding.tsx
+++ dashboard/src/main/home/onboarding/Onboarding.tsx
@@ -1,21 +1,33 @@
 import React, { useContext, useEffect } from "react";
+import { useLocation } from "react-router";
 import { Context } from "shared/Context";
+import { useRouting } from "shared/routing";
 import styled from "styled-components";
 import { OnboardingState } from "./OnboardingState";
 import Routes from "./Routes";
+import StepHandler from "./StepHandler";
 
 const Onboarding = () => {
   const context = useContext(Context);
+  const location = useLocation();
+  const { pushFiltered } = useRouting();
 
   useEffect(() => {
     OnboardingState.actions.initFromGlobalContext(context);
   }, [context]);
 
   useEffect(() => {
+    OnboardingState.actions.restoreState();
     return () => {
       OnboardingState.actions.clearState();
     };
   }, []);
+
+  useEffect(() => {
+    if (StepHandler.currentStep.url !== location.pathname) {
+      pushFiltered(StepHandler.currentStep.url, []);
+    }
+  }, [location.pathname]);
   return (
     <StyledOnboarding>
       <Routes />

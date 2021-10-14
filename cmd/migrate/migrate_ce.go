9127388e69dd4161de5a866ac2d7488d987839db
Alexander Belanger <belanger@sas.upenn.edu>
2021-10-14 08:49:17 -0400

    remove debug line

--- cmd/migrate/migrate_ce.go
+++ cmd/migrate/migrate_ce.go
@@ -3,14 +3,10 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/porter-dev/porter/api/server/shared/config/env"
 	"gorm.io/gorm"
 )
 
 func InstanceMigrate(db *gorm.DB, dbConf *env.DBConf) error {
-	fmt.Println("HIT CE EDITION")
-
 	return nil
 }

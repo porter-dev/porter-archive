1cbd736868c7bb4765983e9ac74548a34a9e5615
Alexander Belanger <belanger@sas.upenn.edu>
2021-10-18 22:47:05 -0400

    update all dev handlers

--- api/server/handlers/provision/provision_doks.go
+++ api/server/handlers/provision/provision_doks.go
@@ -1,11 +1,20 @@
 package provision
 
 import (
+	"encoding/json"
 	"net/http"
 
 	"github.com/porter-dev/porter/api/server/handlers"
 	"github.com/porter-dev/porter/api/server/shared"
+	"github.com/porter-dev/porter/api/server/shared/apierrors"
 	"github.com/porter-dev/porter/api/server/shared/config"
+	"github.com/porter-dev/porter/api/types"
+	"github.com/porter-dev/porter/internal/analytics"
+	"github.com/porter-dev/porter/internal/kubernetes/provisioner"
+	"github.com/porter-dev/porter/internal/kubernetes/provisioner/do/doks"
+	"github.com/porter-dev/porter/internal/models"
+	"github.com/porter-dev/porter/internal/repository"
+	"gorm.io/gorm"
 )
 
 type ProvisionDOKSHandler struct {
@@ -23,87 +32,106 @@ func NewProvisionDOKSHandler(
 }
 
 func (c *ProvisionDOKSHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	// // read the user and project from context
-	// user, _ := r.Context().Value(types.UserScope).(*models.User)
-	// proj, _ := r.Context().Value(types.ProjectScope).(*models.Project)
+	// read the user and project from context
+	user, _ := r.Context().Value(types.UserScope).(*models.User)
+	proj, _ := r.Context().Value(types.ProjectScope).(*models.Project)
 
-	// request := &types.CreateDOKSInfraRequest{
-	// 	ProjectID: proj.ID,
-	// }
+	request := &types.CreateDOKSInfraRequest{
+		ProjectID: proj.ID,
+	}
 
-	// if ok := c.DecodeAndValidate(w, r, request); !ok {
-	// 	return
-	// }
+	if ok := c.DecodeAndValidate(w, r, request); !ok {
+		return
+	}
 
-	// // get the AWS integration
-	// doInt, err := c.Repo().OAuthIntegration().ReadOAuthIntegration(proj.ID, request.DOIntegrationID)
+	// get the DO integration, to check that integration exists and belongs to the project
+	doInt, err := c.Repo().OAuthIntegration().ReadOAuthIntegration(proj.ID, request.DOIntegrationID)
 
-	// if err != nil {
-	// 	if err == gorm.ErrRecordNotFound {
-	// 		c.HandleAPIError(w, r, apierrors.NewErrForbidden(err))
-	// 	} else {
-	// 		c.HandleAPIError(w, r, apierrors.NewErrInternal(err))
-	// 	}
+	if err != nil {
+		if err == gorm.ErrRecordNotFound {
+			c.HandleAPIError(w, r, apierrors.NewErrForbidden(err))
+		} else {
+			c.HandleAPIError(w, r, apierrors.NewErrInternal(err))
+		}
 
-	// 	return
-	// }
+		return
+	}
 
-	// suffix, err := repository.GenerateRandomBytes(6)
+	suffix, err := repository.GenerateRandomBytes(6)
 
-	// if err != nil {
-	// 	c.HandleAPIError(w, r, apierrors.NewErrInternal(err))
-	// 	return
-	// }
+	if err != nil {
+		c.HandleAPIError(w, r, apierrors.NewErrInternal(err))
+		return
+	}
 
-	// infra := &models.Infra{
-	// 	Kind:            types.InfraDOKS,
-	// 	ProjectID:       proj.ID,
-	// 	Suffix:          suffix,
-	// 	Status:          types.StatusCreating,
-	// 	DOIntegrationID: request.DOIntegrationID,
-	// 	CreatedByUserID: user.ID,
-	// }
+	lastApplied, err := json.Marshal(request)
 
-	// // handle write to the database
-	// infra, err = c.Repo().Infra().CreateInfra(infra)
+	// parse infra last applied into DOKS config
+	if err != nil {
+		c.HandleAPIError(w, r, apierrors.NewErrInternal(err))
+		return
+	}
 
-	// if err != nil {
-	// 	c.HandleAPIError(w, r, apierrors.NewErrInternal(err))
-	// 	return
-	// }
+	infra := &models.Infra{
+		Kind:            types.InfraDOKS,
+		ProjectID:       proj.ID,
+		Suffix:          suffix,
+		Status:          types.StatusCreating,
+		DOIntegrationID: request.DOIntegrationID,
+		CreatedByUserID: user.ID,
+		LastApplied:     lastApplied,
+	}
 
-	// // launch provisioning pod
-	// _, err = c.Config().ProvisionerAgent.ProvisionDOKS(
-	// 	&kubernetes.SharedProvisionOpts{
-	// 		ProjectID:           proj.ID,
-	// 		Repo:                c.Repo(),
-	// 		Infra:               infra,
-	// 		Operation:           provisioner.Apply,
-	// 		PGConf:              c.Config().DBConf,
-	// 		RedisConf:           c.Config().RedisConf,
-	// 		ProvImageTag:        c.Config().ServerConf.ProvisionerImageTag,
-	// 		ProvImagePullSecret: c.Config().ServerConf.ProvisionerImagePullSecret,
-	// 	},
-	// 	doInt,
-	// 	c.Config().DOConf,
-	// 	request.DORegion,
-	// 	request.DOKSName,
-	// )
+	// handle write to the database
+	infra, err = c.Repo().Infra().CreateInfra(infra)
 
-	// if err != nil {
-	// 	infra.Status = types.StatusError
-	// 	infra, _ = c.Repo().Infra().UpdateInfra(infra)
-	// 	c.HandleAPIError(w, r, apierrors.NewErrInternal(err))
-	// 	return
-	// }
+	if err != nil {
+		c.HandleAPIError(w, r, apierrors.NewErrInternal(err))
+		return
+	}
 
-	// c.Config().AnalyticsClient.Track(analytics.ClusterProvisioningStartTrack(
-	// 	&analytics.ClusterProvisioningStartTrackOpts{
-	// 		ProjectScopedTrackOpts: analytics.GetProjectScopedTrackOpts(user.ID, proj.ID),
-	// 		ClusterType:            types.InfraDOKS,
-	// 		InfraID:                infra.ID,
-	// 	},
-	// ))
+	opts, err := GetSharedProvisionerOpts(c.Config(), infra)
 
-	// c.WriteResult(w, r, infra.ToInfraType())
+	if err != nil {
+		c.HandleAPIError(w, r, apierrors.NewErrInternal(err))
+		return
+	}
+
+	vaultToken := ""
+
+	if c.Config().CredentialBackend != nil {
+		vaultToken, err = c.Config().CredentialBackend.CreateOAuthToken(doInt)
+
+		if err != nil {
+			c.HandleAPIError(w, r, apierrors.NewErrInternal(err))
+			return
+		}
+	}
+
+	opts.CredentialExchange.VaultToken = vaultToken
+	opts.DOKS = &doks.Conf{
+		DORegion:        request.DORegion,
+		DOKSClusterName: request.DOKSName,
+	}
+
+	opts.OperationKind = provisioner.Apply
+
+	err = c.Config().ProvisionerAgent.Provision(opts)
+
+	if err != nil {
+		infra.Status = types.StatusError
+		infra, _ = c.Repo().Infra().UpdateInfra(infra)
+		c.HandleAPIError(w, r, apierrors.NewErrInternal(err))
+		return
+	}
+
+	c.Config().AnalyticsClient.Track(analytics.ClusterProvisioningStartTrack(
+		&analytics.ClusterProvisioningStartTrackOpts{
+			ProjectScopedTrackOpts: analytics.GetProjectScopedTrackOpts(user.ID, proj.ID),
+			ClusterType:            types.InfraDOKS,
+			InfraID:                infra.ID,
+		},
+	))
+
+	c.WriteResult(w, r, infra.ToInfraType())
 }
--- api/server/handlers/provision/provision_gke.go
+++ api/server/handlers/provision/provision_gke.go
@@ -1,11 +1,20 @@
 package provision
 
 import (
+	"encoding/json"
 	"net/http"
 
 	"github.com/porter-dev/porter/api/server/handlers"
 	"github.com/porter-dev/porter/api/server/shared"
+	"github.com/porter-dev/porter/api/server/shared/apierrors"
 	"github.com/porter-dev/porter/api/server/shared/config"
+	"github.com/porter-dev/porter/api/types"
+	"github.com/porter-dev/porter/internal/analytics"
+	"github.com/porter-dev/porter/internal/kubernetes/provisioner"
+	"github.com/porter-dev/porter/internal/kubernetes/provisioner/gcp/gke"
+	"github.com/porter-dev/porter/internal/models"
+	"github.com/porter-dev/porter/internal/repository"
+	"gorm.io/gorm"
 )
 
 type ProvisionGKEHandler struct {
@@ -23,85 +32,105 @@ func NewProvisionGKEHandler(
 }
 
 func (c *ProvisionGKEHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	// // read the user and project from context
-	// user, _ := r.Context().Value(types.UserScope).(*models.User)
-	// proj, _ := r.Context().Value(types.ProjectScope).(*models.Project)
+	// read the user and project from context
+	user, _ := r.Context().Value(types.UserScope).(*models.User)
+	proj, _ := r.Context().Value(types.ProjectScope).(*models.Project)
 
-	// request := &types.CreateGKEInfraRequest{
-	// 	ProjectID: proj.ID,
-	// }
+	request := &types.CreateGKEInfraRequest{
+		ProjectID: proj.ID,
+	}
 
-	// if ok := c.DecodeAndValidate(w, r, request); !ok {
-	// 	return
-	// }
+	if ok := c.DecodeAndValidate(w, r, request); !ok {
+		return
+	}
 
-	// // get the AWS integration
-	// gcpInt, err := c.Repo().GCPIntegration().ReadGCPIntegration(proj.ID, request.GCPIntegrationID)
+	// get the GCP integration, to check that integration exists and belongs to the project
+	gcpInt, err := c.Repo().GCPIntegration().ReadGCPIntegration(proj.ID, request.GCPIntegrationID)
 
-	// if err != nil {
-	// 	if err == gorm.ErrRecordNotFound {
-	// 		c.HandleAPIError(w, r, apierrors.NewErrForbidden(err))
-	// 	} else {
-	// 		c.HandleAPIError(w, r, apierrors.NewErrInternal(err))
-	// 	}
+	if err != nil {
+		if err == gorm.ErrRecordNotFound {
+			c.HandleAPIError(w, r, apierrors.NewErrForbidden(err))
+		} else {
+			c.HandleAPIError(w, r, apierrors.NewErrInternal(err))
+		}
 
-	// 	return
-	// }
+		return
+	}
 
-	// suffix, err := repository.GenerateRandomBytes(6)
+	suffix, err := repository.GenerateRandomBytes(6)
 
-	// if err != nil {
-	// 	c.HandleAPIError(w, r, apierrors.NewErrInternal(err))
-	// 	return
-	// }
+	if err != nil {
+		c.HandleAPIError(w, r, apierrors.NewErrInternal(err))
+		return
+	}
 
-	// infra := &models.Infra{
-	// 	Kind:             types.InfraGKE,
-	// 	ProjectID:        proj.ID,
-	// 	Suffix:           suffix,
-	// 	Status:           types.StatusCreating,
-	// 	GCPIntegrationID: request.GCPIntegrationID,
-	// 	CreatedByUserID:  user.ID,
-	// }
+	lastApplied, err := json.Marshal(request)
 
-	// // handle write to the database
-	// infra, err = c.Repo().Infra().CreateInfra(infra)
+	// parse infra last applied into GKE config
+	if err != nil {
+		c.HandleAPIError(w, r, apierrors.NewErrInternal(err))
+		return
+	}
 
-	// if err != nil {
-	// 	c.HandleAPIError(w, r, apierrors.NewErrInternal(err))
-	// 	return
-	// }
+	infra := &models.Infra{
+		Kind:             types.InfraGKE,
+		ProjectID:        proj.ID,
+		Suffix:           suffix,
+		Status:           types.StatusCreating,
+		GCPIntegrationID: request.GCPIntegrationID,
+		CreatedByUserID:  user.ID,
+		LastApplied:      lastApplied,
+	}
 
-	// // launch provisioning pod
-	// _, err = c.Config().ProvisionerAgent.ProvisionGKE(
-	// 	&kubernetes.SharedProvisionOpts{
-	// 		ProjectID:           proj.ID,
-	// 		Repo:                c.Repo(),
-	// 		Infra:               infra,
-	// 		Operation:           provisioner.Apply,
-	// 		PGConf:              c.Config().DBConf,
-	// 		RedisConf:           c.Config().RedisConf,
-	// 		ProvImageTag:        c.Config().ServerConf.ProvisionerImageTag,
-	// 		ProvImagePullSecret: c.Config().ServerConf.ProvisionerImagePullSecret,
-	// 	},
-	// 	gcpInt,
-	// 	request.GKEName,
-	// )
+	// handle write to the database
+	infra, err = c.Repo().Infra().CreateInfra(infra)
 
-	// if err != nil {
-	// 	infra.Status = types.StatusError
-	// 	infra, _ = c.Repo().Infra().UpdateInfra(infra)
-	// 	c.HandleAPIError(w, r, apierrors.NewErrInternal(err))
-	// 	return
-	// }
+	if err != nil {
+		c.HandleAPIError(w, r, apierrors.NewErrInternal(err))
+		return
+	}
 
-	// c.Config().AnalyticsClient.Track(analytics.ClusterProvisioningStartTrack(
-	// 	&analytics.ClusterProvisioningStartTrackOpts{
-	// 		ProjectScopedTrackOpts: analytics.GetProjectScopedTrackOpts(user.ID, proj.ID),
-	// 		ClusterType:            types.InfraGKE,
-	// 		InfraID:                infra.ID,
-	// 	},
-	// ))
+	opts, err := GetSharedProvisionerOpts(c.Config(), infra)
 
-	// c.WriteResult(w, r, infra.ToInfraType())
+	if err != nil {
+		c.HandleAPIError(w, r, apierrors.NewErrInternal(err))
+		return
+	}
+
+	vaultToken := ""
+
+	if c.Config().CredentialBackend != nil {
+		vaultToken, err = c.Config().CredentialBackend.CreateGCPToken(gcpInt)
+
+		if err != nil {
+			c.HandleAPIError(w, r, apierrors.NewErrInternal(err))
+			return
+		}
+	}
+
+	opts.CredentialExchange.VaultToken = vaultToken
+	opts.GKE = &gke.Conf{
+		ClusterName: request.GKEName,
+	}
+
+	opts.OperationKind = provisioner.Apply
+
+	err = c.Config().ProvisionerAgent.Provision(opts)
+
+	if err != nil {
+		infra.Status = types.StatusError
+		infra, _ = c.Repo().Infra().UpdateInfra(infra)
+		c.HandleAPIError(w, r, apierrors.NewErrInternal(err))
+		return
+	}
+
+	c.Config().AnalyticsClient.Track(analytics.ClusterProvisioningStartTrack(
+		&analytics.ClusterProvisioningStartTrackOpts{
+			ProjectScopedTrackOpts: analytics.GetProjectScopedTrackOpts(user.ID, proj.ID),
+			ClusterType:            types.InfraGKE,
+			InfraID:                infra.ID,
+		},
+	))
+
+	c.WriteResult(w, r, infra.ToInfraType())
 }

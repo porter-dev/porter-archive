fe8774877de325c9ec7e72e5ea8470f502b77efc
Mohammed Nafees <hello@mnafees.me>
2021-11-24 22:31:47 +0530

    fix failing test

--- internal/repository/test/build_config.go
+++ internal/repository/test/build_config.go
@@ -17,19 +17,32 @@ func NewBuildConfigRepository(canQuery bool) repository.BuildConfigRepository {
 }
 
 func (repo *BuildConfigRepository) CreateBuildConfig(
-	a *models.BuildConfig,
+	bc *models.BuildConfig,
 ) (*models.BuildConfig, error) {
 	if !repo.canQuery {
 		return nil, errors.New("cannot write database")
 	}
 
-	repo.buildConfigs = append(repo.buildConfigs, a)
-	a.ID = uint(len(repo.buildConfigs))
+	repo.buildConfigs = append(repo.buildConfigs, bc)
+	bc.ID = uint(len(repo.buildConfigs))
 
-	return a, nil
+	return bc, nil
 }
 
 func (repo *BuildConfigRepository) UpdateBuildConfig(bc *models.BuildConfig) (*models.BuildConfig, error) {
+	if !repo.canQuery {
+		return nil, errors.New("cannot write database")
+	}
+
 	// TODO
 	return bc, nil
+}
+
+func (repo *BuildConfigRepository) GetBuildConfig(id uint) (*models.BuildConfig, error) {
+	if !repo.canQuery {
+		return nil, errors.New("cannot write database")
+	}
+
+	// TODO
+	return nil, nil
 }

38886d5c61d052fbcc645b1aec952786ca1de81f
Mohammed Nafees <hello@mnafees.me>
2021-12-03 01:10:19 +0530

    New porter driver

--- cli/cmd/apply/porter_driver.go
+++ cli/cmd/apply/porter_driver.go
@@ -1,1 +1,25 @@
 package apply
+
+import (
+	"github.com/porter-dev/switchboard/internal/models"
+	"github.com/porter-dev/switchboard/pkg/drivers"
+)
+
+type Driver struct {
+}
+
+func NewPorterDriver() drivers.Driver {
+	return &Driver{}
+}
+
+func (d *Driver) ShouldApply(resource *models.Resource) bool {
+
+}
+
+func (d *Driver) Apply(resource *models.Resource) (*models.Resource, error) {
+
+}
+
+func (d *Driver) Output() (map[string]interface{}, error) {
+
+}

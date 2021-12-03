f9453981eab05761e269dad2a56d7b0202fc604d
jnfrati <nicofrati@gmail.com>
2021-12-02 21:19:20 -0300

    Added dynamic link to be able to use it as anchor tab or router link based on to prop

---
+++ dashboard/src/components/DynamicLink.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link, LinkProps } from "react-router-dom";
+
+const DynamicLink: React.FC<LinkProps> = ({ to, children, ...props }) => {
+  // It is a simple element with nothing to link to
+  if (!to) return <span {...props}>{children}</span>;
+
+  // It is intended to be an external link
+  if (typeof to === "string" && /^https?:\/\//.test(to))
+    return (
+      <a href={to} {...props}>
+        {children}
+      </a>
+    );
+
+  // Finally, it is an internal link
+  return (
+    <Link to={to} {...props}>
+      {children}
+    </Link>
+  );
+};
+
+export default DynamicLink;

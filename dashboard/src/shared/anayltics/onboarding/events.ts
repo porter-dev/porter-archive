84beb8f6eb15bfe2b6edab226b9be556570e4b51
jnfrati <nicofrati@gmail.com>
2021-12-13 10:15:09 -0300

    Started adding events

---
+++ dashboard/src/shared/anayltics/onboarding/events.ts
@@ -0,0 +1,26 @@
+// Common tracks
+
+export const REDIRECT_TO_GUIDE = "FE Redirect to guide";
+
+// New project tracks
+export const PROJECT_CREATION_NEW_PROJECT_EVENT = "FE Create project";
+
+// CONNECT SOURCE TRACKS
+export const CONNECT_SOURCE_CONNECT_GITHUB_BUTTON_CLICKED = "FE Connect Github";
+export const CONNECT_SOURCE_USE_DOCKER_REGISTRY_CLICKED =
+  "FE Use docker registry";
+export const CONNECT_SOURCE_CONTINUE_AFTER_GITHUB_CONNECT =
+  "FE Continue after Github connect";
+export const CONNECT_SOURCE_INSTALL_ON_MORE_REPOSITORIES_CLICKED =
+  "FE Install on more repositories";
+
+// CONNECT REGISTRY TRACKS
+
+export const CONNECT_REGISTRY_SKIP_REGISTRY_CONNECTION =
+  "FE Skip registry connection";
+export const CONNECT_REGISTRY_INTENT = "FE Connect registry intent";
+export const CONNECT_REGISTRY_ADD_CREDENTIALS =
+  "FE Connect registry added credentials";
+
+export const CONNECT_REGISTRY_CONNECT_REGISTRY_CLICKED =
+  "FE Connect registry clicked";

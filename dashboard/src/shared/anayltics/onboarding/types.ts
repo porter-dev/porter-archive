7ac29e6c3c1c99bd2d416066d6c8f82e08e72d1d
jnfrati <nicofrati@gmail.com>
2021-12-15 01:21:39 -0300

    Added types and tracks to new project and connect source

--- dashboard/src/shared/anayltics/onboarding/types.ts
+++ dashboard/src/shared/anayltics/onboarding/types.ts
@@ -1,3 +1,34 @@
-type RedirectToGuideProps = {
+export type TrackRedirectToGuideProps = {
   step: string;
+  guide_url: string;
+  provider?: string;
+};
+
+export type TrackConnectRegistryIntentProps = {
+  provider: string;
+};
+
+export type TrackRegistryAddCredentialsProps = {
+  step: string;
+  provider: string;
+};
+
+export type TrackConnectRegistryClickedProps = {
+  provider: string;
+  registry_name: string;
+};
+
+export type TrackProvisionIntentProps = {
+  provider: string;
+};
+
+export type TrackProvisionAddCredentialsProps = {
+  step: string;
+  provider: string;
+};
+
+export type TrackProvisionResourcesClickedProps = {
+  provider: string;
+  machine_type: string;
+  cluster_name: string;
 };

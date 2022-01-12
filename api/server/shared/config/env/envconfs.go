3b5dcc745f106699ccee9fb8a48c1e67dc8b1974
Alexander Belanger <belanger@sas.upenn.edu>
2022-01-12 07:47:39 -0500

    fix last merge conflict

--- api/server/shared/config/env/envconfs.go
+++ api/server/shared/config/env/envconfs.go
@@ -94,15 +94,11 @@ type ServerConf struct {
 	RetoolToken string `env:"RETOOL_TOKEN"`
 
 	// Enable pprof profiling endpoints
-<<<<<<< HEAD
 	PprofEnabled    bool `env:"PPROF_ENABLED,default=false"`
 	ProvisionerTest bool `env:"PROVISIONER_TEST,default=false"`
-=======
-	PprofEnabled bool `env:"PPROF_ENABLED,default=false"`
 
 	// Disable filtering for project creation
 	DisableAllowlist bool `env:"DISABLE_ALLOWLIST,default=false"`
->>>>>>> master
 }
 
 // DBConf is the database configuration: if generated from environment variables,

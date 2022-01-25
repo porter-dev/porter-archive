09f995078676d06887ee9d718961f010ee670352
jnfrati <nicofrati@gmail.com>
2022-01-25 18:30:54 -0300

    Redesigned tooltip to avoid material ui one

--- dashboard/src/components/DocsHelper.tsx
+++ dashboard/src/components/DocsHelper.tsx
@@ -1,15 +1,15 @@
-import React, { Component, useState } from "react";
-import styled, { createGlobalStyle } from "styled-components";
-import Button from "@material-ui/core/Button";
-import Tooltip from "@material-ui/core/Tooltip";
-import { ClickAwayListener, TooltipProps } from "@material-ui/core";
+import React from "react";
+import styled from "styled-components";
+
+import { ClickAwayListener } from "@material-ui/core";
 
 type Props = {
   tooltipText: string;
   link: string;
+  placement?: TooltipPlacement;
 };
 
-const DocsHelper: React.FC<Props> = ({ tooltipText, link }) => {
+const DocsHelper: React.FC<Props> = ({ tooltipText, link, placement }) => {
   const [open, setOpen] = React.useState(false);
 
   const handleTooltipClose = () => {
@@ -32,38 +32,79 @@ const DocsHelper: React.FC<Props> = ({ tooltipText, link }) => {
         }}
       >
         <div>
-          <Tooltip
-            PopperProps={{
-              disablePortal: true,
-              placement: "top-end",
-            }}
-            onClose={handleTooltipClose}
-            open={open}
-            interactive
-            disableFocusListener
-            disableHoverListener
-            disableTouchListener
-            title={
+          <HelperButton onClick={handleTooltipToggle}>
+            <i className="material-icons">help_outline</i>
+          </HelperButton>
+          {open && (
+            <Tooltip placement={placement}>
               <StyledContent onClick={handleTooltipOpen}>
                 {tooltipText}
                 <A target="_blank" href={link}>
                   Documentation {">"}
                 </A>
               </StyledContent>
-            }
-          >
-            <HelperButton onClick={handleTooltipToggle}>
-              <i className="material-icons">help_outline</i>
-            </HelperButton>
-          </Tooltip>
+            </Tooltip>
+          )}
         </div>
       </ClickAwayListener>
-      <TooltipStyle />
     </DocsHelperContainer>
   );
 };
 
 export default DocsHelper;
+
+type TooltipPlacement = "top-end" | "bottom-end" | "top-start" | "bottom-start";
+
+const Tooltip = styled.div<{ placement: TooltipPlacement }>`
+  position: absolute;
+  ${({ placement }) => {
+    switch (placement) {
+      case "top-start":
+        return `
+          bottom: 25px;
+          left: 0px;
+        `;
+      case "bottom-end":
+        return `
+          top: 25px;
+          right: 0px;
+        `;
+      case "bottom-start":
+        return `
+          top: 25px;
+          left: 0px;
+        `;
+      case "top-end":
+      default:
+        return `
+          bottom: 25px;
+          right: 0px;
+        `;
+    }
+  }}
+  word-wrap: break-word;
+  min-height: 18px;
+  width: fit-content;
+  padding: 5px 7px;
+  z-index: 999;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  flex: 1;
+  color: white;
+  text-transform: none;
+  opacity: 0;
+  animation: faded-in 0.2s 0.15s;
+  animation-fill-mode: forwards;
+  @keyframes faded-in {
+    from {
+      opacity: 0;
+    }
+    to {
+      opacity: 1;
+    }
+  }
+`;
 
 const StyledContent = styled.div`
   font-family: "Work Sans", sans-serif;
@@ -72,7 +113,8 @@ const StyledContent = styled.div`
   padding: 12px 14px;
   line-height: 1.5em;
   user-select: text;
-  width: calc(100% + 14px);
+  width: max-content;
+  max-width: 300px;
   height: calc(100% + 10px);
   margin-left: -7px;
   height: 100%;
@@ -96,15 +138,6 @@ const HelperButton = styled.div`
   }
 `;
 
-const TooltipStyle = createGlobalStyle`
-  .MuiTooltip-tooltip {
-    background-color: #00000000 !important;
-    font-size: 12px !important;
-    padding: 0px;
-    max-width: 300px !important;    
-  }
-`;
-
 const A = styled.a`
   display: inline-block;
   height: 20px;
@@ -118,4 +151,5 @@ const A = styled.a`
 
 const DocsHelperContainer = styled.div`
   margin-left: auto;
+  position: relative;
 `;

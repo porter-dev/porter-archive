e74adb20b17b2ce1e78eb0d69e2869d8f4b4dd05
Alexander Belanger <belanger@sas.upenn.edu>
2022-03-22 11:40:11 -0400

    clean up imports and declarations

--- dashboard/src/main/home/cluster-dashboard/dashboard/incidents/ExpandedContainer.tsx
+++ dashboard/src/main/home/cluster-dashboard/dashboard/incidents/ExpandedContainer.tsx
@@ -1,13 +1,8 @@
 import Description from "components/Description";
-import useLastSeenPodStatus from "components/events/useLastSeenPodStatus";
 import Heading from "components/form-components/Heading";
-import Loading from "components/Loading";
-import { isEmpty } from "lodash";
-import React, { useContext, useEffect, useMemo, useState } from "react";
-import api from "shared/api";
-import { Context } from "shared/Context";
+import React from "react";
 import styled from "styled-components";
-import { IncidentContainerEvent, IncidentEvent } from "./IncidentPage";
+import { IncidentContainerEvent } from "./IncidentPage";
 
 type Props = {
   container: IncidentContainerEvent;
@@ -35,40 +30,10 @@ const ExpandedContainer: React.FC<Props> = ({ container, logs }) => {
 
 export default ExpandedContainer;
 
-const PorterFormContainer = styled.div`
-  position: relative;
-  min-width: 300px;
-`;
-
-const Br = styled.div`
-  width: 100%;
-  height: 20px;
-`;
-
 const StyledCard = styled.div`
   display: grid;
   grid-row-gap: 15px;
   grid-template-columns: 1;
-`;
-
-const BackArrowContainer = styled.div`
-  width: 100%;
-  height: 24px;
-`;
-
-const BackArrow = styled.div`
-  > i {
-    color: #aaaabb;
-    font-size: 18px;
-    margin-right: 6px;
-  }
-
-  color: #aaaabb;
-  display: flex;
-  align-items: center;
-  font-size: 14px;
-  cursor: pointer;
-  width: 120px;
 `;
 
 const MetadataContainer = styled.div`
@@ -80,20 +45,6 @@ const MetadataContainer = styled.div`
   min-height: 100px;
   font-size: 13px;
   margin: 12px 0;
-`;
-
-const LogTitleContainer = styled.div`
-  padding: 0 20px;
-  margin-bottom: 20px;
-`;
-
-const LogSectionContainer = styled.div`
-  margin-bottom: 3px;
-  border-radius: 6px;
-  background: #2e3135;
-  overflow: hidden;
-  max-height: 500px;
-  font-size: 13px;
 `;
 
 const LogContainer = styled.div`
@@ -108,10 +59,4 @@ const LogContainer = styled.div`
   height: 400px;
   border-radius: 4px;
   margin: 12px 0 24px 0;
-`;
-
-const Log = styled.div`
-  font-family: monospace, sans-serif;
-  font-size: 12px;
-  color: white;
 `;

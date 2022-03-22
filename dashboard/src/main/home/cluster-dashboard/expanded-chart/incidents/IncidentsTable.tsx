abd6a8e8096819e08f6b9682433e96114bc29f15
jnfrati <nicofrati@gmail.com>
2022-03-22 13:25:37 -0300

    Implemented refresh for incidents tables and incident page

--- dashboard/src/main/home/cluster-dashboard/expanded-chart/incidents/IncidentsTable.tsx
+++ dashboard/src/main/home/cluster-dashboard/expanded-chart/incidents/IncidentsTable.tsx
@@ -23,6 +23,7 @@ const IncidentsTable = ({
 
   const [incidents, setIncidents] = useState<IncidentsWithoutEvents[]>(null);
   const [hasError, setHasError] = useState(false);
+  const [isRefreshing, setIsRefreshing] = useState(false);
 
   useEffect(() => {
     let isSubscribed = true;
@@ -57,6 +58,32 @@ const IncidentsTable = ({
       isSubscribed = false;
     };
   }, [currentCluster, currentProject]);
+
+  const refreshIncidents = async () => {
+    setIsRefreshing(true);
+    try {
+      const incidents = await api
+        .getIncidentsByReleaseName<{ incidents: IncidentsWithoutEvents[] }>(
+          "<token>",
+          {
+            namespace: namespace,
+            release_name: releaseName,
+          },
+          {
+            project_id: currentProject.id,
+            cluster_id: currentCluster.id,
+          }
+        )
+        .then((res) => res.data?.incidents || []);
+
+      setIncidents(incidents);
+    } catch (err) {
+      setHasError(true);
+      setCurrentError(err);
+    } finally {
+      setIsRefreshing(false);
+    }
+  };
 
   const columns = useMemo(() => {
     return [
@@ -99,6 +126,8 @@ const IncidentsTable = ({
             );
           }}
           hasError={hasError}
+          onRefresh={refreshIncidents}
+          isRefreshing={isRefreshing}
         />
       </StyledCard>
     </TableWrapper>

6fee7ec0abb3e7e26b6a9153674fa0e9eb5d3d95
jnfrati <nicofrati@gmail.com>
2022-04-01 12:03:23 -0300

    Rollback change to dashboard

--- dashboard/src/main/home/dashboard/Dashboard.tsx
+++ dashboard/src/main/home/dashboard/Dashboard.tsx
@@ -39,7 +39,7 @@ class Dashboard extends Component<PropsType, StateType> {
     infras: [] as InfraType[],
     pressingCtrl: false,
     pressingK: false,
-    showFormDebugger: true,
+    showFormDebugger: false,
   };
 
   refreshInfras = () => {

766da95160b3ca46c9ceb7cdc65429178c3a38cf
jnfrati <nicofrati@gmail.com>
2022-04-13 09:43:53 -0300

    Implemented tag repository

---
+++ internal/repository/tag.go
@@ -0,0 +1,13 @@
+package repository
+
+import "github.com/porter-dev/porter/internal/models"
+
+// GitRepoRepository represents the set of queries on the
+// GitRepo model
+type TagRepository interface {
+	CreateTag(tag *models.Tag) (*models.Tag, error)
+	ReadTagByNameAndProjectId(tagName string, projectID uint) (*models.Tag, error)
+	ListTagsByProjectId(projectId uint) ([]*models.Tag, error)
+	UpdateTag(tag *models.Tag) (*models.Tag, error)
+	DeleteTag(id uint) error
+}

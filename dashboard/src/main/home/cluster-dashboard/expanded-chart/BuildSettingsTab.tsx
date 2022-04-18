b75c2cb4a4cc042aa25ef3af5891fc9592bdc172
jnfrati <nicofrati@gmail.com>
2022-04-18 17:11:38 -0300

    Initialized

---
+++ dashboard/src/main/home/cluster-dashboard/expanded-chart/BuildSettingsTab.tsx
@@ -0,0 +1,50 @@
+import MultiSaveButton from "components/MultiSaveButton";
+import React from "react";
+import styled from "styled-components";
+
+const BuildSettingsTab = () => {
+  return (
+    <Wrapper>
+      <StyledSettingsSection>
+        <MultiSaveButton
+          options={[
+            {
+              text: "Save",
+              onClick: () => console.log("Save only"),
+              description:
+                "Save the values to be applied in the next workflow run",
+            },
+            {
+              text: "Save and re deploy",
+              onClick: () => console.log("Save and redeploy"),
+              description:
+                "Save the values and trigger the workflow to create a new deployment with the latest saved changes",
+            },
+          ]}
+          disabled={false}
+          makeFlush={true}
+          clearPosition={true}
+          statusPosition="right"
+          saveText=""
+        ></MultiSaveButton>
+      </StyledSettingsSection>
+    </Wrapper>
+  );
+};
+
+export default BuildSettingsTab;
+
+const Wrapper = styled.div`
+  width: 100%;
+  padding-bottom: 65px;
+  height: 100%;
+`;
+
+const StyledSettingsSection = styled.div`
+  width: 100%;
+  background: #ffffff11;
+  padding: 0 35px;
+  padding-bottom: 15px;
+  position: relative;
+  border-radius: 8px;
+`;

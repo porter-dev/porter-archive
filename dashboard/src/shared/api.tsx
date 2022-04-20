df6704c314bb54f03a646e60dc829aae45394b4e
jnfrati <nicofrati@gmail.com>
2022-04-20 11:14:40 -0300

    Added re run gh workflow api endpoint

--- dashboard/src/shared/api.tsx
+++ dashboard/src/shared/api.tsx
@@ -1699,6 +1699,22 @@ const upgradePorterAgent = baseApi<
     `/api/projects/${project_id}/clusters/${cluster_id}/agent/upgrade`
 );
 
+const reRunGHWorkflow = baseApi<
+  {},
+  {
+    project_id: number;
+    cluster_id: number;
+    git_installation_id: number;
+    owner: string;
+    name: string;
+    filename: string;
+  }
+>(
+  "POST",
+  ({ project_id, git_installation_id, owner, name, cluster_id, filename }) =>
+    `/api/projects/${project_id}/gitrepos/${git_installation_id}/${owner}/${name}/clusters/${cluster_id}/rerun_workflow?filename=${filename}`
+);
+
 // Bundle export to allow default api import (api.<method> is more readable)
 export default {
   checkAuth,
@@ -1860,4 +1876,5 @@ export default {
   getIncidentLogsByLogId,
   upgradePorterAgent,
   deletePRDeployment,
+  reRunGHWorkflow,
 };

15ff5df9f35485d5b13e3baa99ef2ef350b166c5
Alexander Belanger <belanger@sas.upenn.edu>
2022-06-13 12:22:49 -0400

    add crud endpoints for stacks

--- api/server/handlers/stack/delete.go
+++ api/server/handlers/stack/delete.go
@@ -3,6 +3,7 @@ package stack
 import (
 	"net/http"
 
+	"github.com/porter-dev/porter/api/server/authz"
 	"github.com/porter-dev/porter/api/server/handlers"
 	"github.com/porter-dev/porter/api/server/shared"
 	"github.com/porter-dev/porter/api/server/shared/apierrors"
@@ -13,6 +14,7 @@ import (
 
 type StackDeleteHandler struct {
 	handlers.PorterHandlerWriter
+	authz.KubernetesAgentGetter
 }
 
 func NewStackDeleteHandler(
@@ -20,14 +22,39 @@ func NewStackDeleteHandler(
 	writer shared.ResultWriter,
 ) *StackDeleteHandler {
 	return &StackDeleteHandler{
-		PorterHandlerWriter: handlers.NewDefaultPorterHandler(config, nil, writer),
+		PorterHandlerWriter:   handlers.NewDefaultPorterHandler(config, nil, writer),
+		KubernetesAgentGetter: authz.NewOutOfClusterAgentGetter(config),
 	}
 }
 
 func (p *StackDeleteHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	stack, _ := r.Context().Value(types.StackScope).(*models.Stack)
+	cluster, _ := r.Context().Value(types.ClusterScope).(*models.Cluster)
+	namespace, _ := r.Context().Value(types.NamespaceScope).(string)
 
-	stack, err := p.Repo().Stack().DeleteStack(stack)
+	revision, err := p.Repo().Stack().ReadStackRevisionByNumber(stack.ID, stack.Revisions[0].ID)
+
+	if err != nil {
+		p.HandleAPIError(w, r, apierrors.NewErrInternal(err))
+		return
+	}
+
+	helmAgent, err := p.GetHelmAgent(r, cluster, namespace)
+
+	if err != nil {
+		p.HandleAPIError(w, r, apierrors.NewErrInternal(err))
+		return
+	}
+
+	// delete all resources in stack
+	for _, appResource := range revision.Resources {
+		deleteAppResource(&deleteAppResourceOpts{
+			helmAgent: helmAgent,
+			name:      appResource.Name,
+		})
+	}
+
+	stack, err = p.Repo().Stack().DeleteStack(stack)
 
 	if err != nil {
 		p.HandleAPIError(w, r, apierrors.NewErrInternal(err))

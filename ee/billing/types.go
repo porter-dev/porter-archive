1e8bad537c64b5cb68f374228cba1dffa6a923b4
Alexander Belanger <belanger@sas.upenn.edu>
2022-07-06 09:40:09 -0400

    update request for createteam endpoint

--- ee/billing/types.go
+++ ee/billing/types.go
@@ -4,9 +4,10 @@
 package billing
 
 type CreateCustomerRequest struct {
-	Email     string `json:"email" form:"required"`
-	UserID    uint   `json:"user_id" form:"required"`
-	ProjectID uint   `json:"project_id" form:"required"`
+	Email       string `json:"email" form:"required"`
+	UserID      uint   `json:"user_id" form:"required"`
+	ProjectID   uint   `json:"project_id" form:"required"`
+	ProjectName string `json:"project_name" form:"required"`
 }
 
 type DeleteCustomerRequest struct {

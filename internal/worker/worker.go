fbd9b8da7c1fd65a611cd2bbdc4b42f2caba9218
Mohammed Nafees <hello@mnafees.me>
2022-07-07 00:26:36 +0530

    basic worker pool and job queue

---
+++ internal/worker/worker.go
@@ -0,0 +1,44 @@
+package worker
+
+import (
+	"context"
+	"log"
+
+	"github.com/google/uuid"
+)
+
+type Job interface {
+	ID() uuid.UUID
+	Run() error
+}
+
+type Worker struct {
+	WorkerPool chan chan Job
+	JobChannel chan Job
+	ctx        context.Context
+}
+
+func NewWorker(ctx context.Context, workerPool chan chan Job) Worker {
+	return Worker{
+		WorkerPool: workerPool,
+		JobChannel: make(chan Job),
+		ctx:        ctx,
+	}
+}
+
+func (w Worker) Start() {
+	go func() {
+		for {
+			w.WorkerPool <- w.JobChannel
+
+			select {
+			case job := <-w.JobChannel:
+				if err := job.Run(); err != nil {
+					log.Default().Printf("error running job %s: %s", job.ID(), err.Error())
+				}
+			case <-w.ctx.Done():
+				return
+			}
+		}
+	}()
+}

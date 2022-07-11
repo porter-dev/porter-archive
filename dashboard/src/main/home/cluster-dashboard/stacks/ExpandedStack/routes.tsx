b0ca7b223383e82558a9838dc3fe03eba7207bef
jnfrati <nicofrati@gmail.com>
2022-07-11 20:44:35 -0300

    Implement new env group form for existing stacks.
    Implement routing for expanded stack.
    Added an onCancel function for the NewEnvGroupForm.
    Implemented the onCancel function on places using the NewEnvGroupForm.
    Added some error handling for onSubmit to new env group for existing stacks.

---
+++ dashboard/src/main/home/cluster-dashboard/stacks/ExpandedStack/routes.tsx
@@ -0,0 +1,25 @@
+import React, { useContext } from "react";
+import { Redirect, Route, Switch, useRouteMatch } from "react-router";
+import { Context } from "shared/Context";
+import ExpandedStack from "./ExpandedStack";
+import NewEnvGroup from "./NewEnvGroup";
+
+const routes = () => {
+  const { path } = useRouteMatch();
+
+  return (
+    <Switch>
+      <Route path={`${path}/new-env-group`} exact>
+        <NewEnvGroup />
+      </Route>
+      <Route path={`${path}/`} exact>
+        <ExpandedStack />
+      </Route>
+      <Route path={`*`}>
+        <div>Not found</div>
+      </Route>
+    </Switch>
+  );
+};
+
+export default routes;

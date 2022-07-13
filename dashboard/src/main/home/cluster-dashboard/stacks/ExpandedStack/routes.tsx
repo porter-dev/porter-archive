d7edb16aa1861a0cb7352577b2e5df5fbbfae087
jnfrati <nicofrati@gmail.com>
2022-07-13 13:19:11 -0300

    Implemented routing for new app resource and template selector page

--- dashboard/src/main/home/cluster-dashboard/stacks/ExpandedStack/routes.tsx
+++ dashboard/src/main/home/cluster-dashboard/stacks/ExpandedStack/routes.tsx
@@ -1,24 +1,31 @@
-import React, { useContext } from "react";
-import { Redirect, Route, Switch, useRouteMatch } from "react-router";
-import { Context } from "shared/Context";
+import React from "react";
+import { Route, Switch, useRouteMatch } from "react-router";
+
 import ExpandedStack from "./ExpandedStack";
+import NewAppResourceRoutes from "./NewAppResource";
 import NewEnvGroup from "./NewEnvGroup";
+import ExpandedStackStoreProvider from "./Store";
 
 const ExpandedStackRoutes = () => {
   const { path } = useRouteMatch();
 
   return (
-    <Switch>
-      <Route path={`${path}/new-env-group`} exact>
-        <NewEnvGroup />
-      </Route>
-      <Route path={`${path}/`} exact>
-        <ExpandedStack />
-      </Route>
-      <Route path={`*`}>
-        <div>Not found</div>
-      </Route>
-    </Switch>
+    <ExpandedStackStoreProvider>
+      <Switch>
+        <Route path={`${path}/new-env-group`} exact>
+          <NewEnvGroup />
+        </Route>
+        <Route path={`${path}/new-app-resource`}>
+          <NewAppResourceRoutes />
+        </Route>
+        <Route path={`${path}/`} exact>
+          <ExpandedStack />
+        </Route>
+        <Route path={`*`}>
+          <div>Not found</div>
+        </Route>
+      </Switch>
+    </ExpandedStackStoreProvider>
   );
 };
 

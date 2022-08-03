4d01004b3a596b73ee90c069e756f7fdadd1ff5c
Mohammed Nafees <hello@mnafees.me>
2022-08-03 15:33:26 +0530

    Add handlers for updating and deleting policies

---
+++ api/server/handlers/policy/delete.go
@@ -0,0 +1,43 @@
+package policy
+
+import (
+	"net/http"
+
+	"github.com/porter-dev/porter/api/server/handlers"
+	"github.com/porter-dev/porter/api/server/shared"
+	"github.com/porter-dev/porter/api/server/shared/config"
+	"github.com/porter-dev/porter/api/server/shared/requestutils"
+	"github.com/porter-dev/porter/api/types"
+	"github.com/porter-dev/porter/internal/models"
+)
+
+type PolicyDeleteHandler struct {
+	handlers.PorterHandlerReadWriter
+}
+
+func NewPolicyDeleteHandler(
+	config *config.Config,
+	decoderValidator shared.RequestDecoderValidator,
+	writer shared.ResultWriter,
+) *PolicyDeleteHandler {
+	return &PolicyDeleteHandler{
+		PorterHandlerReadWriter: handlers.NewDefaultPorterHandler(config, decoderValidator, writer),
+	}
+}
+
+func (p *PolicyDeleteHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	proj, _ := r.Context().Value(types.ProjectScope).(*models.Project)
+
+	policyID, reqErr := requestutils.GetURLParamString(r, types.URLParamPolicyID)
+
+	if reqErr != nil {
+		p.HandleAPIError(w, r, reqErr)
+		return
+	}
+
+	policy, err := p.Repo().Policy().ReadPolicy(proj.ID, policyID)
+
+	if err == nil {
+		p.Repo().Policy().DeletePolicy(policy)
+	}
+}
--- internal/models/project_role.go
+++ internal/models/project_role.go
@@ -7,12 +7,12 @@ import (
 type ProjectRole struct {
 	gorm.Model
 
-	ProjectID uint `gorm:"not null;check:project_id>0"`
-	PolicyID  uint `gorm:"not null;check:policy_id>0"`
+	ProjectID uint
+	PolicyID  uint
 
 	UniqueID string `gorm:"unique"`
 
-	Name string `gorm:"not null;check:name!=''"`
+	Name string
 
-	Users []User
+	Users []User `gorm:"many2many:user_roles"`
 }

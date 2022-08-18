71d56a601ff99577220ac0d7af8da9528687de57
Mohammed Nafees <hello@mnafees.me>
2022-08-18 15:53:18 +0530

    Add project role handlers

--- internal/models/project_role.go
+++ internal/models/project_role.go
@@ -1,18 +1,33 @@
 package models
 
 import (
+	"github.com/porter-dev/porter/api/types"
 	"gorm.io/gorm"
 )
 
 type ProjectRole struct {
 	gorm.Model
 
-	ProjectID uint
-
 	UniqueID string `gorm:"unique"`
+
+	ProjectID uint
+	PolicyUID string
 
 	Name string
 
-	Policies []Policy `gorm:"many2many:role_policies"`
-	Users    []User   `gorm:"many2many:user_roles"`
+	Users []User `gorm:"many2many:user_roles"`
+}
+
+func (role *ProjectRole) ToProjectRoleType(policy *types.PolicyDocument) *types.ProjectRole {
+	res := &types.ProjectRole{
+		ID:     role.UniqueID,
+		Name:   role.Name,
+		Policy: policy,
+	}
+
+	for _, user := range role.Users {
+		res.Users = append(res.Users, user.ID)
+	}
+
+	return res
 }
---
+++ internal/repository/project_role.go
@@ -0,0 +1,13 @@
+package repository
+
+import "github.com/porter-dev/porter/internal/models"
+
+// ProjectRoleRepository represents the set of queries on the ProjectRole model
+type ProjectRoleRepository interface {
+	CreateProjectRole(role *models.ProjectRole) (*models.ProjectRole, error)
+	ReadProjectRole(projectID uint, roleUID string) (*models.ProjectRole, error)
+	ListProjectRoles(projectID uint) ([]*models.ProjectRole, error)
+	UpdateUsersInProjectRole(projectID uint, roleUID string, userIDs []uint) error
+	UpdateProjectRole(role *models.ProjectRole) (*models.ProjectRole, error)
+	DeleteProjectRole(role *models.ProjectRole) (*models.ProjectRole, error)
+}

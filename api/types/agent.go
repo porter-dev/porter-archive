6cc3693a5c57d1f2328022a8e8cc277de0d99352
Mohammed Nafees <hello@mnafees.me>
2022-10-05 22:22:24 +0530

    return agent info in detect endpoint

--- api/types/agent.go
+++ api/types/agent.go
@@ -1,5 +1,7 @@
 package types
 
 type GetAgentResponse struct {
-	Version string `json:"version"`
+	Version       string `json:"version"`
+	LatestVersion string `json:"latest_version"`
+	ShouldUpgrade bool   `json:"should_upgrade"`
 }

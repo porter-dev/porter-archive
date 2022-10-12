75e950470baaa1402b26828fc9520c31da7c34cf
Alexander Belanger <belanger@sas.upenn.edu>
2022-10-12 12:16:29 -0400

    forward and backward continue times for agent

--- api/types/incident.go
+++ api/types/incident.go
@@ -116,8 +116,9 @@ type LogLine struct {
 }
 
 type GetLogResponse struct {
-	ContinueTime *time.Time `json:"continue_time"`
-	Logs         []LogLine  `json:"logs"`
+	BackwardContinueTime *time.Time `json:"backward_continue_time"`
+	ForwardContinueTime  *time.Time `json:"forward_continue_time"`
+	Logs                 []LogLine  `json:"logs"`
 }
 
 type GetEventRequest struct {

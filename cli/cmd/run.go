b9bdcf2e59d1136de62c7bb3dba6ce49e63f9548
Mohammed Nafees <hello@mnafees.me>
2022-10-21 10:51:37 -0400

    use for both builders

--- cli/cmd/run.go
+++ cli/cmd/run.go
@@ -129,7 +129,6 @@ func run(_ *types.GetAuthenticatedUserResponse, client *api.Client, args []strin
 		}
 
 		if release.BuildConfig != nil &&
-			strings.Contains(release.BuildConfig.Builder, "heroku") &&
 			execArgs[0] != "/cnb/lifecycle/launcher" &&
 			execArgs[0] != "launcher" {
 			// this is a buildpacks release using a heroku builder, prepend the launcher

ea97f5687f6ef0e9547ad3557e922bfb348df2a9
Alexander Belanger <belanger@sas.upenn.edu>
2022-10-28 09:53:12 -0400

    add fix for string utils casing

--- dashboard/src/shared/string_utils.ts
+++ dashboard/src/shared/string_utils.ts
@@ -91,6 +91,12 @@ export const timeFrom = (
 };
 
 export const capitalize = (s: string) => {
+  if (s.length == 0) {
+    return s;
+  } else if (s.length == 1) {
+    return s.charAt(0).toUpperCase();
+  }
+
   return s.charAt(0).toUpperCase() + s.substring(1).toLowerCase();
 };
 

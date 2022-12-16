729ee33111d41132f42c78c47e8eaa018c3d12d7
Stefan McShane <stefanmcshane@users.noreply.github.com>
2022-12-16 17:47:50 -0500

    tidy success case test

--- internal/repository/gorm/api_token_test.go
+++ internal/repository/gorm/api_token_test.go
@@ -15,16 +15,17 @@ func TestListAPITokensByProjectID(t *testing.T) {
 	initMultiUser(tester, t)
 	defer cleanup(tester, t)
 
-	// expected := false
-
 	projectID := tester.initProjects[0].Model.ID
 	found, err := tester.repo.APIToken().ListAPITokensByProjectID(projectID)
 	if err != nil {
 		t.Fatalf("%v\n", err)
 	}
 
-	// if found != expected {
-	// 	t.Errorf("expected found to be %t but got: %t", expected, found)
-	// }
-	t.Logf("stefan, %#v", found[0])
+	if len(found) != 1 {
+		t.Errorf("expected to find 1 row, found %d", len(found))
+	}
+
+	if found[0].ID != 1 {
+		t.Errorf("expected found to be %d but got: %d", 1, found[0].ID)
+	}
 }

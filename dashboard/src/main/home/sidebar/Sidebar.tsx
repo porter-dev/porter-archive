51870bb5a08b0855a181ca267a8e89a4a592fec8
Soham Parekh <sohamparekh@Sohams-MacBook-Pro.local>
2023-01-19 11:04:09 -0700

    hotfix: enable infra

--- dashboard/src/main/home/sidebar/Sidebar.tsx
+++ dashboard/src/main/home/sidebar/Sidebar.tsx
@@ -116,7 +116,7 @@ class Sidebar extends Component<PropsType, StateType> {
           </NavButton>
           {currentProject &&
             currentProject.managed_infra_enabled &&
-            user?.isPorterUser && (
+            (user?.isPorterUser || [6683].includes(currentProject.id)) && (
               <NavButton path={"/infrastructure"}>
                 <i className="material-icons">build_circle</i>
                 Infrastructure

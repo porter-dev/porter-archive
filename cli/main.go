8960c8b367d44f8abd4f1827b7d6827cfd9b62b4
Porter Support <93286801+portersupport@users.noreply.github.com>
2023-02-06 19:38:14 -0500

    Update main.go

--- cli/main.go
+++ cli/main.go
@@ -1,38 +1,11 @@
-//go:build cli
 // +build cli
 
 package main
 
 import (
-	"os"
-	"time"
-
-	"github.com/fatih/color"
-	"github.com/getsentry/sentry-go"
 	"github.com/porter-dev/porter/cli/cmd"
-	"github.com/porter-dev/porter/cli/cmd/config"
-	"github.com/porter-dev/porter/cli/cmd/errors"
 )
 
 func main() {
-	if errors.SentryDSN != "" {
-		err := sentry.Init(sentry.ClientOptions{
-			Dsn:         errors.SentryDSN,
-			Environment: "cli",
-			Debug:       config.Version == "dev",
-			Release:     config.Version,
-			IgnoreErrors: []string{
-				"Forbidden",
-			},
-		})
-
-		if err != nil {
-			color.New(color.FgRed).Fprintf(os.Stderr, "error initialising sentry: %s\n", err)
-			os.Exit(1)
-		}
-
-		defer sentry.Flush(2 * time.Second)
-	}
-
 	cmd.Execute()
 }

3a52d1c2ff7741e51c1f3c7b0e739d7747ce7fa4
Stefan McShane <stefanmcshane@users.noreply.github.com>
2023-04-05 16:53:52 -0400

    disabling migrations

--- cmd/migrate/main.go
+++ cmd/migrate/main.go
@@ -1,153 +1,140 @@
 package main
 
 import (
-	"errors"
-	"fmt"
 	"log"
 
-	"github.com/porter-dev/porter/api/server/shared/config/envloader"
-	"github.com/porter-dev/porter/cmd/migrate/keyrotate"
-	"github.com/porter-dev/porter/cmd/migrate/populate_source_config_display_name"
-	"github.com/porter-dev/porter/cmd/migrate/startup_migrations"
-
-	adapter "github.com/porter-dev/porter/internal/adapter"
-	"github.com/porter-dev/porter/internal/models"
-	"github.com/porter-dev/porter/internal/repository/gorm"
-	lr "github.com/porter-dev/porter/pkg/logger"
-
 	"github.com/joeshaw/envdecode"
-	pgorm "gorm.io/gorm"
 )
 
 func main() {
-	logger := lr.NewConsole(true)
-	logger.Info().Msg("running migrations")
+	// logger := lr.NewConsole(true)
+	// logger.Info().Msg("running migrations")
 
-	envConf, err := envloader.FromEnv()
-	if err != nil {
-		logger.Fatal().Err(err).Msg("could not load env conf")
-		return
-	}
+	// envConf, err := envloader.FromEnv()
+	// if err != nil {
+	// 	logger.Fatal().Err(err).Msg("could not load env conf")
+	// 	return
+	// }
 
-	db, err := adapter.New(envConf.DBConf)
-	if err != nil {
-		logger.Fatal().Err(err).Msg("could not connect to the database")
-		return
-	}
+	// db, err := adapter.New(envConf.DBConf)
+	// if err != nil {
+	// 	logger.Fatal().Err(err).Msg("could not connect to the database")
+	// 	return
+	// }
 
-	err = gorm.AutoMigrate(db, envConf.ServerConf.Debug)
-	if err != nil {
-		logger.Fatal().Err(err).Msg("gorm auto-migration failed")
-		return
-	}
-	if err := db.Raw("ALTER TABLE clusters DROP CONSTRAINT IF EXISTS fk_cluster_token_caches").Error; err != nil {
-		logger.Fatal().Err(err).Msg("failed to drop cluster token cache constraint")
-		return
-	}
-	if err := db.Raw("ALTER TABLE cluster_token_caches DROP CONSTRAINT IF EXISTS fk_clusters_token_cache").Error; err != nil {
-		logger.Fatal().Err(err).Msg("failed to drop clusters token cache constraint")
-		return
-	}
+	// err = gorm.AutoMigrate(db, envConf.ServerConf.Debug)
+	// if err != nil {
+	// 	logger.Fatal().Err(err).Msg("gorm auto-migration failed")
+	// 	return
+	// }
+	// if err := db.Raw("ALTER TABLE clusters DROP CONSTRAINT IF EXISTS fk_cluster_token_caches").Error; err != nil {
+	// 	logger.Fatal().Err(err).Msg("failed to drop cluster token cache constraint")
+	// 	return
+	// }
+	// if err := db.Raw("ALTER TABLE cluster_token_caches DROP CONSTRAINT IF EXISTS fk_clusters_token_cache").Error; err != nil {
+	// 	logger.Fatal().Err(err).Msg("failed to drop clusters token cache constraint")
+	// 	return
+	// }
 
-	err = db.Transaction(func(tx *pgorm.DB) error {
-		if err := db.Exec("alter table aws_assume_role_chains DROP CONSTRAINT IF EXISTS fk_projects;").Error; err != nil {
-			return fmt.Errorf("failed to drop fk constraint for assume role chains: %w", err)
-		}
-		if err := db.Exec("alter table aws_assume_role_chains ADD CONSTRAINT fk_projects FOREIGN KEY(project_id) REFERENCES projects(id);").Error; err != nil {
-			return fmt.Errorf("failed to create fk constraint for assume role chains: %w", err)
-		}
+	// err = db.Transaction(func(tx *pgorm.DB) error {
+	// 	if err := db.Exec("alter table aws_assume_role_chains DROP CONSTRAINT IF EXISTS fk_projects;").Error; err != nil {
+	// 		return fmt.Errorf("failed to drop fk constraint for assume role chains: %w", err)
+	// 	}
+	// 	if err := db.Exec("alter table aws_assume_role_chains ADD CONSTRAINT fk_projects FOREIGN KEY(project_id) REFERENCES projects(id);").Error; err != nil {
+	// 		return fmt.Errorf("failed to create fk constraint for assume role chains: %w", err)
+	// 	}
 
-		if err := db.Exec("alter table aws_assume_role_chains ADD unique (project_id, source_arn, target_arn);").Error; err != nil {
-			return fmt.Errorf("failed to create unique constraint for assume role chains: %w", err)
-		}
-		return nil
-	})
-	if err != nil {
-		logger.Fatal().Err(err).Msg("error updating cluster control plane tables")
-	}
+	// 	if err := db.Exec("alter table aws_assume_role_chains ADD unique (project_id, source_arn, target_arn);").Error; err != nil {
+	// 		return fmt.Errorf("failed to create unique constraint for assume role chains: %w", err)
+	// 	}
+	// 	return nil
+	// })
+	// if err != nil {
+	// 	logger.Fatal().Err(err).Msg("error updating cluster control plane tables")
+	// }
 
-	tx := db.Begin()
+	// tx := db.Begin()
 
-	switch tx.Dialector.Name() {
-	case "sqlite":
-		if err := tx.Raw("PRAGMA schema.locking_mode = EXCLUSIVE").Error; err != nil {
-			tx.Rollback()
+	// switch tx.Dialector.Name() {
+	// case "sqlite":
+	// 	if err := tx.Raw("PRAGMA schema.locking_mode = EXCLUSIVE").Error; err != nil {
+	// 		tx.Rollback()
 
-			logger.Fatal().Err(err).Msg("error acquiring lock on db_migrations")
-			return
-		}
-	case "postgres":
-		if err := tx.Raw("LOCK TABLE db_migrations IN SHARE ROW EXCLUSIVE MODE").Error; err != nil {
-			tx.Rollback()
+	// 		logger.Fatal().Err(err).Msg("error acquiring lock on db_migrations")
+	// 		return
+	// 	}
+	// case "postgres":
+	// 	if err := tx.Raw("LOCK TABLE db_migrations IN SHARE ROW EXCLUSIVE MODE").Error; err != nil {
+	// 		tx.Rollback()
 
-			logger.Fatal().Err(err).Msg("error acquiring lock on db_migrations")
-			return
-		}
-	}
+	// 		logger.Fatal().Err(err).Msg("error acquiring lock on db_migrations")
+	// 		return
+	// 	}
+	// }
 
-	dbMigration := &models.DbMigration{}
+	// dbMigration := &models.DbMigration{}
 
-	if err := tx.Model(&models.DbMigration{}).First(dbMigration).Error; err != nil {
-		if errors.Is(err, pgorm.ErrRecordNotFound) {
-			dbMigration.Version = 0
-		} else {
-			tx.Rollback()
+	// if err := tx.Model(&models.DbMigration{}).First(dbMigration).Error; err != nil {
+	// 	if errors.Is(err, pgorm.ErrRecordNotFound) {
+	// 		dbMigration.Version = 0
+	// 	} else {
+	// 		tx.Rollback()
 
-			logger.Fatal().Err(err).Msg("failed to check for db migration version")
-			return
-		}
-	}
+	// 		logger.Fatal().Err(err).Msg("failed to check for db migration version")
+	// 		return
+	// 	}
+	// }
 
-	latestMigrationVersion := startup_migrations.LatestMigrationVersion
+	// latestMigrationVersion := startup_migrations.LatestMigrationVersion
 
-	if dbMigration.Version < latestMigrationVersion {
-		for ver, fn := range startup_migrations.StartupMigrations {
-			if ver > dbMigration.Version {
-				err := fn(tx, logger)
-				if err != nil {
-					tx.Rollback()
+	// if dbMigration.Version < latestMigrationVersion {
+	// 	for ver, fn := range startup_migrations.StartupMigrations {
+	// 		if ver > dbMigration.Version {
+	// 			err := fn(tx, logger)
+	// 			if err != nil {
+	// 				tx.Rollback()
 
-					logger.Fatal().Err(err).Msg("failed to run startup migration script")
-					return
-				}
-			}
-		}
+	// 				logger.Fatal().Err(err).Msg("failed to run startup migration script")
+	// 				return
+	// 			}
+	// 		}
+	// 	}
 
-		dbMigration.Version = latestMigrationVersion
+	// 	dbMigration.Version = latestMigrationVersion
 
-		if err := tx.Save(dbMigration).Error; err != nil {
-			tx.Rollback()
+	// 	if err := tx.Save(dbMigration).Error; err != nil {
+	// 		tx.Rollback()
 
-			logger.Fatal().Err(err).Msg("failed to update migration version to latest")
-			return
-		}
-	}
+	// 		logger.Fatal().Err(err).Msg("failed to update migration version to latest")
+	// 		return
+	// 	}
+	// }
 
-	tx.Commit()
+	// tx.Commit()
 
-	if shouldRotate, oldKeyStr, newKeyStr := shouldKeyRotate(); shouldRotate {
-		oldKey := [32]byte{}
-		newKey := [32]byte{}
+	// if shouldRotate, oldKeyStr, newKeyStr := shouldKeyRotate(); shouldRotate {
+	// 	oldKey := [32]byte{}
+	// 	newKey := [32]byte{}
 
-		copy(oldKey[:], []byte(oldKeyStr))
-		copy(newKey[:], []byte(newKeyStr))
+	// 	copy(oldKey[:], []byte(oldKeyStr))
+	// 	copy(newKey[:], []byte(newKeyStr))
 
-		err := keyrotate.Rotate(db, &oldKey, &newKey)
-		if err != nil {
-			logger.Fatal().Err(err).Msg("key rotation failed")
-		}
-	}
+	// 	err := keyrotate.Rotate(db, &oldKey, &newKey)
+	// 	if err != nil {
+	// 		logger.Fatal().Err(err).Msg("key rotation failed")
+	// 	}
+	// }
 
-	if shouldPopulateSourceConfigDisplayName() {
-		err := populate_source_config_display_name.PopulateSourceConfigDisplayName(db, logger)
-		if err != nil {
-			logger.Fatal().Err(err).Msg("failed to populate source config display name")
-		}
-	}
+	// if shouldPopulateSourceConfigDisplayName() {
+	// 	err := populate_source_config_display_name.PopulateSourceConfigDisplayName(db, logger)
+	// 	if err != nil {
+	// 		logger.Fatal().Err(err).Msg("failed to populate source config display name")
+	// 	}
+	// }
 
-	if err := InstanceMigrate(db, envConf.DBConf); err != nil {
-		logger.Fatal().Err(err).Msg("vault migration failed")
-	}
+	// if err := InstanceMigrate(db, envConf.DBConf); err != nil {
+	// 	logger.Fatal().Err(err).Msg("vault migration failed")
+	// }
 }
 
 type RotateConf struct {

3df86af855f03f4369e079aabe3e1c6f9f16509c
Justin Rhee <jusrhee@sas.upenn.edu>
2023-04-12 14:57:18 -0400

    standard clickable

--- dashboard/src/shared/themes/standard.ts
+++ dashboard/src/shared/themes/standard.ts
@@ -2,7 +2,7 @@ const theme = {
   bg: "#202227",
   fg: "#27292e",
   clickable: {
-    bg: "#27292e",
+    bg: "linear-gradient(180deg, #26292e, #24272c)",
   },
   text: {
     primary: "#ffffff",

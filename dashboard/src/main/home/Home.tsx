3179a9927c01c1f9e8f410e708556e35bb76783d
jusrhee <justin@porter.run>
2023-04-14 15:19:31 -0400

    Update Home.tsx

--- dashboard/src/main/home/Home.tsx
+++ dashboard/src/main/home/Home.tsx
@@ -263,7 +263,7 @@ const Home: React.FC<Props> = (props) => {
               .then((res) => {
                 const usage = res.data;
                 setUsage(usage);
-                if (usage.exceeded) {
+                if (usage.exceeded && false) {
                   setCurrentModal("UsageWarningModal", { usage });
                 }
               })

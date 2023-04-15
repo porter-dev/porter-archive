a5b99b43ea2a95a48b99fe9dddb21e4148c53aae
Joshua Hoeflich <joshuaharry411@icloud.com>
2023-04-14 20:48:05 -0500

    Prettier

--- dashboard/src/App.test.tsx
+++ dashboard/src/App.test.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
 
 describe("Our application", () => {
   test("Renders without crashing", () => {
     expect(() => render(<App />)).not.toThrow();
-  })
-})
+  });
+});

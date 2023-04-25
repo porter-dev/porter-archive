afd3bc56bf08d93d22d2b6202cbb73492be19d61
Joshua Hoeflich <joshuaharry411@icloud.com>
2023-04-25 10:37:02 -0500

    Fix more warnings and package-lock.json

--- dashboard/jest.config.js
+++ dashboard/jest.config.js
@@ -2,8 +2,9 @@ module.exports = {
   moduleDirectories: ["node_modules", "src"],
   testEnvironment: "jsdom",
   moduleNameMapper: {
-    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|css)$":
+    "\\.(eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|css)$":
       "identity-obj-proxy",
+    "\\.(png|jpeg|jpg|gif|svg)$": "<rootDir>/__mocks__/fileMock.js",
   },
   transformIgnorePatterns: ["node_modules/(?!@porter-dev)"],
 };

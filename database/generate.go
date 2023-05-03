962145acd3272bb5e84c83a73e292c8c925547bc
Stefan McShane <stefanmcshane@users.noreply.github.com>
2023-05-03 19:35:17 -0400

    storing migrate command

--- database/generate.go
+++ database/generate.go
@@ -2,3 +2,4 @@ package database
 
 //go:generate sqlc generate -f sqlc.yaml
 //go:generate atlas migrate diff --dir file://migrations --to file://schema.sql --dev-url "postgresql://porter:porter@localhost:5432/atlas?sslmode=disable" --format "{{ sql . ' ' }}"
+//go:generate atlas migrate lint --dir file://migrations --dev-url "postgresql://porter:porter@localhost:5432/atlas?sslmode=disable" --latest 1

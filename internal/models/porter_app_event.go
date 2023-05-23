267bfd81e5750b6fd74167310a935dcd310d785c
Stefan McShane <stefanmcshane@users.noreply.github.com>
2023-05-23 17:28:03 -0400

    add db migration

---
+++ internal/models/porter_app_event.go
@@ -0,0 +1,34 @@
+package models
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+// PorterAppEvent represents an event that occurs on a Porter stack during a stacks lifecycle.
+type PorterAppEvent struct {
+	gorm.Model
+
+	// ID is a unique identifier for a given event
+	ID uuid.UUID `gorm:"type:uuid;primaryKey" json:"id"`
+	// Status contains the accepted status' of a given event such as SUCCESS, FAILED, PROGRESSING, etc.
+	Status string `json:"status"`
+	// Type represents a supported Porter Stack Event
+	Type string `json:"type"`
+	// TypeExternalSource represents an external event source such as Github, or Gitlab. This is not always required but will commonly be see in build events
+	TypeExternalSource string `json:"type_source,omitempty"`
+	// CreatedAt is the time (UTC) that a given event was created. This should not change
+	CreatedAt time.Time `json:"created_at"`
+	// UpdatedAt is the time (UTC) that an event was last updated. This can occur when an event was created as PROGRESSING, then was marked as SUCCESSFUL for example
+	UpdatedAt time.Time `json:"updated_at"`
+	// PorterAppID is the ID that the given event relates to
+	PorterAppID string `json:"porter_app_id"`
+	Metadata    JSONB  `json:"metadata" sql:"type:jsonb" gorm:"type:jsonb"`
+}
+
+// TableName overrides the table name
+func (PorterAppEvent) TableName() string {
+	return "porter_app_events"
+}

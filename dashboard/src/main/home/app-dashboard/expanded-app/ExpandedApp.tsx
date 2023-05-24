a405b7a38b1dd70b9eb5345145593839fcd91ed6
Justin Rhee <jusrhee@Justins-MacBook-Air.local>
2023-05-24 16:19:40 -0400

    hide activity feed

--- dashboard/src/main/home/app-dashboard/expanded-app/ExpandedApp.tsx
+++ dashboard/src/main/home/app-dashboard/expanded-app/ExpandedApp.tsx
@@ -838,7 +838,7 @@ const ExpandedApp: React.FC<Props> = ({ ...props }) => {
                     ? hasBuiltImage
                       ? [
                         { label: "Overview", value: "overview" },
-                        { label: "Activity", value: "activity" },
+                        // { label: "Activity", value: "activity" },
                         { label: "Events", value: "events" },
                         { label: "Logs", value: "logs" },
                         { label: "Metrics", value: "metrics" },
@@ -853,7 +853,7 @@ const ExpandedApp: React.FC<Props> = ({ ...props }) => {
                       ]
                       : [
                         { label: "Overview", value: "overview" },
-                        { label: "Activity", value: "activity" },
+                        // { label: "Activity", value: "activity" },
                         { label: "Pre-deploy", value: "pre-deploy" },
                         {
                           label: "Environment",
@@ -864,7 +864,7 @@ const ExpandedApp: React.FC<Props> = ({ ...props }) => {
                       ]
                     : [
                       { label: "Overview", value: "overview" },
-                      { label: "Activity", value: "activity" },
+                      // { label: "Activity", value: "activity" },
                       { label: "Events", value: "events" },
                       { label: "Logs", value: "logs" },
                       { label: "Metrics", value: "metrics" },

4fbad4af89abfe170dca8cf88562676132d8156a
Soham Dessai <sd5we@virginia.edu>
2023-05-29 10:13:40 -0400

    Address Changes

--- dashboard/src/main/home/app-dashboard/expanded-app/ExpandedApp.tsx
+++ dashboard/src/main/home/app-dashboard/expanded-app/ExpandedApp.tsx
@@ -54,55 +54,10 @@ import Anser, { AnserJsonEntry } from "anser";
 import dayjs from "dayjs";
 import Modal from "components/porter/Modal";
 import TitleSection from "components/TitleSection";
+import GHALogsModal from "./status/GHALogsModal";
 
 type Props = RouteComponentProps & {};
 
-interface ExpandedIncidentLogsProps {
-  logs: Log[];
-}
-
-const ExpandedIncidentLogs = ({ logs }: ExpandedIncidentLogsProps) => {
-  if (!logs.length) {
-    return (
-      <LogsLoadWrapper>
-        <Loading />
-      </LogsLoadWrapper>
-    );
-  }
-
-  return (
-    <LogsSectionWrapper>
-      <StyledLogsSection>
-        {logs?.map((log, i) => {
-          return (
-            <LogSpan key={[log.lineNumber, i].join(".")}>
-              <span className="line-number">{log.lineNumber}.</span>
-              <span className="line-timestamp">
-                {dayjs(log.timestamp).format("MMM D, YYYY HH:mm:ss")}
-              </span>
-              <LogOuter key={[log.lineNumber, i].join(".")}>
-                {log.line?.map((ansi, j) => {
-                  if (ansi.clearLine) {
-                    return null;
-                  }
-
-                  return (
-                    <LogInnerSpan
-                      key={[log.lineNumber, i, j].join(".")}
-                      ansi={ansi}
-                    >
-                      {ansi.content.replace(/ /g, "\u00a0")}
-                    </LogInnerSpan>
-                  );
-                })}
-              </LogOuter>
-            </LogSpan>
-          );
-        })}
-      </StyledLogsSection>
-    </LogsSectionWrapper>
-  );
-};
 const icons = [
   "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/ruby/ruby-plain.svg",
   "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-plain.svg",
@@ -386,7 +341,10 @@ const ExpandedApp: React.FC<Props> = ({ ...props }) => {
           zip.forEach(async function (relativePath, zipEntry) {
             const fileData = await zip.file(relativePath)?.async("string");
 
-            if (fileData) {
+            if (
+              fileData &&
+              fileData.includes("Run porter-dev/porter-cli-action@v0.1.0")
+            ) {
               const lines = fileData.split("\n");
 
               lines.forEach((line, index) => {
@@ -410,17 +368,6 @@ const ExpandedApp: React.FC<Props> = ({ ...props }) => {
     } catch (error) {
       console.log(error);
     }
-  };
-
-  const renderExpandedEventMessage = () => {
-    if (!logs) {
-      return <Loading />;
-    }
-    return (
-      <>
-        <ExpandedIncidentLogs logs={logs} />
-      </>
-    );
   };
 
   const fetchPorterYamlContent = async (
@@ -946,13 +893,13 @@ const ExpandedApp: React.FC<Props> = ({ ...props }) => {
                       type="error"
                       suffix={
                         <>
-                          <Link
-                            hasunderline
-                            target="_blank"
-                            to={`https://github.com/${appData.app.repo_name}/actions`}
-                          >
-                            Check GitHub Actions
-                          </Link>
+                          <>
+                            <RefreshButton
+                              onClick={() => window.location.reload()}
+                            >
+                              <img src={refresh} /> Refresh
+                            </RefreshButton>
+                          </>
                         </>
                       }
                     >
@@ -963,30 +910,23 @@ const ExpandedApp: React.FC<Props> = ({ ...props }) => {
                           marginBottom: "-20px",
                         }}
                       >
-                        Your Build Was Not Successful
+                        Your build was not successful
                         <Spacer inline width="15px" />
                         <>
-                          <LogsButton onClick={() => setModalVisible(true)}>
+                          <Link
+                            hasunderline
+                            target="_blank"
+                            onClick={() => setModalVisible(true)}
+                          >
                             View Logs
-                            <i
-                              className="material-icons"
-                              style={{ fontSize: "15px", marginLeft: "3px" }}
-                            >
-                              open_in_new
-                            </i>
-                          </LogsButton>
+                          </Link>
                           {modalVisible && (
-                            <Modal
-                              closeModal={() => setModalVisible(false)}
-                              width={"800px"}
-                            >
-                              <TitleSection icon={danger}>
-                                <Text size={16}>
-                                  Logs for {appData.app.name}
-                                </Text>
-                              </TitleSection>
-                              {renderExpandedEventMessage()}
-                            </Modal>
+                            <GHALogsModal
+                              appData={appData}
+                              logs={logs}
+                              modalVisible={false}
+                              setModalVisible={setModalVisible}
+                            />
                           )}
                         </>
                       </div>
@@ -1207,11 +1147,6 @@ const BranchTag = styled.div`
   text-overflow: ellipsis;
 `;
 
-const BranchSection = styled.div`
-  background: ${(props) => props.theme.fg};
-  border: 1px solid #494b4f;
-`;
-
 const SmallIcon = styled.img<{ opacity?: string; height?: string }>`
   height: ${(props) => props.height || "15px"};
   opacity: ${(props) => props.opacity || 1};
@@ -1258,116 +1193,4 @@ const StyledExpandedApp = styled.div`
       opacity: 1;
     }
   }
-`;
-
-const HeaderWrapper = styled.div`
-  position: relative;
-`;
-const LastDeployed = styled.div`
-  font-size: 13px;
-  margin-left: 8px;
-  margin-top: -1px;
-  display: flex;
-  align-items: center;
-  color: #aaaabb66;
-`;
-const Dot = styled.div`
-  margin-right: 16px;
-`;
-const InfoWrapper = styled.div`
-  display: flex;
-  align-items: center;
-  margin-left: 3px;
-  margin-top: 22px;
-`;
-
-const LogsSectionWrapper = styled.div`
-  position: relative;
-`;
-
-const StyledLogsSection = styled.div`
-  margin-top: 20px;
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  position: relative;
-  font-size: 13px;
-  max-height: 400px;
-  border-radius: 8px;
-  border: 1px solid #ffffff33;
-  border-top: none;
-  background: #101420;
-  animation: floatIn 0.3s;
-  animation-timing-function: ease-out;
-  animation-fill-mode: forwards;
-  overflow-y: auto;
-  overflow-wrap: break-word;
-  position: relative;
-  @keyframes floatIn {
-    from {
-      opacity: 0;
-      transform: translateY(10px);
-    }
-    to {
-      opacity: 1;
-      transform: translateY(0px);
-    }
-  }
-`;
-
-const LogSpan = styled.div`
-  font-family: monospace;
-  user-select: text;
-  display: flex;
-  align-items: flex-end;
-  gap: 8px;
-  width: 100%;
-  & > * {
-    padding-block: 5px;
-  }
-  & > .line-timestamp {
-    height: 100%;
-    color: #949effff;
-    opacity: 0.5;
-    font-family: monospace;
-    min-width: fit-content;
-    padding-inline-end: 5px;
-  }
-  & > .line-number {
-    height: 100%;
-    background: #202538;
-    display: inline-block;
-    text-align: right;
-    min-width: 45px;
-    padding-inline-end: 5px;
-    opacity: 0.3;
-    font-family: monospace;
-  }
-`;
-
-const LogOuter = styled.div`
-  display: inline-block;
-  word-wrap: anywhere;
-  flex-grow: 1;
-  font-family: monospace, sans-serif;
-  font-size: 12px;
-`;
-
-const LogInnerSpan = styled.span`
-  font-family: monospace, sans-serif;
-  font-size: 12px;
-  font-weight: ${(props: { ansi: Anser.AnserJsonEntry }) =>
-    props.ansi?.decoration && props.ansi?.decoration == "bold" ? "700" : "400"};
-  color: ${(props: { ansi: Anser.AnserJsonEntry }) =>
-    props.ansi?.fg ? `rgb(${props.ansi?.fg})` : "white"};
-  background-color: ${(props: { ansi: Anser.AnserJsonEntry }) =>
-    props.ansi?.bg ? `rgb(${props.ansi?.bg})` : "transparent"};
-`;
-
-export const ViewLogsWrapper = styled.div`
-  margin-bottom: -15px;
-  margin-top: 15px;
-`;
-const LogsLoadWrapper = styled.div`
-  height: 50px;
 `;

4b6a1e51fb3f057769382d57845da14d2c48e35e
David Townley <davidtownley@Davids-MacBook-Air.local>
2023-06-12 15:04:47 -0400

    add telemetry

--- internal/kubernetes/config.go
+++ internal/kubernetes/config.go
@@ -11,6 +11,8 @@ import (
 	"strings"
 	"time"
 
+	"github.com/porter-dev/porter/internal/telemetry"
+
 	"github.com/bufbuild/connect-go"
 	porterv1 "github.com/porter-dev/api-contracts/generated/go/porter/v1"
 	"github.com/porter-dev/api-contracts/generated/go/porter/v1/porterv1connect"
@@ -63,6 +65,9 @@ func GetDynamicClientOutOfClusterConfig(conf *OutOfClusterConfig) (dynamic.Inter
 
 // GetAgentOutOfClusterConfig creates a new Agent using the OutOfClusterConfig
 func GetAgentOutOfClusterConfig(conf *OutOfClusterConfig) (*Agent, error) {
+	ctx, span := telemetry.NewSpan(context.Background(), "get-agent-out-of-cluster-config")
+	defer span.End()
+
 	if conf.AllowInClusterConnections && conf.Cluster.AuthMechanism == models.InCluster {
 		return GetAgentInClusterConfig(conf.DefaultNamespace)
 	}
@@ -70,26 +75,28 @@ func GetAgentOutOfClusterConfig(conf *OutOfClusterConfig) (*Agent, error) {
 	var restConf *rest.Config
 
 	if conf.Cluster.ProvisionedBy == "CAPI" {
-		rc, err := restConfigForCAPICluster(context.Background(), conf.CAPIManagementClusterClient, *conf.Cluster)
+		telemetry.WithAttributes(span, telemetry.AttributeKV{Key: "capi-provisioned", Value: true})
+
+		rc, err := restConfigForCAPICluster(ctx, conf.CAPIManagementClusterClient, *conf.Cluster)
 		if err != nil {
-			return nil, err
+			return nil, telemetry.Error(ctx, span, err, "error getting rest config for capi cluster")
 		}
 		restConf = rc
 	} else {
 		rc, err := conf.ToRESTConfig()
 		if err != nil {
-			return nil, err
+			return nil, telemetry.Error(ctx, span, err, "error getting rest config")
 		}
 		restConf = rc
 	}
 
 	if restConf == nil {
-		return nil, fmt.Errorf("error getting rest config for cluster %s", conf.Cluster.ProvisionedBy)
+		return nil, telemetry.Error(ctx, span, nil, "error getting rest config for cluster")
 	}
 
 	clientset, err := kubernetes.NewForConfig(restConf)
 	if err != nil {
-		return nil, err
+		return nil, telemetry.Error(ctx, span, err, "error getting new clientset for config")
 	}
 
 	return &Agent{conf, clientset}, nil
@@ -97,38 +104,56 @@ func GetAgentOutOfClusterConfig(conf *OutOfClusterConfig) (*Agent, error) {
 
 // restConfigForCAPICluster gets the kubernetes rest API client for a CAPI cluster
 func restConfigForCAPICluster(ctx context.Context, mgmtClusterConnection porterv1connect.ClusterControlPlaneServiceClient, cluster models.Cluster) (*rest.Config, error) {
+	ctx, span := telemetry.NewSpan(ctx, "rest-config-for-capi-cluster")
+	defer span.End()
+
 	kc, err := kubeConfigForCAPICluster(ctx, mgmtClusterConnection, cluster)
 	if err != nil {
-		return nil, err
+		return nil, telemetry.Error(ctx, span, err, "error getting kubeconfig")
 	}
 
 	rc, err := writeKubeConfigToFileAndRestClient([]byte(kc))
 	if err != nil {
-		return nil, err
+		return nil, telemetry.Error(ctx, span, err, "error writing kubeconfig to file")
 	}
 	return rc, nil
 }
 
 // kubeConfigForCAPICluster grabs the raw kube config for a capi cluster
 func kubeConfigForCAPICluster(ctx context.Context, mgmtClusterConnection porterv1connect.ClusterControlPlaneServiceClient, cluster models.Cluster) (string, error) {
-	kubeconfigResp, err := mgmtClusterConnection.KubeConfigForCluster(context.Background(), connect.NewRequest(
+	ctx, span := telemetry.NewSpan(ctx, "kubeconfig-capi")
+	defer span.End()
+
+	if cluster.ProjectID == 0 {
+		return "", telemetry.Error(ctx, span, nil, "missing project id")
+	}
+	if cluster.ID == 0 {
+		return "", telemetry.Error(ctx, span, nil, "missing cluster id")
+	}
+
+	telemetry.WithAttributes(span,
+		telemetry.AttributeKV{Key: "project-id", Value: cluster.ProjectID},
+		telemetry.AttributeKV{Key: "cluster-id", Value: cluster.ID},
+	)
+
+	kubeconfigResp, err := mgmtClusterConnection.KubeConfigForCluster(ctx, connect.NewRequest(
 		&porterv1.KubeConfigForClusterRequest{
 			ProjectId: int64(cluster.ProjectID),
 			ClusterId: int64(cluster.ID),
 		},
 	))
 	if err != nil {
-		return "", fmt.Errorf("error getting capi config: %w", err)
+		return "", telemetry.Error(ctx, span, err, "error getting capi config")
 	}
 	if kubeconfigResp.Msg == nil {
-		return "", errors.New("no kubeconfig returned for capi cluster")
+		return "", telemetry.Error(ctx, span, nil, "no msg returned for capi cluster")
 	}
 	if kubeconfigResp.Msg.KubeConfig == "" {
-		return "", errors.New("no kubeconfig returned for capi cluster")
+		return "", telemetry.Error(ctx, span, nil, "no kubeconfig returned for capi cluster")
 	}
 	decodedKubeconfig, err := base64.StdEncoding.DecodeString(kubeconfigResp.Msg.KubeConfig)
 	if err != nil {
-		return "", fmt.Errorf("error decoding kubeconfig: %w", err)
+		return "", telemetry.Error(ctx, span, nil, "error decoding capi cluster")
 	}
 	return string(decodedKubeconfig), nil
 }
@@ -207,22 +232,27 @@ type OutOfClusterConfig struct {
 // the result of ToRawKubeConfigLoader, and also adds a custom http transport layer
 // if necessary (required for GCP auth)
 func (conf *OutOfClusterConfig) ToRESTConfig() (*rest.Config, error) {
+	ctx, span := telemetry.NewSpan(context.Background(), "ooc-to-rest-config")
+	defer span.End()
+
 	if conf.Cluster.ProvisionedBy == "CAPI" {
-		rc, err := restConfigForCAPICluster(context.Background(), conf.CAPIManagementClusterClient, *conf.Cluster)
+		telemetry.WithAttributes(span, telemetry.AttributeKV{Key: "capi-provisioned", Value: true})
+
+		rc, err := restConfigForCAPICluster(ctx, conf.CAPIManagementClusterClient, *conf.Cluster)
 		if err != nil {
-			return nil, err
+			return nil, telemetry.Error(ctx, span, err, "error getting config for capi cluster")
 		}
 		return rc, nil
 	}
 
-	cmdConf, err := conf.GetClientConfigFromCluster()
+	cmdConf, err := conf.GetClientConfigFromCluster(ctx)
 	if err != nil {
-		return nil, err
+		return nil, telemetry.Error(ctx, span, err, "error getting client config from cluster")
 	}
 
 	restConf, err := cmdConf.ClientConfig()
 	if err != nil {
-		return nil, err
+		return nil, telemetry.Error(ctx, span, err, "error getting client config")
 	}
 
 	restConf.Timeout = conf.Timeout
@@ -234,7 +264,10 @@ func (conf *OutOfClusterConfig) ToRESTConfig() (*rest.Config, error) {
 // ToRawKubeConfigLoader creates a clientcmd.ClientConfig from the raw kubeconfig found in
 // the OutOfClusterConfig. It does not implement loading rules or overrides.
 func (conf *OutOfClusterConfig) ToRawKubeConfigLoader() clientcmd.ClientConfig {
-	cmdConf, _ := conf.GetClientConfigFromCluster()
+	ctx, span := telemetry.NewSpan(context.Background(), "ooc-to-raw-kubeconfig-loader")
+	defer span.End()
+
+	cmdConf, _ := conf.GetClientConfigFromCluster(ctx)
 
 	return cmdConf
 }
@@ -277,41 +310,51 @@ func (conf *OutOfClusterConfig) ToRESTMapper() (meta.RESTMapper, error) {
 
 // GetClientConfigFromCluster will construct new clientcmd.ClientConfig using
 // the configuration saved within a Cluster model
-func (conf *OutOfClusterConfig) GetClientConfigFromCluster() (clientcmd.ClientConfig, error) {
+func (conf *OutOfClusterConfig) GetClientConfigFromCluster(ctx context.Context) (clientcmd.ClientConfig, error) {
+	ctx, span := telemetry.NewSpan(ctx, "ooc-get-client-config-from-cluster")
+	defer span.End()
+
 	if conf.Cluster == nil {
-		return nil, fmt.Errorf("cluster cannot be nil")
+		return nil, telemetry.Error(ctx, span, nil, "cluster cannot be nil")
 	}
 
 	if conf.Cluster.ProvisionedBy == "CAPI" {
-		rc, err := kubeConfigForCAPICluster(context.Background(), conf.CAPIManagementClusterClient, *conf.Cluster)
+		telemetry.WithAttributes(span, telemetry.AttributeKV{Key: "capi-provisioned", Value: true})
+
+		rc, err := kubeConfigForCAPICluster(ctx, conf.CAPIManagementClusterClient, *conf.Cluster)
 		if err != nil {
-			return nil, err
+			return nil, telemetry.Error(ctx, span, err, "error getting capi kube config")
 		}
 		clientConfig, err := clientcmd.NewClientConfigFromBytes([]byte(rc))
 		if err != nil {
-			return nil, err
+			return nil, telemetry.Error(ctx, span, err, "error getting config from bytes")
 		}
 		rawConfig, err := clientConfig.RawConfig()
 		if err != nil {
-			return nil, err
+			return nil, telemetry.Error(ctx, span, err, "error getting raw config")
 		}
 
 		overrides := &clientcmd.ConfigOverrides{}
 
-		overrides.Context = api.Context{
-			Namespace: conf.DefaultNamespace,
+		if conf.DefaultNamespace != "" {
+			telemetry.WithAttributes(span, telemetry.AttributeKV{Key: "namespace-override", Value: conf.DefaultNamespace})
+			overrides.Context = api.Context{
+				Namespace: conf.DefaultNamespace,
+			}
 		}
 
 		return clientcmd.NewDefaultClientConfig(rawConfig, overrides), nil
 	}
 
 	if conf.Cluster.AuthMechanism == models.Local {
+		telemetry.WithAttributes(span, telemetry.AttributeKV{Key: "local-provisioned", Value: true})
+
 		kubeAuth, err := conf.Repo.KubeIntegration().ReadKubeIntegration(
 			conf.Cluster.ProjectID,
 			conf.Cluster.KubeIntegrationID,
 		)
 		if err != nil {
-			return nil, err
+			return nil, telemetry.Error(ctx, span, err, "error reading kube integration")
 		}
 
 		return clientcmd.NewClientConfigFromBytes(kubeAuth.Kubeconfig)
@@ -319,12 +362,13 @@ func (conf *OutOfClusterConfig) GetClientConfigFromCluster() (clientcmd.ClientCo
 
 	apiConfig, err := conf.CreateRawConfigFromCluster()
 	if err != nil {
-		return nil, err
+		return nil, telemetry.Error(ctx, span, err, "error creating raw config from cluster")
 	}
 
 	overrides := &clientcmd.ConfigOverrides{}
 
 	if conf.DefaultNamespace != "" {
+		telemetry.WithAttributes(span, telemetry.AttributeKV{Key: "namespace-override", Value: conf.DefaultNamespace})
 		overrides.Context = api.Context{
 			Namespace: conf.DefaultNamespace,
 		}

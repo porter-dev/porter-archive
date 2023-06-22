1152957c64e1e598c19fde6ffbbd07d6b7e988cc
Feroze Mohideen <feroze@porter.run>
2023-06-22 17:40:42 -0400

    re-enable delete services (#3185)

--- dashboard/src/main/home/app-dashboard/new-app-flow/ServiceContainer.tsx
+++ dashboard/src/main/home/app-dashboard/new-app-flow/ServiceContainer.tsx
@@ -18,7 +18,6 @@ import ReleaseTabs from "./ReleaseTabs";
 interface ServiceProps {
   service: Service;
   chart?: any;
-  readOnly?: boolean;
   editService: (service: Service) => void;
   deleteService: () => void;
   defaultExpanded: boolean;
@@ -28,7 +27,6 @@ interface ServiceProps {
 const ServiceContainer: React.FC<ServiceProps> = ({
   service,
   chart,
-  readOnly,
   deleteService,
   editService,
   defaultExpanded,
@@ -112,7 +110,7 @@ const ServiceContainer: React.FC<ServiceProps> = ({
           {renderIcon(service)}
           {service.name.trim().length > 0 ? service.name : "New Service"}
         </ServiceTitle>
-        {service.canDelete && !readOnly && (
+        {service.canDelete && (
           <ActionButton onClick={deleteService}>
             <span className="material-icons">delete</span>
           </ActionButton>

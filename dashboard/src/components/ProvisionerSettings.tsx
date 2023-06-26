588daf0c6064a2f13db68596e3c74b4f174b2d79
Feroze Mohideen <feroze@porter.run>
2023-06-26 12:30:11 -0400

    remove console logs on provision (#3200)

--- dashboard/src/components/ProvisionerSettings.tsx
+++ dashboard/src/components/ProvisionerSettings.tsx
@@ -116,7 +116,7 @@ const ProvisionerSettings: React.FC<Props> = (props) => {
     try {
       await api.updateOnboardingStep("<token>", { step }, {});
     } catch (err) {
-      console.log(err);
+      // console.log(err);
     }
   };
 
@@ -381,7 +381,6 @@ const ProvisionerSettings: React.FC<Props> = (props) => {
 
     if (contract?.cluster) {
       let eksValues: EKS = contract.cluster?.eksKind as EKS;
-      console.log(eksValues);
       if (eksValues == null) {
         return
       }

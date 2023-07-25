b121721d12441e4dbb979ba28089717d415fb9b4
Feroze Mohideen <feroze@porter.run>
2023-07-25 14:56:15 -0400

    that would've been bad

--- dashboard/src/shared/types.tsx
+++ dashboard/src/shared/types.tsx
@@ -270,6 +270,8 @@ export interface ProjectType {
   stacks_enabled: boolean;
   simplified_view_enabled: boolean;
   azure_enabled: boolean;
+  helm_values_enabled: boolean;
+  env_group_enabled: boolean;
   roles: {
     id: number;
     kind: string;

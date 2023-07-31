21e59f61ef1b650cefa19b91fec82cfed748c5fc
Feroze Mohideen <feroze@porter.run>
2023-07-31 12:48:24 -0400

    add comment

--- internal/models/porter_app.go
+++ internal/models/porter_app.go
@@ -53,6 +53,8 @@ func (a *PorterApp) ToPorterAppType() *types.PorterApp {
 }
 
 // ToPorterAppTypeWithRevision generates an external types.PorterApp with the latest helm revision number to be shared over REST
+// this is a bit hacky since it mixes db information with non-db information in the response
+// TODO: rethink this when we switch to versioned porter.yamls
 func (a *PorterApp) ToPorterAppTypeWithRevision(revision int) *types.PorterApp {
 	return &types.PorterApp{
 		ID:                 a.ID,

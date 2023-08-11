38f671d10a7532d2e7c2fd9aecf180577d7c41f6
Soham Dessai <sd5we@virginia.edu>
2023-08-11 12:04:32 -0400

    Update Title

--- dashboard/src/main/home/add-on-dashboard/ExpandedTemplate.tsx
+++ dashboard/src/main/home/add-on-dashboard/ExpandedTemplate.tsx
@@ -70,33 +70,37 @@ const ExpandedTemplate: React.FC<Props> = ({
   return (
     <StyledExpandedTemplate>
       <Container row spaced>
-        <TitleContainer >
+        <Container row>
           <Icon src={hardcodedIcons[currentTemplate.name] || currentTemplate.icon} />
 
-          <TitleContainer>
-            <Text size={16}>
-              <Capitalize>
-                {hardcodedNames[currentTemplate.name] || currentTemplate.name}
-              </Capitalize>
-            </Text>
-            <Text color={"helper"} size={12}>
+          <TitleContainer >
+            <Container row spaced>
+              <Text size={20}>
+
+                <Capitalize>
+                  {hardcodedNames[currentTemplate.name] || currentTemplate.name}
+                </Capitalize>
+              </Text>
+              {Object.keys(DISPLAY_TAGS_MAP).map(tagKey => (
+                currentTemplate.tags?.includes(tagKey) &&
+                <Tag
+                  style={{ background: DISPLAY_TAGS_MAP[tagKey].color }}
+                >
+                  {DISPLAY_TAGS_MAP[tagKey].label}
+                </Tag>))}
+            </Container>
+            <Text color={"helper"} size={10}>
               {currentTemplate.description}
             </Text>
           </TitleContainer>
-        </TitleContainer>
+        </Container>
 
         <Button onClick={() => proceed(form)}>
           <AddI className="material-icons">add</AddI>
           Deploy add-on
         </Button>
       </Container>
-      {/* {Object.keys(DISPLAY_TAGS_MAP).map(tagKey => (
-        currentTemplate.tags?.includes(tagKey) &&
-        <Tag
-          style={{ background: DISPLAY_TAGS_MAP[tagKey].color }}
-        >
-          {DISPLAY_TAGS_MAP[tagKey].label}
-        </Tag>))} */}
+
       <Spacer height="15px" />
       {
         isLoading ? <Loading offset="-150px" /> : (
@@ -119,76 +123,74 @@ const ExpandedTemplate: React.FC<Props> = ({
 export default ExpandedTemplate;
 
 const MarkdownWrapper = styled.div`
-  font-size: 13px;
-  line-height: 1.5;
-  color: #aaaabb;
+        font-size: 13px;
+        line-height: 1.5;
+        color: #aaaabb;
   > div {
     > h1 {
-      color: ${({ theme }) => theme.text.primary};
-      font-size: 16px;
-      font-weight: 400;
+          color: ${({ theme }) => theme.text.primary};
+        font-size: 16px;
+        font-weight: 400;
     }
     > h2 {
-      color: ${({ theme }) => theme.text.primary};
-      font-size: 16px;
-      font-weight: 400;
+          color: ${({ theme }) => theme.text.primary};
+        font-size: 16px;
+        font-weight: 400;
     }
     > h3 {
-      color: ${({ theme }) => theme.text.primary};
-      font-size: 16px;
-      font-weight: 400;
+          color: ${({ theme }) => theme.text.primary};
+        font-size: 16px;
+        font-weight: 400;
     }
   }
-  padding-bottom: 80px;
-`;
+        padding-bottom: 80px;
+        `;
 
 const Icon = styled.img`
-  height: 46px;
-  margin-right: 15px;
-  border-radius: 10px;            
-  background: ${(props) => props.theme.fg};
-  padding: 10px;              
-  box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.1);
-`;
+        height: 46px;
+        margin-right: 15px;
+        border-radius: 10px;
+        background: ${(props) => props.theme.fg};
+        padding: 10px;
+        box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.1);
+        `;
 
 const Capitalize = styled.span`
-  text-transform: capitalize;
-`;
+        text-transform: capitalize;
+        `;
 
 const I = styled.i`
-  color: white;
-  font-size: 16px;
-`;
+        color: white;
+        font-size: 16px;
+        `;
 
 const AddI = styled.i`
-  color: white;
-  font-size: 14px;
-  display: flex;
-  align-items: center;
-  margin-right: 10px;
-  justify-content: center;
-`;
+        color: white;
+        font-size: 14px;
+        display: flex;
+        align-items: center;
+        margin-right: 10px;
+        justify-content: center;
+        `;
 
 const StyledExpandedTemplate = styled.div`
-  width: 100%;
-  height: 100%;
-`;
+        width: 100%;
+        height: 100%;
+        `;
 
 const Tag = styled.div<{ size?: string, right?: string, bottom?: string, left?: string }>`
-  position: absolute;
-  bottom: ${props => props.bottom || 'auto'};
-  left: ${props => props.left || 'auto'};
-  right: ${props => props.right || 'auto'};
-  font-size: 10px;
-  background: #480ca8;
-  padding: 5px;
-  margin-left: 2px;
-  border-radius: 4px; 
-  opacity: 0.7;
-`;
+          margin-left: 15px;
+          font-size: 10px;
+          background: #480ca8;
+          padding: 5px;
+          border-radius: 4px;
+          opacity: 0.7;
+          `;
 
 const TitleContainer = styled.div`
-display: flex;
-flex-direction: row;
-align-items: center;  /* This will vertically align the items in the center */
-`;
+          display: flex;
+          flex-direction: column;
+          align-items: start;
+          justify-content: center;
+          max-width: 500px;
+        `;

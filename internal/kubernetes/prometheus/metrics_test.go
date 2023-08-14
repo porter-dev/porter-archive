cab73fff06567bc2fff1c0b0887509dd1e3dd969
Jose Diaz-Gonzalez <email@josediazgonzalez.com>
2023-08-14 15:49:05 -0400

    feat: add support for exposing nginx response codes via metrics endpoint
    The metrics endpoint now supports an 'nginx:status' metric with a required nginx_status_level (int 1-5) querystring argument and 'kind=Ingress'. This will return nginx_ingress_controller_requests scoped to the specified app, which can then be graphed in the frontend.

---
+++ internal/kubernetes/prometheus/metrics_test.go
@@ -0,0 +1,84 @@
+package prometheus
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_getNginxStatusQuery(t *testing.T) {
+	type input struct {
+		opts           *QueryOpts
+		selectionRegex string
+	}
+	type output struct {
+		query string
+		err   error
+	}
+
+	var tests = []struct {
+		name     string
+		input    input
+		expected struct {
+			query string
+			err   error
+		}
+	}{
+		// the table itself
+		{
+			"missing status level",
+			input{
+				&QueryOpts{},
+				"process-app-web",
+			},
+			output{
+				"",
+				errors.New("invalid nginx status level specified"),
+			},
+		},
+		{
+			"invalid status level",
+			input{
+				&QueryOpts{
+					NginxStatusLevel: 18,
+				},
+				"process-app-web",
+			},
+			output{
+				"",
+				errors.New("invalid nginx status level specified"),
+			},
+		},
+		{
+			"valid status level",
+			input{
+				&QueryOpts{
+					Name:             "process-app-web",
+					Namespace:        "app-namespace",
+					NginxStatusLevel: 2,
+				},
+				"process-app-web",
+			},
+			output{
+				`round(sum by (ingress)(irate(nginx_ingress_controller_requests{exported_namespace=~"app-namespace",ingress="process-app-web",service="process-app-web",status=~"2.."}[5m])), 0.001)`,
+				nil,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			query, err := getNginxStatusQuery(tt.input.opts, tt.input.selectionRegex)
+			if query != tt.expected.query {
+				t.Errorf("got %s, want %s", query, tt.expected.query)
+			}
+			if tt.expected.err == nil {
+				assert.Nil(t, err, "expected nil, got %v", err)
+			} else {
+				if assert.Error(t, err) {
+					assert.Equal(t, tt.expected.err, err)
+				}
+			}
+		})
+	}
+}

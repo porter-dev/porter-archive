ea43585360c9e85054662cd4936df2d37a9fdab9
ianedwards <ianedwards559@gmail.com>
2023-08-15 16:32:23 -0400

    revision and target types for validate apply (#3381)

---
+++ internal/models/deployment_target.go
@@ -0,0 +1,26 @@
+package models
+
+import (
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+// DeploymentTarget represents a deployment target on a given cluster
+type DeploymentTarget struct {
+	gorm.Model
+
+	// ID is a UUID for the Revision
+	ID uuid.UUID `gorm:"type:uuid;primaryKey" json:"id"`
+
+	// ClusterID is the ID of the cluster that is being targeted.
+	ClusterID int `json:"cluster_id"`
+
+	// ProjectID is the ID of the project that the target belongs to.
+	ProjectID int `json:"project_id"`
+
+	// Selector is the identifier to target.
+	Selector string `json:"selector"`
+
+	// SelectorType is the kind of selector (i.e. NAMESPACE or LABEL).
+	SelectorType string `json:"selector_type"`
+}

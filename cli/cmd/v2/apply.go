799c73d00c1d0d7f81d585ba39aac51036dbdc44
d-g-town <66391417+d-g-town@users.noreply.github.com>
2023-08-18 08:37:47 -0400

    apply validate yaml (#3391)
    Co-authored-by: David Townley <davidtownley@Davids-MacBook-Air.local>

--- cli/cmd/v2/apply.go
+++ cli/cmd/v2/apply.go
@@ -3,12 +3,12 @@ package v2
 import (
 	"context"
 	"encoding/base64"
+	"errors"
 	"fmt"
 	"os"
 	"path/filepath"
 
 	"github.com/fatih/color"
-	"github.com/porter-dev/api-contracts/generated/go/helpers"
 	porterv1 "github.com/porter-dev/api-contracts/generated/go/porter/v1"
 	api "github.com/porter-dev/porter/api/client"
 	"github.com/porter-dev/porter/cli/cmd/config"
@@ -16,8 +16,6 @@ import (
 
 // Apply implements the functionality of the `porter apply` command for validate apply v2 projects
 func Apply(ctx context.Context, cliConf *config.CLIConfig, client *api.Client, porterYamlPath string) error {
-	appProto := &porterv1.PorterApp{}
-
 	if len(porterYamlPath) == 0 {
 		return fmt.Errorf("porter yaml is empty")
 	}
@@ -29,26 +27,46 @@ func Apply(ctx context.Context, cliConf *config.CLIConfig, client *api.Client, p
 
 	b64YAML := base64.StdEncoding.EncodeToString(porterYaml)
 
-	resp, err := client.ParseYAML(ctx, cliConf.Project, cliConf.Cluster, b64YAML)
+	parseResp, err := client.ParseYAML(ctx, cliConf.Project, cliConf.Cluster, b64YAML)
 	if err != nil {
 		return fmt.Errorf("error calling parse yaml endpoint: %w", err)
 	}
 
-	if resp.B64AppProto == "" {
-		return fmt.Errorf("b64 app proto is empty")
+	if parseResp.B64AppProto == "" {
+		return errors.New("b64 app proto is empty")
 	}
 
-	decoded, err := base64.StdEncoding.DecodeString(resp.B64AppProto)
+	targetResp, err := client.DefaultDeploymentTarget(ctx, cliConf.Project, cliConf.Cluster)
 	if err != nil {
-		return fmt.Errorf("unable to decode b64 app: %w", err)
+		return fmt.Errorf("error calling default deployment target endpoint: %w", err)
 	}
 
-	err = helpers.UnmarshalContractObject(decoded, appProto)
+	if targetResp.DeploymentTargetID == "" {
+		return errors.New("deployment target id is empty")
+	}
+
+	validateResp, err := client.ValidatePorterApp(ctx, cliConf.Project, cliConf.Cluster, parseResp.B64AppProto, targetResp.DeploymentTargetID)
 	if err != nil {
-		return fmt.Errorf("unable to unmarshal app: %w", err)
+		return fmt.Errorf("error calling validate endpoint: %w", err)
 	}
 
-	color.New(color.FgGreen).Printf("Successfully parsed Porter YAML file %+v\n", appProto) // nolint:errcheck,gosec
+	if validateResp.ValidatedBase64AppProto == "" {
+		return errors.New("validated b64 app proto is empty")
+	}
+
+	applyResp, err := client.ApplyPorterApp(ctx, cliConf.Project, cliConf.Cluster, validateResp.ValidatedBase64AppProto, targetResp.DeploymentTargetID)
+	if err != nil {
+		return fmt.Errorf("error calling apply endpoint: %w", err)
+	}
+
+	if applyResp.AppRevisionId == "" {
+		return errors.New("app revision id is empty")
+	}
+	if applyResp.CLIAction == porterv1.EnumCLIAction_ENUM_CLI_ACTION_UNSPECIFIED {
+		return errors.New("cli action is unknown")
+	}
+
+	color.New(color.FgGreen).Printf("Successfully applied Porter YAML as revision %v, next action: %v\n", applyResp.AppRevisionId, applyResp.CLIAction) // nolint:errcheck,gosec
 
 	return nil
 }

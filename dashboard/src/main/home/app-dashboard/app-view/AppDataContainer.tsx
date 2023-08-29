5fe8745ddfc36b967b9abc591f9f5a2a62f47f9f
ianedwards <ianedwards559@gmail.com>
2023-08-29 15:55:24 -0400

    update and delete apps (#3450)

--- dashboard/src/main/home/app-dashboard/app-view/AppDataContainer.tsx
+++ dashboard/src/main/home/app-dashboard/app-view/AppDataContainer.tsx
@@ -14,6 +14,10 @@ import TabSelector from "components/TabSelector";
 import { useHistory } from "react-router";
 import { match } from "ts-pattern";
 import Overview from "./tabs/Overview";
+import { useAppValidation } from "lib/hooks/useAppValidation";
+import api from "shared/api";
+import { useQueryClient } from "@tanstack/react-query";
+import Settings from "./tabs/Settings";
 
 // commented out tabs are not yet implemented
 // will be included as support is available based on data from app revisions rather than helm releases
@@ -39,6 +43,7 @@ type AppDataContainerProps = {
 
 const AppDataContainer: React.FC<AppDataContainerProps> = ({ tabParam }) => {
   const history = useHistory();
+  const queryClient = useQueryClient();
   const {
     porterApp,
     latestProto,
@@ -48,6 +53,9 @@ const AppDataContainer: React.FC<AppDataContainerProps> = ({ tabParam }) => {
     deploymentTargetId,
     servicesFromYaml,
   } = useLatestRevision();
+  const { validateApp } = useAppValidation({
+    deploymentTargetID: deploymentTargetId,
+  });
 
   const currentTab = useMemo(() => {
     if (tabParam && validTabs.includes(tabParam as ValidTab)) {
@@ -86,7 +94,32 @@ const AppDataContainer: React.FC<AppDataContainerProps> = ({ tabParam }) => {
       source: latestSource,
     },
   });
-  const { reset } = porterAppFormMethods;
+  const { reset, handleSubmit } = porterAppFormMethods;
+
+  const onSubmit = handleSubmit(async (data) => {
+    try {
+      const validatedAppProto = await validateApp(data);
+      await api.applyApp(
+        "<token>",
+        {
+          b64_app_proto: btoa(validatedAppProto.toJsonString()),
+          deployment_target_id: deploymentTargetId,
+        },
+        {
+          project_id: projectId,
+          cluster_id: clusterId,
+        }
+      );
+
+      await queryClient.invalidateQueries([
+        "getLatestRevision",
+        projectId,
+        clusterId,
+        deploymentTargetId,
+        porterApp.name,
+      ]);
+    } catch (err) {}
+  });
 
   useEffect(() => {
     if (servicesFromYaml) {
@@ -95,32 +128,38 @@ const AppDataContainer: React.FC<AppDataContainerProps> = ({ tabParam }) => {
         source: latestSource,
       });
     }
-  }, [servicesFromYaml]);
+  }, [servicesFromYaml, currentTab]);
 
   return (
     <FormProvider {...porterAppFormMethods}>
-      <RevisionsList
-        latestRevisionNumber={latestRevision.revision_number}
-        deploymentTargetId={deploymentTargetId}
-        projectId={projectId}
-        clusterId={clusterId}
-        appName={porterApp.name}
-        sourceType={latestSource.type}
-      />
-      <Spacer y={1} />
-      <TabSelector
-        noBuffer
-        options={[{ label: "Overview", value: "overview" }]}
-        currentTab={currentTab}
-        setCurrentTab={() => {
-          history.push(`/apps/${porterApp.name}/${currentTab}`);
-        }}
-      />
-      <Spacer y={1} />
-      {match(currentTab)
-        .with("overview", () => <Overview />)
-        .otherwise(() => null)}
-      <Spacer y={2} />
+      <form onSubmit={onSubmit}>
+        <RevisionsList
+          latestRevisionNumber={latestRevision.revision_number}
+          deploymentTargetId={deploymentTargetId}
+          projectId={projectId}
+          clusterId={clusterId}
+          appName={porterApp.name}
+          sourceType={latestSource.type}
+        />
+        <Spacer y={1} />
+        <TabSelector
+          noBuffer
+          options={[
+            { label: "Overview", value: "overview" },
+            { label: "Settings", value: "settings" },
+          ]}
+          currentTab={currentTab}
+          setCurrentTab={(tab) => {
+            history.push(`/apps/${porterApp.name}/${tab}`);
+          }}
+        />
+        <Spacer y={1} />
+        {match(currentTab)
+          .with("overview", () => <Overview />)
+          .with("settings", () => <Settings />)
+          .otherwise(() => null)}
+        <Spacer y={2} />
+      </form>
     </FormProvider>
   );
 };

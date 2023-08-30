50e04068ff6f8478579481efff501b34fc56e55a
Ian Edwards <ianedwards559@gmail.com>
2023-08-30 16:42:32 -0400

    improve reivsions list and bug fixes

--- dashboard/src/main/home/app-dashboard/app-view/tabs/Overview.tsx
+++ dashboard/src/main/home/app-dashboard/app-view/tabs/Overview.tsx
@@ -36,7 +36,6 @@ const Overview: React.FC = () => {
           <Text size={16}>Pre-deploy job</Text>
           <Spacer y={0.5} />
           <ServiceList
-            limitOne={true}
             addNewText={"Add a new pre-deploy job"}
             prePopulateService={deserializeService({
               service: defaultSerialized({

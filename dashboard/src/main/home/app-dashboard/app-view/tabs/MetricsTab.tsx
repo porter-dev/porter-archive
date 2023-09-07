064f02653ab2588ee90aa224a0da64c18d90fa24
David Townley <davidtownley@Davids-MacBook-Air.local>
2023-09-07 13:08:14 -0400

    remove old code

--- dashboard/src/main/home/app-dashboard/app-view/tabs/MetricsTab.tsx
+++ dashboard/src/main/home/app-dashboard/app-view/tabs/MetricsTab.tsx
@@ -1,13 +1,5 @@
 import React, { useMemo } from "react";
-import { useFormContext, useFormState } from "react-hook-form";
-import Logs from "../../validate-apply/logs/Logs"
-import {Service} from "../../validate-apply/metrics/MetricsSection"
-import {
-    defaultSerialized,
-    deserializeService, ServiceWithName,
-} from "lib/porter-apps/services";
-import Error from "components/porter/Error";
-import Button from "components/porter/Button";
+import {ServiceWithName} from "lib/porter-apps/services";
 import { useLatestRevision } from "../LatestRevisionContext";
 import MetricsSection from "../../validate-apply/metrics/MetricsSection";
 
@@ -21,49 +13,6 @@ const MetricsTab: React.FC = () => {
             service: service,
         }
     })
-
-    // const services = serviceNames.map((serviceName) => {
-    //     const service = latestProto.services[serviceName];
-    //
-    //     const serviceValues: Service  = {
-    //         name: serviceName,
-    //         kind: "",
-    //         ingress_enabled: service.config.case === "webConfig" && !service.config.value.private,
-    //         absolute_name: service.absoluteName === "" ? undefined : service.absoluteName,
-    //     }
-    //
-    //     if (service.config.case === "webConfig") {
-    //         serviceValues.kind = "web"
-    //         if (service.config.value.autoscaling != null && service.config.value.autoscaling.enabled) {
-    //             const autoscaling = service.config.value.autoscaling
-    //             serviceValues.autoscaling = {
-    //                 minReplicas: autoscaling.minInstances,
-    //                 maxReplicas: autoscaling.maxInstances,
-    //                 targetCPUUtilizationPercentage: autoscaling.cpuThresholdPercent,
-    //                 targetMemoryUtilizationPercentage: autoscaling.memoryThresholdPercent,
-    //             }
-    //         }
-    //     }
-    //
-    //     if (service.config.case === "workerConfig") {
-    //         serviceValues.kind = "worker"
-    //         if (service.config.value.autoscaling != null && service.config.value.autoscaling.enabled) {
-    //             const autoscaling = service.config.value.autoscaling
-    //             serviceValues.autoscaling = {
-    //                 minReplicas: autoscaling.minInstances,
-    //                 maxReplicas: autoscaling.maxInstances,
-    //                 targetCPUUtilizationPercentage: autoscaling.cpuThresholdPercent,
-    //                 targetMemoryUtilizationPercentage: autoscaling.memoryThresholdPercent,
-    //             }
-    //         }
-    //     }
-    //
-    //     if (service.config.case === "jobConfig") {
-    //         serviceValues.kind = "job"
-    //     }
-    //
-    //     return serviceValues
-    // })
 
     return (
         <>

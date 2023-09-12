52da13d02ab83d5ac0338bd6c42e53f1e74cec54
Ian Edwards <ianedwards559@gmail.com>
2023-09-12 14:19:55 -0400

    move creation of porter domain to apply endpoint

---
+++ internal/porter_app/create_subdomain.go
@@ -0,0 +1,75 @@
+package porter_app
+
+import (
+	"context"
+
+	"github.com/porter-dev/porter/internal/integrations/powerdns"
+	"github.com/porter-dev/porter/internal/kubernetes"
+	"github.com/porter-dev/porter/internal/kubernetes/domain"
+	"github.com/porter-dev/porter/internal/repository"
+	"github.com/porter-dev/porter/internal/telemetry"
+)
+
+type CreatePorterSubdomainInput struct {
+	AppName    string
+	RootDomain string
+	KubernetesAgent            *kubernetes.Agent
+	PowerDNSClient      *powerdns.Client
+	DNSRecordRepository repository.DNSRecordRepository
+}
+
+func CreatePorterSubdomain(ctx context.Context, input CreatePorterSubdomainInput) (string, error) {
+	ctx, span := telemetry.NewSpan(ctx, "create-porter-subdomain")
+	defer span.End()
+
+	var createdDomain string
+
+	if input.KubernetesAgent == nil {
+		return "", telemetry.Error(ctx, span, nil, "k8s agent is nil")
+	}
+	if input.PowerDNSClient == nil {
+		return "", telemetry.Error(ctx, span, nil, "powerdns client is nil")
+	}
+	if input.AppName == "" {
+		return "", telemetry.Error(ctx, span, nil, "app name is empty")
+	}
+	if input.RootDomain == "" {
+		return "", telemetry.Error(ctx, span, nil, "root domain is empty")
+	}
+
+	endpoint, found, err := domain.GetNGINXIngressServiceIP(input.KubernetesAgent.Clientset)
+	if err != nil {
+		return createdDomain, telemetry.Error(ctx, span, err, "error getting nginx ingress service ip")
+	}
+	if !found {
+		return createdDomain, telemetry.Error(ctx, span, nil, "target cluster does not have nginx ingress")
+	}
+
+	createDomainConf := domain.CreateDNSRecordConfig{
+		ReleaseName: input.AppName,
+		RootDomain:  input.RootDomain,
+		Endpoint:    endpoint,
+	}
+
+	record := createDomainConf.NewDNSRecordForEndpoint()
+
+	record, err = input.DNSRecordRepository.CreateDNSRecord(record)
+
+	if err != nil {
+		return createdDomain, telemetry.Error(ctx, span, nil, "error creating dns record")
+	}
+	if record == nil {
+		return createdDomain, telemetry.Error(ctx, span, nil, "dns record is nil")
+	}
+
+	_record := domain.DNSRecord(*record)
+
+	err = _record.CreateDomain(input.PowerDNSClient)
+	if err != nil {
+		return createdDomain, telemetry.Error(ctx, span, err, "error creating domain")
+	}
+
+	createdDomain = _record.Hostname
+
+	return createdDomain, nil
+}

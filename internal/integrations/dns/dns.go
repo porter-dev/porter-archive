e855b9f05ca28fbcca7a224d9dcdb11e29a8373a
jose-fully-ported <141160579+jose-fully-ported@users.noreply.github.com>
2023-09-15 16:46:18 -0400

    POR-1703: add support for cloudflare dns (#3559)

--- internal/integrations/dns/dns.go
+++ internal/integrations/dns/dns.go
@@ -1,19 +1,43 @@
 package dns
 
-import "github.com/porter-dev/porter/internal/integrations/powerdns"
+// RecordType strongly types dns record types
+type RecordType int
+
+const (
+	// RecordType_A represents a DNS RecordType_A record
+	RecordType_A RecordType = iota
+
+	// RecordType_CNAME represents a DNS RecordType_CNAME record
+	RecordType_CNAME
+)
+
+// WrappedClient is an interface describing a wrapper
+// around a particular dns implementation
+type WrappedClient interface {
+	CreateARecord(record Record) error
+	CreateCNAMERecord(record Record) error
+}
 
 // Client wraps the underlying powerdns client
 // providing a stable api around interacting with DNS
 type Client struct {
-	Client *powerdns.Client
+	Client WrappedClient
 }
 
-// CreateARecord creates a new A record
-func (c Client) CreateARecord(value, hostname string) error {
-	return c.Client.CreateARecord(value, hostname)
+// Record describes a specific DNS record to create
+// and can include implementation-specific attributes
+type Record struct {
+	Type       RecordType
+	Name       string
+	RootDomain string
+	Value      string
 }
 
-// CreateCNAMERecord creates a new CNAME record
-func (c Client) CreateCNAMERecord(value, hostname string) error {
-	return c.Client.CreateCNAMERecord(value, hostname)
+// CreateRecord creates a new dns record
+func (c Client) CreateRecord(record Record) error {
+	if record.Type == RecordType_A {
+		return c.Client.CreateARecord(record)
+	}
+
+	return c.Client.CreateCNAMERecord(record)
 }

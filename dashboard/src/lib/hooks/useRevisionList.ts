e34ed7d351f65f6ea4d2146496659470c70b695b
Feroze Mohideen <feroze@porter.run>
2023-09-19 10:59:39 -0400

    [POR-1721] Add status footers to porter yaml v2 (#3584)

--- dashboard/src/lib/hooks/useRevisionList.ts
+++ dashboard/src/lib/hooks/useRevisionList.ts
@@ -3,12 +3,12 @@ import { useContext, useEffect, useState } from "react";
 import { Context } from "shared/Context";
 import api from "shared/api";
 import { z } from "zod";
-import {AppRevision, appRevisionValidator} from "../revisions/types";
-import {useLatestRevision} from "../../main/home/app-dashboard/app-view/LatestRevisionContext";
+import { AppRevision, appRevisionValidator } from "../revisions/types";
+import { useLatestRevision } from "../../main/home/app-dashboard/app-view/LatestRevisionContext";
 
 export function useRevisionList(appName: string, deploymentTargetId: string) {
   const { currentProject, currentCluster } = useContext(Context);
-  const {latestRevision} = useLatestRevision();
+  const { latestRevision } = useLatestRevision();
 
   const [
     revisionList,
@@ -19,29 +19,33 @@ export function useRevisionList(appName: string, deploymentTargetId: string) {
     return [];
   }
 
-  const {data} = useQuery(
-      ["listAppRevisions", currentProject.id, currentCluster.id, appName, deploymentTargetId, latestRevision],
-      async () => {
-        const res = await api.listAppRevisions(
-            "<token>",
-            {
-              deployment_target_id: deploymentTargetId,
-            },
-            {
-              project_id: currentProject.id,
-              cluster_id: currentCluster.id,
-              porter_app_name: appName,
-            }
-        );
+  const { data } = useQuery(
+    ["listAppRevisions", currentProject.id, currentCluster.id, appName, deploymentTargetId, latestRevision],
+    async () => {
+      const res = await api.listAppRevisions(
+        "<token>",
+        {
+          deployment_target_id: deploymentTargetId,
+        },
+        {
+          project_id: currentProject.id,
+          cluster_id: currentCluster.id,
+          porter_app_name: appName,
+        }
+      );
 
-        const revisions = await z
-            .object({
-              app_revisions: z.array(appRevisionValidator),
-            })
-            .parseAsync(res.data);
+      const revisions = await z
+        .object({
+          app_revisions: z.array(appRevisionValidator),
+        })
+        .parseAsync(res.data);
 
-        return revisions;
-      }
+      return revisions;
+    },
+    {
+      enabled: !!currentProject && !!currentCluster,
+      refetchInterval: 5000,
+    }
   );
 
   useEffect(() => {
@@ -54,10 +58,10 @@ export function useRevisionList(appName: string, deploymentTargetId: string) {
 }
 
 export function useRevisionIdToNumber(appName: string, deploymentTargetId: string) {
-    const revisionList = useRevisionList(appName, deploymentTargetId);
-    const revisionIdToNumber: Record<string, number> = Object.fromEntries(revisionList.map(r => ([r.id, r.revision_number ])))
+  const revisionList = useRevisionList(appName, deploymentTargetId);
+  const revisionIdToNumber: Record<string, number> = Object.fromEntries(revisionList.map(r => ([r.id, r.revision_number])))
 
-    return revisionIdToNumber;
+  return revisionIdToNumber;
 }
 
 export function useLatestRevisionNumber(appName: string, deploymentTargetId: string) {

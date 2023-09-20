16b8bc3772d891382f1972dc3e8b6f9dce0cfca9
Feroze Mohideen <feroze@porter.run>
2023-09-20 16:40:05 -0400

    use date utility

--- dashboard/src/main/home/app-dashboard/validate-apply/jobs/utils.ts
+++ dashboard/src/main/home/app-dashboard/validate-apply/jobs/utils.ts
@@ -1,5 +1,6 @@
 import { JobRun } from "lib/hooks/useJobs";
 import { timeFrom } from "shared/string_utils";
+import { differenceInSeconds, formatDuration } from 'date-fns';
 
 export const ranFor = (start: string, end?: string | number) => {
     const duration = timeFrom(start, end);
@@ -17,30 +18,7 @@ export const getDuration = (jobRun: JobRun): string => {
 
     const endTimeStamp = jobRun.status.completionTime ? new Date(jobRun.status.completionTime).getTime() : Date.now()
 
-    const timeDifferenceMilliseconds = endTimeStamp - startTimeStamp;
+    const timeDifferenceInSeconds = differenceInSeconds(endTimeStamp, startTimeStamp);
 
-    const seconds = Math.floor(timeDifferenceMilliseconds / 1000);
-    const weeks = Math.floor(seconds / 604800);
-    const remainingDays = Math.floor((seconds % 604800) / 86400);
-    const remainingHours = Math.floor((seconds % 86400) / 3600);
-    const remainingMinutes = Math.floor((seconds % 3600) / 60);
-    const remainingSeconds = seconds % 60;
-
-    if (weeks > 0) {
-        return `${weeks}w ${remainingDays}d`;
-    }
-
-    if (remainingDays > 0) {
-        return `${remainingDays}d ${remainingHours}h`;
-    }
-
-    if (remainingHours > 0) {
-        return `${remainingHours}h ${remainingMinutes}m`;
-    }
-
-    if (remainingMinutes > 0) {
-        return `${remainingMinutes}m ${remainingSeconds}s`;
-    }
-
-    return `${remainingSeconds}s`;
+    return formatDuration({ seconds: timeDifferenceInSeconds });
 };

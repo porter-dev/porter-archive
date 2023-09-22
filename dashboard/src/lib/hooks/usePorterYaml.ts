55f6aba13131c560a9c51dc42ebbaea8367ee61f
Ian Edwards <ianedwards559@gmail.com>
2023-09-22 10:55:08 -0400

    always return validated build

--- dashboard/src/lib/hooks/usePorterYaml.ts
+++ dashboard/src/lib/hooks/usePorterYaml.ts
@@ -82,7 +82,10 @@ export const usePorterYaml = ({
         Boolean(source.git_repo_name) &&
         Boolean(source.git_branch),
       retry: (_failureCount, error) => {
-        if (error.response.data?.error?.includes("404")) {
+        if (
+          error.response.data?.error?.includes("404") ||
+          error.response.data?.error?.includes("not found")
+        ) {
           setPorterYamlFound(false);
           return false;
         }
@@ -125,6 +128,7 @@ export const usePorterYaml = ({
           overrides: proto,
           useDefaults,
         });
+        console.log("build from serviceOverrides", build);
 
         if (services.length || predeploy || build) {
           setDetectedServices({

9d871a6b51d8458279601fd0d9a6640a4a2d3729
Feroze Mohideen <feroze@porter.run>
2023-09-27 12:48:16 -0400

    add update command

--- dashboard/src/main/home/app-dashboard/app-view/tabs/BuildSettings.tsx
+++ dashboard/src/main/home/app-dashboard/app-view/tabs/BuildSettings.tsx
@@ -10,6 +10,11 @@ import Button from "components/porter/Button";
 import Error from "components/porter/Error";
 import { match } from "ts-pattern";
 import ImageSettings from "../../image-settings/ImageSettings";
+import styled from "styled-components";
+import copy from "assets/copy-left.svg"
+import CopyToClipboard from "components/CopyToClipboard";
+import Link from "components/porter/Link";
+
 
 type Props = {
   redeployOnSave: boolean;
@@ -24,7 +29,7 @@ const BuildSettings: React.FC<Props> = ({
     watch,
     formState: { isSubmitting, errors },
   } = useFormContext<PorterAppFormData>();
-  const { projectId, latestRevision } = useLatestRevision();
+  const { projectId, latestRevision, latestProto } = useLatestRevision();
 
   const build = watch("app.build");
   const source = watch("source");
@@ -96,11 +101,55 @@ const BuildSettings: React.FC<Props> = ({
           >
             Save image settings
           </Button>
+          <Spacer y={1} />
+          <Text size={16}>Update command</Text>
+          <Spacer y={0.5} />
+          <Text color="helper">If you have the <Link to="https://docs.porter.run/standard/cli/command-reference/porter-update" target="_blank"><Text>Porter CLI</Text></Link> installed, you can update your application image tag by running the following command: </Text>
+          <Spacer y={0.5} />
+          <IdContainer>
+            <Code>{`$ porter app update-tag ${latestProto.name} --tag latest`}</Code>
+            <CopyContainer>
+              <CopyToClipboard text={`porter app update-tag ${latestProto.name} --tag latest`}>
+                <CopyIcon src={copy} alt="copy" />
+              </CopyToClipboard>
+            </CopyContainer>
+          </IdContainer>
         </>
       }
-
     </>
   );
 };
 
 export default BuildSettings;
+
+const Code = styled.span`
+  font-family: monospace;
+`;
+
+const IdContainer = styled.div`
+    background: #000000;  
+    border-radius: 5px;
+    padding: 10px;
+    display: flex;
+    width: 100%;
+    border-radius: 5px;
+    border: 1px solid ${({ theme }) => theme.border};
+    align-items: center;
+`;
+
+const CopyContainer = styled.div`
+  display: flex;
+  align-items: center;
+  margin-left: auto;
+`;
+
+const CopyIcon = styled.img`
+  cursor: pointer;
+  margin-left: 5px;
+  margin-right: 5px;
+  width: 15px;
+  height: 15px;
+  :hover {
+    opacity: 0.8;
+  }
+`;

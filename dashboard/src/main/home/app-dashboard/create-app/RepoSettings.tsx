c8aac0c6c7c1690cd467ee13cac66e1600eac04d
Ian Edwards <ianedwards559@gmail.com>
2023-09-28 11:06:25 -0400

    stop dockerfile detection if app exists

--- dashboard/src/main/home/app-dashboard/create-app/RepoSettings.tsx
+++ dashboard/src/main/home/app-dashboard/create-app/RepoSettings.tsx
@@ -74,7 +74,7 @@ const RepoSettings: React.FC<Props> = ({
   );
 
   useEffect(() => {
-    if (!branchContents) {
+    if (!branchContents || appExists) {
       return;
     }
 
@@ -85,7 +85,7 @@ const RepoSettings: React.FC<Props> = ({
     if (!hasDockerfile) {
       setValue("app.build.buildpacks", []);
     }
-  }, [branchContents]);
+  }, [branchContents, appExists]);
 
   return (
     <div>

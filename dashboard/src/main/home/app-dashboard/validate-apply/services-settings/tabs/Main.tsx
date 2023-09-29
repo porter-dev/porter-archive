a305793c654e673cbd0352b7b3d06129279df3a6
d-g-town <66391417+d-g-town@users.noreply.github.com>
2023-09-29 15:52:27 -0400

    support allowConcurrency, suspendCron, and timeoutSeconds (#3686)
    Co-authored-by: David Townley <davidtownley@Davids-MacBook-Air.local>

--- dashboard/src/main/home/app-dashboard/validate-apply/services-settings/tabs/Main.tsx
+++ dashboard/src/main/home/app-dashboard/validate-apply/services-settings/tabs/Main.tsx
@@ -1,6 +1,6 @@
 import React, { useCallback } from "react";
 import cronstrue from "cronstrue";
-import { useFormContext } from "react-hook-form";
+import {Controller, useFormContext} from "react-hook-form";
 
 import { ControlledInput } from "components/porter/ControlledInput";
 import Spacer from "components/porter/Spacer";
@@ -8,6 +8,7 @@ import { PorterAppFormData } from "lib/porter-apps";
 import { ClientService } from "lib/porter-apps/services";
 import Text from "components/porter/Text";
 import Link from "components/porter/Link";
+import Checkbox from "components/porter/Checkbox";
 
 type MainTabProps = {
   index: number;
@@ -16,7 +17,7 @@ type MainTabProps = {
 };
 
 const MainTab: React.FC<MainTabProps> = ({ index, service, isPredeploy = false }) => {
-  const { register, watch } = useFormContext<PorterAppFormData>();
+  const { register, control, watch } = useFormContext<PorterAppFormData>();
   const cron = watch(`app.services.${index}.config.cron.value`);
 
   const getScheduleDescription = useCallback((cron: string) => {
@@ -62,8 +63,28 @@ const MainTab: React.FC<MainTabProps> = ({ index, service, isPredeploy = false }
             }
             {...register(`app.services.${index}.config.cron.value`)}
           />
+
           <Spacer y={0.5} />
           {getScheduleDescription(cron)}
+            <Spacer y={0.5} />
+            <Controller
+                name={`app.services.${index}.config.suspendCron.value`}
+                control={control}
+                render={({ field: { value, onChange } }) => (
+                    <Checkbox
+                        checked={value}
+                        disabled={service.config.suspendCron?.readOnly}
+                        toggleChecked={() => {
+                            onChange(!value);
+                        }}
+                        disabledTooltip={
+                            "You may only edit this field in your porter.yaml."
+                        }
+                    >
+                        <Text color="helper">Suspend cron job</Text>
+                    </Checkbox>
+                )}
+            />
         </>
       )}
     </>

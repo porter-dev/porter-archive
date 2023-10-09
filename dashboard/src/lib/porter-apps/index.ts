023f6cbc2ea2722c59b6bbdf638cdf02f9b34a3a
ianedwards <ianedwards559@gmail.com>
2023-10-09 17:14:57 -0400

    Revert "POR-1902 validate name against existing porter apps (#3774)"
    This reverts commit 32cd1395ab875fd617d4de7e906098973edfa6f4.

--- dashboard/src/lib/porter-apps/index.ts
+++ dashboard/src/lib/porter-apps/index.ts
@@ -59,13 +59,7 @@ export const deletionValidator = z.object({
 export const clientAppValidator = z.object({
   name: z.object({
     readOnly: z.boolean(),
-    value: z
-      .string()
-      .min(1, { message: "Name must be at least 1 character" })
-      .max(30, { message: "Name must be 30 characters or less" })
-      .regex(/^[a-z0-9-]{1,61}$/, {
-        message: 'Lowercase letters, numbers, and "-" only.',
-      }),
+    value: z.string(),
   }),
   envGroups: z
     .object({ name: z.string(), version: z.bigint() })

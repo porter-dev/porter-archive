a8ce5e0e7bfd59d61f8c6157a44d55aca8155463
Soham Dessai <sd5we@virginia.edu>
2023-10-19 17:39:51 -0400

    Hotfix GCP

--- internal/porter_app/test/parse_test.go
+++ internal/porter_app/test/parse_test.go
@@ -226,12 +226,13 @@ var v1_result_nobuild_no_image = &porterv1.PorterApp{
 			Type: 2,
 		},
 		"example-web": {
-			Name:         "example-web",
-			RunOptional:  pointer.String("node index.js"),
-			Instances:    0,
-			Port:         8080,
-			CpuCores:     0.1,
-			RamMegabytes: 256,
+			Name:           "example-web",
+			RunOptional:    pointer.String("node index.js"),
+			Instances:      0,
+			Port:           8080,
+			CpuCores:       0.1,
+			RamMegabytes:   256,
+			GpuCoresNvidia: 0.4,
 			Config: &porterv1.Service_WebConfig{
 				WebConfig: &porterv1.WebServiceConfig{
 					Autoscaling: &porterv1.Autoscaling{
@@ -288,12 +289,13 @@ var v1_result_nobuild_no_image = &porterv1.PorterApp{
 			Type: 2,
 		},
 		{
-			Name:         "example-web",
-			RunOptional:  pointer.String("node index.js"),
-			Instances:    0,
-			Port:         8080,
-			CpuCores:     0.1,
-			RamMegabytes: 256,
+			Name:           "example-web",
+			RunOptional:    pointer.String("node index.js"),
+			Instances:      0,
+			Port:           8080,
+			CpuCores:       0.1,
+			RamMegabytes:   256,
+			GpuCoresNvidia: 0.4,
 			Config: &porterv1.Service_WebConfig{
 				WebConfig: &porterv1.WebServiceConfig{
 					Autoscaling: &porterv1.Autoscaling{

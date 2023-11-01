6f593c99989ad76792a4bd8f0fd4235aea8cfb13
Soham Dessai <sd5we@virginia.edu>
2023-11-01 15:52:44 -0400

    Remove Usage

--- dashboard/src/components/ProvisionerFlow.tsx
+++ dashboard/src/components/ProvisionerFlow.tsx
@@ -33,13 +33,6 @@ const ProvisionerFlow: React.FC<Props> = ({ }) => {
   const [useCloudFormationForm, setUseCloudFormationForm] = useState(true);
   const [selectedProvider, setSelectedProvider] = useState("");
 
-  const isUsageExceeded = useMemo(() => {
-    if (!hasBillingEnabled) {
-      return false;
-    }
-    return usage?.current.clusters >= usage?.limit.clusters;
-  }, [usage]);
-
   const markStepCostConsent = async (step: string, provider: string) => {
     try {
       await api.updateOnboardingStep("<token>", { step, provider }, { project_id: currentProject.id });
@@ -66,14 +59,13 @@ const ProvisionerFlow: React.FC<Props> = ({ }) => {
                 <Block
                   key={i}
                   disabled={
-                    !currentProject?.multi_cluster && (isUsageExceeded ||
+                    !currentProject?.multi_cluster && (
                       (provider === "gcp" && !currentProject?.azure_enabled))
 
                   }
                   onClick={() => {
                     if (
                       !(
-                        isUsageExceeded ||
                         (provider === "gcp" && !currentProject?.azure_enabled)
                       )
                     ) {

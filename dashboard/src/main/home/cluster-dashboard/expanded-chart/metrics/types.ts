b9a3cf0c2aa3a01946956da208de0cc9ccd2b21b
jose-fully-ported <141160579+jose-fully-ported@users.noreply.github.com>
2023-11-06 16:53:18 -0500

    [POR-1917] use the correct key path when checking if a service is private for displaying the throughput graph (#3934)
    Co-authored-by: Feroze Mohideen <feroze@porter.run>

--- dashboard/src/main/home/cluster-dashboard/expanded-chart/metrics/types.ts
+++ dashboard/src/main/home/cluster-dashboard/expanded-chart/metrics/types.ts
@@ -1,79 +1,81 @@
 export type MetricsCPUDataResponse = {
   pod?: string;
-  results: {
+  results: Array<{
     date: number;
     cpu: string;
-  }[];
+  }>;
 };
 
 export type MetricsMemoryDataResponse = {
   pod?: string;
-  results: {
+  results: Array<{
     date: number;
     memory: string;
-  }[];
+  }>;
 };
 
 export type MetricsNetworkDataResponse = {
   pod?: string;
-  results: {
+  results: Array<{
     date: number;
     bytes: string;
-  }[];
+  }>;
 };
 
 export type MetricsNGINXErrorsDataResponse = {
   pod?: string;
-  results: {
+  results: Array<{
     date: number;
     error_pct: string;
-  }[];
+  }>;
 };
 
 export type MetricsNGINXLatencyDataResponse = {
   pod?: string;
-  results: {
+  results: Array<{
     date: number;
     latency: string;
-  }[];
+  }>;
 };
 
 export type MetricsNGINXStatusDataResponse = {
   pod?: string;
-  results: {
+  results: Array<{
     "1xx": string;
     "2xx": string;
     "3xx": string;
     "4xx": string;
     "5xx": string;
     date: number;
-  }[];
-}
+  }>;
+};
 
 export type MetricsHpaReplicasDataResponse = {
   pod?: string;
-  results: {
+  results: Array<{
     date: number;
     replicas: string;
-  }[];
+  }>;
+};
+
+export type GenericMetricResponseResults = {
+  "1xx": string;
+  "2xx": string;
+  "3xx": string;
+  "4xx": string;
+  "5xx": string;
+  date: number;
+  cpu: string;
+  memory: string;
+  bytes: string;
+  error_pct: string;
+  replicas: string;
+  latency: string;
 };
 
 export type GenericMetricResponse = {
   pod?: string;
-  results: {
-    "1xx": string;
-    "2xx": string;
-    "3xx": string;
-    "4xx": string;
-    "5xx": string;
-    date: number;
-    cpu: string;
-    memory: string;
-    bytes: string;
-    error_pct: string;
-    replicas: string;
-    latency: string;
-  }[];
+  results: GenericMetricResponseResults[];
 };
 
 export type NormalizedMetricsData = {

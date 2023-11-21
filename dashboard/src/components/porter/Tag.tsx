a98894ecc6be097e149d2eb833a2f1c6292e2864
Feroze Mohideen <feroze@porter.run>
2023-11-21 12:47:17 -0500

    undo change

--- dashboard/src/components/porter/Tag.tsx
+++ dashboard/src/components/porter/Tag.tsx
@@ -6,7 +6,6 @@ type Props = {
   hoverable?: boolean;
   children: React.ReactNode;
   borderColor?: string;
-  style?: React.CSSProperties;
 };
 
 const Tag: React.FC<Props> = ({
@@ -14,7 +13,6 @@ const Tag: React.FC<Props> = ({
   hoverable = true,
   children,
   borderColor,
-  style,
 }) => {
   return (
     <StyledTag
@@ -22,7 +20,6 @@ const Tag: React.FC<Props> = ({
       hoverable={hoverable}
       hoverColor={backgroundColor ?? "#ffffff44"}
       borderColor={borderColor}
-      style={style}
     >
       {children}
     </StyledTag>

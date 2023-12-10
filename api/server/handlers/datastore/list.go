95793e3a0c0798ca2691fe2f16f1662ddc5f1a92
Jose Diaz-Gonzalez <jose@porter.run>
2023-12-09 21:03:58 -0500

    scratch

--- api/server/handlers/datastore/list.go
+++ api/server/handlers/datastore/list.go
@@ -1,11 +1,10 @@
 package datastore
 
 import (
-	"context"
 	"net/http"
-	"sort"
 
-	"github.com/aws/aws-sdk-go/aws/arn"
+	"connectrpc.com/connect"
+	porterv1 "github.com/porter-dev/api-contracts/generated/go/porter/v1"
 	"github.com/porter-dev/porter/api/server/authz"
 	"github.com/porter-dev/porter/api/server/handlers"
 	"github.com/porter-dev/porter/api/server/shared"
@@ -17,13 +16,35 @@ import (
 	"github.com/porter-dev/porter/internal/telemetry"
 )
 
-// ListResponseEntry describes an outbound datastore status response entry
+// ListRequest is a struct that represents the various filter options used for
+// retrieving the datastores
+type ListRequest struct {
+	// IncludeConfig controls whether to include the datastore config or not
+	IncludeConfig bool `schema:"include_config"`
+
+	// IncludeEnvGroup controls whether to include the datastore env group or not
+	IncludeEnvGroup bool `schema:"include_env_group"`
+
+	// IncludeMetadata controls whether to include datastore metadata or not
+	IncludeMetadata bool `schema:"include_metadata"`
+
+	// IncludeStatus controls whether to include the datastore status or not
+	IncludeStatus bool `schema:"include_status"`
+}
+
+// ListResponseEntry describes an outbound datastores response entry
 type ListResponseEntry struct {
 	// Name is the name of the datastore
 	Name string `json:"name"`
 
 	// Type is the type of the datastore
 	Type string `json:"type"`
+
+	// Status is the status of the datastore
+	Status string `json:"status,omitempty"`
+
+	// Env is the env group for the datastore
+	Env *porterv1.DatastoreEnv `json:"env,omitempty"`
 }
 
 // ListHandler is a struct for handling datastore status requests
@@ -50,6 +71,11 @@ func (h *ListHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 	project, _ := ctx.Value(types.ProjectScope).(*models.Project)
 
+	request := &ListRequest{}
+	if ok := h.DecodeAndValidate(w, r, request); !ok {
+		return
+	}
+
 	cloudProviderType, err := requestutils.GetURLParamString(r, types.URLParamCloudProviderType)
 	if err != nil {
 		err := telemetry.Error(ctx, span, err, "error parsing cloud provider type")
@@ -68,129 +94,41 @@ func (h *ListHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 	switch cloudProviderType {
 	case "aws":
-		res, err := h.AwsDatastores(ctx, *project, cloudProviderID, r)
+		req := connect.NewRequest(&porterv1.ListDatastoresRequest{
+			ProjectId:              int64(project.ID),
+			CloudProvider:          porterv1.EnumCloudProvider_ENUM_CLOUD_PROVIDER_AWS,
+			CloudProviderAccountId: cloudProviderID,
+			IncludeConfig:          request.IncludeConfig,
+			IncludeEnvGroup:        request.IncludeEnvGroup,
+			IncludeMetadata:        request.IncludeMetadata,
+			IncludeStatus:          request.IncludeStatus,
+		})
+		resp, err := h.Config().ClusterControlPlaneClient.ListDatastores(ctx, req)
 		if err != nil {
-			err := telemetry.Error(ctx, span, err, "error retrieving datastores")
-			h.HandleAPIError(w, r, apierrors.NewErrPassThroughToClient(err, http.StatusBadRequest))
+			err := telemetry.Error(ctx, span, err, "error listing datastores from ccp")
+			h.HandleAPIError(w, r, apierrors.NewErrPassThroughToClient(err, http.StatusInternalServerError))
 			return
 		}
-		h.WriteResult(w, r, res)
+		if resp.Msg == nil {
+			err := telemetry.Error(ctx, span, err, "missing response message from ccp")
+			h.HandleAPIError(w, r, apierrors.NewErrPassThroughToClient(err, http.StatusInternalServerError))
+			return
+		}
+
+		response := []ListResponseEntry{}
+		for _, datastore := range resp.Msg.Datastores {
+			response = append(response, ListResponseEntry{
+				Name:   datastore.Name,
+				Type:   datastore.Type.Enum().String(),
+				Status: datastore.Status,
+				Env:    datastore.Env,
+			})
+		}
+
+		h.WriteResult(w, r, response)
 	default:
 		err := telemetry.Error(ctx, span, nil, "unsupported cloud provider")
 		h.HandleAPIError(w, r, apierrors.NewErrPassThroughToClient(err, http.StatusBadRequest))
 		return
 	}
-}
-
-// AwsDatastores retrieves all datastores provisioned to a given project/
-func (h *ListHandler) AwsDatastores(ctx context.Context, project models.Project, awsAccountID string, r *http.Request) ([]ListResponseEntry, error) {
-	ctx, span := telemetry.NewSpan(ctx, "aws-datastores")
-	defer span.End()
-	telemetry.WithAttributes(span, telemetry.AttributeKV{Key: "project-id", Value: project.ID})
-
-	res := []ListResponseEntry{}
-
-	clusters, err := h.Repo().Cluster().ListClustersByProjectID(project.ID)
-	if err != nil {
-		return res, telemetry.Error(ctx, span, err, "clusters for project could not be retrieved")
-	}
-
-	capiProvisionerEnabled := project.GetFeatureFlag(models.CapiProvisionerEnabled, h.Config().LaunchDarklyClient)
-	for _, cluster := range clusters {
-		clusterInAccount := false
-		if capiProvisionerEnabled {
-			b, err := arn.Parse(cluster.CloudProviderCredentialIdentifier)
-			if err != nil {
-				return res, telemetry.Error(ctx, span, err, "cluster cloud provider arn could not be parsed")
-			}
-
-			clusterInAccount = b.AccountID == awsAccountID
-		} else {
-			if cluster.AWSIntegrationID == 0 {
-				continue
-			}
-
-			awsIntegration, err := h.Repo().AWSIntegration().ReadAWSIntegration(project.ID, cluster.AWSIntegrationID)
-			if err != nil {
-				return res, telemetry.Error(ctx, span, err, "aws integration could not be found")
-			}
-
-			b, err := arn.Parse(awsIntegration.AWSArn)
-			if err != nil {
-				return res, telemetry.Error(ctx, span, err, "aws integration arn could not be parsed")
-			}
-
-			clusterInAccount = b.AccountID == awsAccountID
-		}
-
-		if clusterInAccount {
-			datastores, err := h.getAwsDatastoresForCluster(ctx, r, cluster)
-			if err != nil {
-				return res, telemetry.Error(ctx, span, err, "error getting datastores cluster")
-			}
-			res = append(res, datastores...)
-		}
-	}
-
-	sort.Slice(res, func(i, j int) bool {
-		return res[i].Name < res[j].Name
-	})
-
-	return res, nil
-}
-
-func (h *ListHandler) getAwsDatastoresForCluster(ctx context.Context, r *http.Request, cluster *models.Cluster) ([]ListResponseEntry, error) {
-	ctx, span := telemetry.NewSpan(ctx, "get-aws-datastores")
-	defer span.End()
-
-	namespace := "ack-system"
-	telemetry.WithAttributes(span, telemetry.AttributeKV{Key: "namespace", Value: namespace})
-	telemetry.WithAttributes(span, telemetry.AttributeKV{Key: "cluster-id", Value: cluster.ID})
-
-	res := []ListResponseEntry{}
-	helmAgent, err := h.GetHelmAgent(ctx, r, cluster, "")
-	if err != nil {
-		return res, telemetry.Error(ctx, span, err, "error getting helm agent")
-	}
-
-	request := &types.ListReleasesRequest{
-		ReleaseListFilter: &types.ReleaseListFilter{
-			Namespace: namespace,
-			Limit:     100,
-			Skip:      0,
-			ByDate:    false,
-			StatusFilter: []string{
-				"deployed", "uninstalled", "pending", "pending", "pending-install", "pending-upgrade", "pending-rollback", "failed",
-			},
-		},
-	}
-
-	releases, err := helmAgent.ListReleases(ctx, namespace, request.ReleaseListFilter)
-	if err != nil {
-		return res, telemetry.Error(ctx, span, err, "error getting helm releases")
-	}
-
-	awsDatastoreCharts := map[string]bool{
-		"elasticache-redis":     true,
-		"rds-postgresql":        true,
-		"rds-postgresql-aurora": true,
-	}
-
-	telemetry.WithAttributes(span, telemetry.AttributeKV{Key: "release-count", Value: len(releases)})
-	for _, helmRel := range releases {
-		if helmRel.Chart == nil {
-			continue
-		}
-
-		if _, ok := awsDatastoreCharts[helmRel.Chart.Name()]; !ok {
-			continue
-		}
-
-		res = append(res, ListResponseEntry{
-			Name: helmRel.Name,
-			Type: helmRel.Chart.Name(),
-		})
-	}
-
-	return res, nil
 }

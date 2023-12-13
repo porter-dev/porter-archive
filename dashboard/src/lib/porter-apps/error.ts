2aefa3f952b4ec533acd8784babb59328f103143
Feroze Mohideen <feroze@porter.run>
2023-12-13 12:18:18 -0500

    fix messaging

--- dashboard/src/lib/porter-apps/error.ts
+++ dashboard/src/lib/porter-apps/error.ts
@@ -4,7 +4,8 @@ export const ERROR_CODE_APPLICATION_ROLLBACK_FAILED = 92;
 
 export const ERROR_CODE_TO_SUMMARY: Record<number, string> = {
   [ERROR_CODE_UPDATE_FAILED]: "The latest version failed to deploy",
-  [ERROR_CODE_APPLICATION_ROLLBACK]: "The application was rolled back",
+  [ERROR_CODE_APPLICATION_ROLLBACK]:
+    "A version deployment failure triggered an auto-rollback",
   [ERROR_CODE_APPLICATION_ROLLBACK_FAILED]:
-    "The application attempted rollback, but the new deployment failed",
+    "Porter attempted a rollback, but the new deployment failed",
 };

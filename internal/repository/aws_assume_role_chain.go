c4a2c529f9a69f7fabbdd70ae5b816f69dc69e41
Stefan McShane <stefanmcshane@users.noreply.github.com>
2024-01-10 18:14:15 -0500

    Require role access for credential updates (#4139)

--- internal/repository/aws_assume_role_chain.go
+++ internal/repository/aws_assume_role_chain.go
@@ -12,4 +12,6 @@ type AWSAssumeRoleChainer interface {
 	// List returns the final hop in an assume role chain, where the ARN accounts
 	// are not owned by Porter
 	List(ctx context.Context, projectID uint) ([]*models.AWSAssumeRoleChain, error)
+	// ListByAwsAccountId returns the final hops in an assume role chain, where the ARN accounts match the supplied AWS account ID
+	ListByAwsAccountId(ctx context.Context, targetAwsAccountId string) ([]*models.AWSAssumeRoleChain, error)
 }

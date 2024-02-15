1724513912d4679d606133e474c156768c670cd8
Feroze Mohideen <feroze@porter.run>
2024-02-15 13:27:10 -0500

    type fix

--- dashboard/src/main/home/infrastructure-dashboard/tabs/overview/EKSClusterOverview.tsx
+++ dashboard/src/main/home/infrastructure-dashboard/tabs/overview/EKSClusterOverview.tsx
@@ -1,13 +1,14 @@
-import React, { useEffect, useMemo } from "react";
+import React, { useMemo } from "react";
 import { useFormContext } from "react-hook-form";
-
-import { SUPPORTED_AWS_REGIONS } from "lib/clusters/constants";
 
 import Container from "components/porter/Container";
 import Select from "components/porter/Select";
 import Spacer from "components/porter/Spacer";
 import Text from "components/porter/Text";
-import { CloudProviderAWS } from "lib/clusters/constants";
+import {
+  CloudProviderAWS,
+  SUPPORTED_AWS_REGIONS,
+} from "lib/clusters/constants";
 import { type ClientClusterContract } from "lib/clusters/types";
 
 import NodeGroups from "../../shared/NodeGroups";
@@ -17,16 +18,17 @@ const EKSClusterOverview: React.FC = () => {
   const region = watch("cluster.config.region");
 
   const label = useMemo(() => {
-    return SUPPORTED_AWS_REGIONS.find(x => x.name === region)?.displayName;
-  }
-  , [region]);
+    return SUPPORTED_AWS_REGIONS.find((x) => x.name === region)?.displayName;
+  }, [region]);
 
   return (
     <>
       <Container style={{ width: "300px" }}>
         <Text size={16}>AWS region</Text>
         <Spacer y={0.5} />
-        <Text color="helper">Your cluster is running in the following region.</Text>
+        <Text color="helper">
+          Your cluster is running in the following region.
+        </Text>
         <Spacer y={0.7} />
         <Select
           options={[{ value: region, label: label || "" }]}
@@ -35,9 +37,7 @@ const EKSClusterOverview: React.FC = () => {
         />
       </Container>
       <Spacer y={1} />
-      <Text size={16}>
-        Node groups
-      </Text>
+      <Text size={16}>Node groups</Text>
       <Spacer y={0.5} />
       <Text color="helper">
         Configure node groups to support custom workloads.{" "}

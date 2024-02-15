b1b81d3292080dcb192bacd7d28a4aea3533805b
Feroze Mohideen <feroze@porter.run>
2024-02-14 20:43:28 -0500

    unblock new

--- dashboard/src/main/home/infrastructure-dashboard/ClusterDashboard.tsx
+++ dashboard/src/main/home/infrastructure-dashboard/ClusterDashboard.tsx
@@ -4,12 +4,12 @@ import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 import Loading from "components/Loading";
-// import Button from "components/porter/Button";
+import Button from "components/porter/Button";
 import Container from "components/porter/Container";
 import Fieldset from "components/porter/Fieldset";
 import Icon from "components/porter/Icon";
 import Image from "components/porter/Image";
-// import PorterLink from "components/porter/Link";
+import PorterLink from "components/porter/Link";
 import SearchBar from "components/porter/SearchBar";
 import Select from "components/porter/Select";
 import Spacer from "components/porter/Spacer";
@@ -139,12 +139,12 @@ const ClusterDashboard: React.FC = () => {
           }}
         />
 
-        {/* <Spacer inline x={2} />
+        <Spacer inline x={2} />
         <PorterLink to="/infrastructure/new">
           <Button onClick={() => ({})} height="30px" width="70px">
             <I className="material-icons">add</I> New
           </Button>
-        </PorterLink> */}
+        </PorterLink>
       </Container>
       <Spacer y={1} />
       {isLoading ? (
@@ -317,14 +317,14 @@ const ToggleIcon = styled.img`
   min-width: 12px;
 `;
 
-// const I = styled.i`
-//   color: white;
-//   font-size: 14px;
-//   display: flex;
-//   align-items: center;
-//   margin-right: 5px;
-//   justify-content: center;
-// `;
+const I = styled.i`
+  color: white;
+  font-size: 14px;
+  display: flex;
+  align-items: center;
+  margin-right: 5px;
+  justify-content: center;
+`;
 
 const StyledAppDashboard = styled.div`
   width: 100%;

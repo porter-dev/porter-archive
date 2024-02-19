a97946e6609c576216030f05b4bf938f2ecbf47f
jusrhee <justin@porter.run>
2024-02-19 11:04:23 -0500

    remove sandbox placeholder after cloud select (#4294)

--- dashboard/src/main/home/onboarding/Onboarding.tsx
+++ dashboard/src/main/home/onboarding/Onboarding.tsx
@@ -178,24 +178,6 @@ const Onboarding = () => {
             capitalize={false}
           />
           <ProvisionerFlow />
-          <DashboardPlaceholder>
-            <Text size={16}>
-              Want to test Porter without linking your own cloud account?
-            </Text>
-            <Spacer y={0.5} />
-            <Text color={"helper"}>
-              Get started with the Porter sandbox environment.
-            </Text>
-            <Spacer y={1} />
-            <Link to="https://sandbox.porter.run">
-              <Button alt height="35px">
-                Deploy on the Porter sandbox <Spacer inline x={1} />{" "}
-                <i className="material-icons" style={{ fontSize: "18px" }}>
-                  east
-                </i>
-              </Button>
-            </Link>
-          </DashboardPlaceholder>
           <Div />
         </Wrapper>
       );

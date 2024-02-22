18ddb88a28deadd5b5bdb5dcf54c6a991d1bdd98
Stefan McShane <stefanmcshane@users.noreply.github.com>
2024-02-22 15:05:05 -0500

    Add cidr table

---
+++ internal/models/ipam.go
@@ -0,0 +1,27 @@
+package models
+
+import (
+	"net"
+
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+// Ipam represents a an entry in the Ipam table
+type Ipam struct {
+	gorm.Model
+
+	// ID is a UUID for the APIContract
+	ID uuid.UUID `gorm:"type:uuid;primaryKey" json:"id"`
+
+	// ProjectID is the ID of the project that the config belongs to.
+	// This should be a foreign key, but GORM doesnt play well with FKs.
+	ProjectID int `json:"project_id"`
+
+	CIDR net.IPNet `gorm:"type:cidr;column:cidr_range"`
+}
+
+// TableName overrides the table name
+func (Ipam) TableName() string {
+	return "ipam"
+}

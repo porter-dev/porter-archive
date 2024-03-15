074487994d34f0172895e1923d5d95bd716049ad
Feroze Mohideen <feroze@porter.run>
2024-03-15 14:49:42 -0400

    consolidate datastore connection info

--- dashboard/src/main/home/database-dashboard/tabs/ConnectTab.tsx
+++ dashboard/src/main/home/database-dashboard/tabs/ConnectTab.tsx
@@ -1,9 +1,8 @@
-import React, { useMemo } from "react";
+import React from "react";
 import styled from "styled-components";
 
 import CopyToClipboard from "components/CopyToClipboard";
 import Container from "components/porter/Container";
-import Input from "components/porter/Input";
 import Link from "components/porter/Link";
 import Spacer from "components/porter/Spacer";
 import Text from "components/porter/Text";
@@ -11,91 +10,43 @@ import Text from "components/porter/Text";
 import copy from "assets/copy-left.svg";
 
 import { useDatastoreContext } from "../DatabaseContextProvider";
+import ConnectionInfo from "../shared/ConnectionInfo";
 
 const ConnectTab: React.FC = () => {
   const { datastore } = useDatastoreContext();
 
-  const keyValues = useMemo(() => {
-    const datastoreEnvEntries = Object.entries(datastore.env?.variables ?? {});
-    const datastoreSecretEntries = Object.entries(
-      datastore.env?.secret_variables ?? {}
-    );
-    const keyValues = [
-      ...datastoreEnvEntries.map(([key, value]) => ({
-        key,
-        value,
-        secret: false,
-      })),
-      ...datastoreSecretEntries.map(([key, value]) => ({
-        key,
-        value,
-        secret: true,
-      })),
-    ];
-
-    return keyValues;
-  }, [datastore]);
   return (
     <CredentialsTabContainer>
       <Container row>
         <Text size={16}>Application connection</Text>
       </Container>
-      {keyValues.length !== 0 && (
+      {datastore.credential.host !== "" && (
         <>
           <Spacer y={0.5} />
           <Text color="helper">
-            Once an app is connected to this datastore, it has access to its
-            credentials through the following environment variables:
+            All apps deployed in your cluster can access the datastore using the
+            following credentials:
           </Text>
-          <StyledInputArray>
-            {keyValues.map(
-              (
-                entry: { key: string; value: string; secret: boolean },
-                i: number
-              ) => {
-                return (
-                  <InputWrapper key={i}>
-                    <Input
-                      placeholder="ex: key"
-                      width="120px"
-                      value={entry.key}
-                      setValue={() => ({})}
-                      disabled={entry.secret}
-                      disabledTooltip={"Stored as a secret on your cluster"}
-                    />
-                    <Spacer x={0.5} inline />
-                    <Input
-                      placeholder="ex: key"
-                      width="500px"
-                      value={entry.value}
-                      setValue={() => ({})}
-                      disabled={entry.secret}
-                      disabledTooltip={"Stored as a secret on your cluster"}
-                    />
-                    {!entry.secret && (
-                      <>
-                        <Spacer x={0.5} inline />
-                        <CopyToClipboard text={entry.value}>
-                          <CopyIcon src={copy} alt="copy" />
-                        </CopyToClipboard>
-                      </>
-                    )}
-                  </InputWrapper>
-                );
-              }
-            )}
-          </StyledInputArray>
+          <Spacer y={0.5} />
+          <ConnectionInfo
+            connectionInfo={datastore.credential}
+            type={datastore.template.type}
+          />
+          <Spacer y={0.5} />
+          <Text color="warner">
+            For security, access to the datastore is restricted - connection
+            attempts from outside the cluster will not succeed.
+          </Text>
           <Spacer y={0.5} />
           <Text color="helper">
             The datastore client of your application should use these
-            environment variables to create a connection.
+            credentials to create a connection.
           </Text>
           {datastore.template.type.name === "ELASTICACHE" && (
             <>
               <Spacer y={0.5} />
               <Text color="warner">
-                In order for connection to succeed, your datastore client must
-                connect via SSL.
+                Your datastore client must connect via SSL.
               </Text>
             </>
           )}
@@ -104,19 +55,15 @@ const ConnectTab: React.FC = () => {
       <Spacer y={1} />
       <Text size={16}>Local connection</Text>
       <Spacer y={0.5} />
-      <Text color="warner">
-        The credentials above will only work for apps running on your cluster.
-      </Text>
-      <Spacer y={0.5} />
       <Text color="helper">
-        However, if you have authenticated via the{" "}
+        For local connection, you can create a temporary, secure tunnel to this
+        datastore using the{" "}
         <Link
           to="https://docs.porter.run/standard/cli/command-reference/porter-datastore-connect"
           target="_blank"
         >
           <Text>Porter CLI</Text>
-        </Link>{" "}
-        , you can create a secure tunnel to this datastore to connect locally:
+        </Link>
       </Text>
       <Spacer y={0.5} />
       <IdContainer>
@@ -167,15 +114,4 @@ const CopyIcon = styled.img`
 
 const Code = styled.span`
   font-family: monospace;
-`;
-
-const StyledInputArray = styled.div`
-  margin-bottom: 15px;
-  margin-top: 22px;
-`;
-
-const InputWrapper = styled.div`
-  display: flex;
-  align-items: center;
-  margin-top: 5px;
 `;

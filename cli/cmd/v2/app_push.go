2159c049dbc1a41fba0268c047f69e8df865ec98
Ian Edwards <ianedwards559@gmail.com>
2024-03-15 16:34:02 -0400

    add comments on stub

--- cli/cmd/v2/app_push.go
+++ cli/cmd/v2/app_push.go
@@ -2,8 +2,10 @@ package v2
 
 import "context"
 
+// AppPushInput is the input to the AppPush function
 type AppPushInput struct{}
 
+// AppPush pushes an app to a remote registry
 func AppPush(ctx context.Context, inp AppPushInput) error {
 	return nil
 }

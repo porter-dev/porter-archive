6e39cd92565a9765d39ec7eedd1d51b6b05bdf36
Feroze Mohideen <feroze@porter.run>
2024-03-15 16:21:36 -0400

    redesign

--- dashboard/src/main/home/database-dashboard/DatabaseTabs.tsx
+++ dashboard/src/main/home/database-dashboard/DatabaseTabs.tsx
@@ -7,7 +7,6 @@ import TabSelector from "components/TabSelector";
 
 import { useDatastoreContext } from "./DatabaseContextProvider";
 import DatastoreProvisioningIndicator from "./DatastoreProvisioningIndicator";
-import ConnectedAppsTab from "./tabs/ConnectedAppsTab";
 import ConnectTab from "./tabs/ConnectTab";
 import MetricsTab from "./tabs/MetricsTab";
 import SettingsTab from "./tabs/SettingsTab";
@@ -44,7 +43,7 @@ const DatabaseTabs: React.FC<DbTabProps> = ({ tabParam }) => {
   const tabs = useMemo(() => {
     return [
       { label: "Connect", value: "connect" },
-      { label: "Connected Apps", value: "connected-apps" },
+      // { label: "Connected Apps", value: "connected-apps" },
       // { label: "Configuration", value: "configuration" },
       { label: "Settings", value: "settings" },
     ];
@@ -70,9 +69,8 @@ const DatabaseTabs: React.FC<DbTabProps> = ({ tabParam }) => {
         .with("settings", () => <SettingsTab />)
         .with("metrics", () => <MetricsTab />)
         // .with("configuration", () => <ConfigurationTab />)
-        .with("connected-apps", () => <ConnectedAppsTab />)
+        // .with("connected-apps", () => <ConnectedAppsTab />)
         .otherwise(() => null)}
-      <Spacer y={2} />
     </div>
   );
 };
--- dashboard/src/main/home/database-dashboard/tabs/ConnectTab.tsx
+++ dashboard/src/main/home/database-dashboard/tabs/ConnectTab.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 
 import CopyToClipboard from "components/CopyToClipboard";
@@ -10,78 +10,105 @@ import Text from "components/porter/Text";
 import copy from "assets/copy-left.svg";
 
 import { useDatastoreContext } from "../DatabaseContextProvider";
+import ConnectAppsModal from "../shared/ConnectAppsModal";
 import ConnectionInfo from "../shared/ConnectionInfo";
 
 const ConnectTab: React.FC = () => {
   const { datastore } = useDatastoreContext();
+  const [showConnectAppsModal, setShowConnectAppsModal] = useState(false);
 
   return (
-    <CredentialsTabContainer>
-      <Container row>
-        <Text size={16}>Application connection</Text>
-      </Container>
-      {datastore.credential.host !== "" && (
-        <>
-          <Spacer y={0.5} />
-          <Text color="helper">
-            All apps deployed in your cluster can access this datastore using
-            the following credentials:
-          </Text>
-          <Spacer y={0.5} />
-          <ConnectionInfo
-            connectionInfo={datastore.credential}
-            type={datastore.template.type}
-          />
-          <Spacer y={0.5} />
-          <Text color="warner">
-            For security, access to the datastore is restricted - connection
-            attempts from outside the cluster will not succeed.
-          </Text>
-          <Spacer y={0.5} />
-          <Text color="helper">
-            The datastore client of your application should use these
-            credentials to create a connection.
-          </Text>
-          {datastore.template.type.name === "ELASTICACHE" && (
-            <>
-              <Spacer y={0.5} />
-              <Text color="warner">
-                Your datastore client must connect via SSL.
-              </Text>
-            </>
-          )}
-        </>
-      )}
-      <Spacer y={1} />
-      <Text size={16}>Local connection</Text>
-      <Spacer y={0.5} />
-      <Text color="helper">
-        For local connection, you can create a temporary, secure tunnel to this
-        datastore using the{" "}
-        <Link
-          to="https://docs.porter.run/standard/cli/command-reference/porter-datastore-connect"
-          target="_blank"
-        >
-          <Text>Porter CLI</Text>
-        </Link>
-      </Text>
-      <Spacer y={0.5} />
-      <IdContainer>
-        <Code>{`$ porter datastore connect ${datastore.name}`}</Code>
-        <CopyContainer>
-          <CopyToClipboard text={`porter datastore connect ${datastore.name}`}>
-            <CopyIcon src={copy} alt="copy" />
-          </CopyToClipboard>
-        </CopyContainer>
-      </IdContainer>
-    </CredentialsTabContainer>
+    <ConnectTabContainer>
+      <div
+        style={{
+          width: "100%",
+          height: "100%",
+          paddingRight: "10px",
+          borderRight: "1px #aaaabb55 solid",
+        }}
+      >
+        <Container row>
+          <Text size={16}>Credentials</Text>
+        </Container>
+        {datastore.credential.host !== "" && (
+          <>
+            <Spacer y={0.5} />
+            <ConnectionInfo
+              connectionInfo={datastore.credential}
+              type={datastore.template.type}
+            />
+            <Spacer y={0.5} />
+            <Text color="warner">
+              For security, access to the datastore is restricted - connection
+              attempts from outside the cluster will not succeed.
+            </Text>
+            <Spacer y={0.5} />
+            <Text color="helper">
+              The datastore client of your application should use these
+              credentials to create a connection.{" "}
+              {datastore.template.type.name === "ELASTICACHE" && (
+                <Text color="warner">
+                  The datastore client must connect via SSL.
+                </Text>
+              )}
+            </Text>
+            <Spacer y={1} />
+            <ConnectAppButton
+              onClick={() => {
+                setShowConnectAppsModal(true);
+              }}
+            >
+              <I className="material-icons add-icon">add</I>
+              Inject credentials into an app
+            </ConnectAppButton>
+            {showConnectAppsModal && (
+              <ConnectAppsModal
+                closeModal={() => {
+                  setShowConnectAppsModal(false);
+                }}
+                apps={[]}
+                onSubmit={async () => {}}
+              />
+            )}
+          </>
+        )}
+      </div>
+      <div style={{ width: "100%", height: "100%", paddingLeft: "10px" }}>
+        <Text size={16}>Local connection</Text>
+        <Spacer y={0.5} />
+        <Text color="helper">
+          For local connection, you can create a temporary, secure tunnel to
+          this datastore using the{" "}
+          <Link
+            to="https://docs.porter.run/standard/cli/command-reference/porter-datastore-connect"
+            target="_blank"
+          >
+            <Text>Porter CLI</Text>
+          </Link>
+        </Text>
+        <Spacer y={0.5} />
+        <IdContainer>
+          <Code>{`$ porter datastore connect ${datastore.name}`}</Code>
+          <CopyContainer>
+            <CopyToClipboard
+              text={`porter datastore connect ${datastore.name}`}
+            >
+              <CopyIcon src={copy} alt="copy" />
+            </CopyToClipboard>
+          </CopyContainer>
+        </IdContainer>
+      </div>
+    </ConnectTabContainer>
   );
 };
 
 export default ConnectTab;
 
-const CredentialsTabContainer = styled.div`
+const ConnectTabContainer = styled.div`
   width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: row;
 `;
 
 const IdContainer = styled.div`
@@ -114,4 +141,35 @@ const CopyIcon = styled.img`
 
 const Code = styled.span`
   font-family: monospace;
+`;
+
+const ConnectAppButton = styled.div`
+  color: #aaaabb;
+  background: ${({ theme }) => theme.fg};
+  border: 1px solid #494b4f;
+  :hover {
+    border: 1px solid #7a7b80;
+    color: white;
+  }
+  display: flex;
+  align-items: center;
+  border-radius: 5px;
+  height: 40px;
+  font-size: 13px;
+  width: 100%;
+  padding-left: 10px;
+  cursor: pointer;
+  .add-icon {
+    width: 30px;
+    font-size: 20px;
+  }
+`;
+
+const I = styled.i`
+  color: white;
+  font-size: 14px;
+  display: flex;
+  align-items: center;
+  margin-right: 7px;
+  justify-content: center;
 `;

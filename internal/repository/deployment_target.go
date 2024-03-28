52808da7fe0cc843dc4154372356c0cbae69e2b0
d-g-town <66391417+d-g-town@users.noreply.github.com>
2024-03-28 16:35:11 -0400

    support target CLI (#4465)

--- internal/repository/deployment_target.go
+++ internal/repository/deployment_target.go
@@ -8,8 +8,10 @@ import (
 type DeploymentTargetRepository interface {
 	// DeploymentTargetBySelectorAndSelectorType finds a deployment target for a projectID and clusterID by its selector and selector type
 	DeploymentTargetBySelectorAndSelectorType(projectID uint, clusterID uint, selector, selectorType string) (*models.DeploymentTarget, error)
+	// ListForCluster returns all deployment targets for a project and cluster
+	ListForCluster(projectID uint, clusterID uint, preview bool) ([]*models.DeploymentTarget, error)
 	// List returns all deployment targets for a project
-	List(projectID uint, clusterID uint, preview bool) ([]*models.DeploymentTarget, error)
+	List(projectID uint, preview bool) ([]*models.DeploymentTarget, error)
 	// CreateDeploymentTarget creates a new deployment target
 	CreateDeploymentTarget(deploymentTarget *models.DeploymentTarget) (*models.DeploymentTarget, error)
 	// DeploymentTarget retrieves a deployment target by its id if a uuid is provided or by name

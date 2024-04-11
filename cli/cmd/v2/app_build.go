0b53acce2fa2c3343451cb8a382a967333e5dc6a
Ian Edwards <ianedwards559@gmail.com>
2024-04-11 10:22:21 -0400

    pull image before build by default

--- cli/cmd/v2/app_build.go
+++ cli/cmd/v2/app_build.go
@@ -37,6 +37,8 @@ type AppBuildInput struct {
 	ImageTag string
 	// PatchOperations is the set of patch operations to apply to the app build
 	PatchOperations []v2.PatchOperation
+	// PullImageBeforeBuild is a flag indicating whether to pull the previous image before building
+	PullImageBeforeBuild bool
 }
 
 // AppBuild builds an app using a combination of the provided flag values and build settings from the latest app revision
@@ -95,12 +97,13 @@ func AppBuild(ctx context.Context, inp AppBuildInput) error {
 	}
 
 	buildInput, err := buildInputFromBuildSettings(buildInputFromBuildSettingsInput{
-		projectID: cliConf.Project,
-		appName:   inp.AppName,
-		commitSHA: tagForBuild,
-		image:     buildSettings.Image,
-		build:     buildSettings.Build,
-		buildEnv:  buildEnvVariables,
+		projectID:            cliConf.Project,
+		appName:              inp.AppName,
+		commitSHA:            tagForBuild,
+		image:                buildSettings.Image,
+		build:                buildSettings.Build,
+		buildEnv:             buildEnvVariables,
+		pullImageBeforeBuild: inp.PullImageBeforeBuild,
 	})
 	if err != nil {
 		return fmt.Errorf("error creating build input from build settings: %w", err)

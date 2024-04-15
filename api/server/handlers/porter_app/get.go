aaf8fd50ef6e41bec71be6590a6b7addb8119fe2
dgtown <david@porter.run>
2024-04-15 11:57:44 -0400

    helm revision number

--- api/server/handlers/porter_app/get.go
+++ api/server/handlers/porter_app/get.go
@@ -77,6 +77,7 @@ func (c *GetPorterAppHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) 
 		}
 
 		c.WriteResult(w, r, app.ToPorterAppTypeWithRevision(helmRelease.Version))
+		return
 	}
 
 	c.WriteResult(w, r, app.ToPorterAppType())

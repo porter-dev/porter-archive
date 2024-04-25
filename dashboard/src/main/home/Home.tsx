b9bc775f254b4163bf2414bda13b90c4e5cc1de2
Mauricio Araujo <mauricio.araujo@proton.me>
2024-04-25 12:14:52 -0400

    remove test

--- dashboard/src/main/home/Home.tsx
+++ dashboard/src/main/home/Home.tsx
@@ -387,8 +387,7 @@ const Home: React.FC<Props> = (props) => {
   };
 
   const showCardBanner = !hasPaymentEnabled;
-  // const trialExpired = plan && isTrialExpired(plan.trial_info.ending_before);
-  const trialExpired = true;
+  const trialExpired = plan && isTrialExpired(plan.trial_info.ending_before);
 
   return (
     <ThemeProvider

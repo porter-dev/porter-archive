517c54a7d5f5f9d06ba750aa3c874bec0724b13c
Mauricio Araujo <mauricio.araujo@proton.me>
2024-04-25 10:50:05 -0400

    Add porter cloud prefix to ingest events

--- api/server/handlers/billing/ingest.go
+++ api/server/handlers/billing/ingest.go
@@ -66,7 +66,7 @@ func (c *IngestEventsHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) 
 		telemetry.AttributeKV{Key: "usage-events-count", Value: len(ingestEventsRequest.Events)},
 	)
 
-	// For Porter Cloud events, we apend a prefix to avoid collisions
+	// For Porter Cloud events, we apend a prefix to avoid collisions before sending to Metronome
 	if proj.EnableSandbox {
 		for i := range ingestEventsRequest.Events {
 			ingestEventsRequest.Events[i].CustomerID = fmt.Sprintf("porter-cloud-%s", ingestEventsRequest.Events[i].CustomerID)

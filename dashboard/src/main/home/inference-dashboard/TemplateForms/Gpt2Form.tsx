32706403eb850b993c90400142ef75b8bfe7d3fe
jusrhee <justin@porter.run>
2024-05-14 19:16:03 -0400

    updated inference tab

--- dashboard/src/main/home/inference-dashboard/TemplateForms/Gpt2Form.tsx
+++ dashboard/src/main/home/inference-dashboard/TemplateForms/Gpt2Form.tsx
@@ -1,12 +1,10 @@
 import React from "react";
-import { Controller, useFormContext } from "react-hook-form";
 
 import Button from "components/porter/Button";
 import { ControlledInput } from "components/porter/ControlledInput";
 import Spacer from "components/porter/Spacer";
 import Text from "components/porter/Text";
 import VerticalSteps from "components/porter/VerticalSteps";
-import { type Gpt2 } from "lib/models";
 
 const Gpt2Form: React.FC = () => {
   return (
@@ -23,20 +21,6 @@ const Gpt2Form: React.FC = () => {
             width="300px"
             placeholder="ex: klara-af-b2"
           />
-        </>,
-        <>
-          <Text size={16}>Resources</Text>
-          <Spacer y={0.5} />
-          <Text color="helper">
-            Configure compute resources for your model. You can change these
-            later.
-          </Text>
-          <Spacer y={0.5} />
-          <Text>Replicas</Text>
-          <Spacer y={0.5} />
-          <Text color="helper">
-            Configure the number of replicas for your model.
-          </Text>
         </>,
         <Button key={2}>Deploy model</Button>,
       ]}

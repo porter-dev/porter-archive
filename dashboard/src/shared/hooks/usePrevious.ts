b3bb5cbd9357193fa92579939d84eb4ef5589dee
ianedwards <ianedwards559@gmail.com>
2024-05-21 16:28:45 -0400

    first half of deletions (#4669)

--- dashboard/src/shared/hooks/usePrevious.ts
+++
@@ -1,9 +0,0 @@
-import { useEffect, useRef } from "react";
-
-export const usePrevious = (value: any, initialValue: any) => {
-  const ref = useRef(initialValue);
-  useEffect(() => {
-    ref.current = value;
-  });
-  return ref.current;
-};
